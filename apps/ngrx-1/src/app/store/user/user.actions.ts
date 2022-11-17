a675cf101f83104f9eaae3318bccbf305ac0d86c
thomas <thomas.ext@squadracer.com>
2022-11-17 19:15:03 +0100

    feat(exercice2): typo

--- apps/ngrx-1/src/app/store/user/user.actions.ts
+++ apps/ngrx-1/src/app/store/user/user.actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from './user.model';
 
-export const loadUsers = createAction('[User] Load Users');
+export const loadUsers = createAction('[User] Load User');
 
 export const loadUsersSuccess = createAction(
   '[User] Load Users Success',
