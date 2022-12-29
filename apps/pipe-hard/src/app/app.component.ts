e159e6dee691a8598e783e99a92fa7836d9a9273
tom-englert <mail@tom-englert.de>
2022-12-29 16:04:11 +0100

    feat(challenge10): propose solution

--- apps/pipe-hard/src/app/app.component.ts
+++ apps/pipe-hard/src/app/app.component.ts
@@ -1,18 +1,57 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { PersonUtils } from './person.utils';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Utils = { [key: string]: (...args: any[]) => any };
+
+function execute<TUtils extends Utils, TFunction extends keyof TUtils>(
+  utils: TUtils,
+  functionName: TFunction,
+  args: Parameters<typeof utils[TFunction]>
+) {
+  const func = utils[functionName];
+  return func(...args);
+}
+
+export abstract class CallFunctionPipeBase<TUtils extends Utils> {
+  constructor(private readonly utils: TUtils) {}
+
+  transform(
+    functionName: keyof TUtils,
+    ...args: Parameters<TUtils[typeof functionName]>
+  ) {
+    return execute(this.utils, functionName, args);
+  }
+}
+
+@Pipe({
+  standalone: true,
+  pure: true,
+  name: 'personUtils',
+})
+export class CallPersonUtilsPipe
+  extends CallFunctionPipeBase<typeof PersonUtils>
+  implements PipeTransform
+{
+  constructor() {
+    super(PersonUtils);
+  }
+}
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, CallPersonUtilsPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let activity of activities">
       {{ activity.name }} :
       <div
         *ngFor="let person of persons; let index = index; let isFirst = first">
-        {{ showName(person.name, index) }}
-        {{ isAllowed(person.age, isFirst, activity.minimumAge) }}
+        {{ 'showName' | personUtils : person.name : index }}
+        {{
+          'isAllowed' | personUtils : person.age : isFirst : activity.minimumAge
+        }}
       </div>
     </div>
   `,
@@ -29,8 +68,4 @@ export class AppComponent {
     { name: 'hiking', minimumAge: 25 },
     { name: 'dancing', minimumAge: 1 },
   ];
-
-  showName = PersonUtils.showName;
-
-  isAllowed = PersonUtils.isAllowed;
 }
