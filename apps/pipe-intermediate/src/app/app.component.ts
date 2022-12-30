634f1e5ec2bb7911dff988d1bbc9eb5ad1a75f8e
tom-englert <mail@tom-englert.de>
2022-12-30 08:42:13 +0100

    feat(challenge9): adapt sample to updated description

--- apps/pipe-intermediate/src/app/app.component.ts
+++ apps/pipe-intermediate/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { NgFor } from '@angular/common';
-import { Component, Pipe, PipeTransform } from '@angular/core';
+import {
+  Component,
+  Inject,
+  InjectionToken,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+
+const CallFunctionPipeSource = new InjectionToken('CallFunctionPipeSource');
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 @Pipe({
@@ -8,25 +16,11 @@ import { Component, Pipe, PipeTransform } from '@angular/core';
   name: 'callFunction',
 })
 export class CallFunctionPipe implements PipeTransform {
-  transform(
-    functionName: keyof Omit<CallFunctionPipe, 'transform'>,
-    ...args: any[]
-  ): any {
-    const func = <any>this[functionName];
+  constructor(@Inject(CallFunctionPipeSource) private readonly source: any) {}
+
+  transform(functionName: string, ...args: any[]): any {
+    const func = this.source[functionName];
     return func(...args);
-  }
-
-  showName(name: string, index: number) {
-    // very heavy computation
-    return `Piped: ${name} - ${index}`;
-  }
-
-  isAllowed(age: number, isFirst: boolean) {
-    if (isFirst) {
-      return 'always allowed';
-    } else {
-      return age > 25 ? 'allowed' : 'declined';
-    }
   }
 }
 
@@ -34,6 +28,7 @@ export class CallFunctionPipe implements PipeTransform {
   standalone: true,
   imports: [NgFor, CallFunctionPipe],
   selector: 'app-root',
+  providers: [{ provide: CallFunctionPipeSource, useExisting: AppComponent }],
   template: `
     <div *ngFor="let person of persons; let index = index; let isFirst = first">
       {{ 'showName' | callFunction : person.name : index }}
@@ -47,4 +42,17 @@ export class AppComponent {
     { name: 'Jack', age: 15 },
     { name: 'John', age: 30 },
   ];
+
+  showName(name: string, index: number) {
+    // very heavy computation
+    return `Piped: ${name} - ${index}`;
+  }
+
+  isAllowed(age: number, isFirst: boolean) {
+    if (isFirst) {
+      return 'always allowed';
+    } else {
+      return age > 25 ? 'allowed' : 'declined';
+    }
+  }
 }
