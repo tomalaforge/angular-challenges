5bc8bf8dd7c1e9ccda8e88043af2b930193799c9
balajibadly <balajibalamurugan236@gmail.com>
2022-12-31 10:34:57 +0530

    feat: :zap: pipe intermediate challenge solution
    created new compute pipe and used it in app comp templates
    
    breaking changes - none

--- apps/pipe-intermediate/src/app/app.component.ts
+++ apps/pipe-intermediate/src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'compute',
+  pure: true,
+  standalone: true,
+})
+export class ComputePipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  transform(value: any, funcName: 'showName' | 'isAllowed'): string {
+    return funcName === 'isAllowed'
+      ? this.isAllowed(value.age, value.isFirst)
+      : this.showName(value.name, value.index);
+  }
+
+  private showName(name: string, index: number) {
+    // very heavy computation
+    return `${name} - ${index}`;
+  }
+
+  private isAllowed(age: number, isFirst: boolean) {
+    if (isFirst) {
+      return 'always allowed';
+    } else {
+      return age > 25 ? 'allowed' : 'declined';
+    }
+  }
+}
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, ComputePipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index; let isFirst = first">
-      {{ showName(person.name, index) }}
-      {{ isAllowed(person.age, isFirst) }}
+      {{ { name: person?.name, index } | compute : 'showName' }}
+      {{ { age: person?.age, isFirst } | compute : 'isAllowed' }}
     </div>
   `,
 })
