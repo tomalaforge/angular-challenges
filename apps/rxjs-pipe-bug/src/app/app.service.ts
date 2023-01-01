667b7e9a7458dbeae4d589d7f2f8bccf3300d6dd
tom-englert <mail@tom-englert.de>
2023-01-01 14:26:18 +0100

    feat(challenge11): propose solution

--- apps/rxjs-pipe-bug/src/app/app.service.ts
+++ apps/rxjs-pipe-bug/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { merge, mergeMap, Observable, of, take } from 'rxjs';
+import { combineLatest, map, Observable, switchMap, take } from 'rxjs';
 import { LocalDBService, TopicType } from './localDB.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,12 +11,10 @@ export class AppService {
   deleteOldTopics(type: TopicType): Observable<boolean> {
     return this.dbService.searchByType(type).pipe(
       take(1),
-      mergeMap((topicToDelete) =>
-        topicToDelete.length > 0
-          ? topicToDelete
-              .map((t) => this.dbService.deleteOneTopic(t.id))
-              .reduce((acc, curr) => merge(acc, curr), of(true))
-          : of(true)
+      switchMap((topicsToDelete) =>
+        combineLatest(
+          topicsToDelete.map((t) => this.dbService.deleteOneTopic(t.id))
+        ).pipe(map((results) => !results.some((result) => !result)))
       )
     );
   }
