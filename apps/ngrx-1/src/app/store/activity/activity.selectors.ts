855f482fe64551aa8a90e50dafaf77724e98a7ca
Alex P. Scigalszky <alexpablo.90@gmail.com>
2023-01-03 21:21:22 +0000

    chore: activity with teacher list

--- apps/ngrx-1/src/app/store/activity/activity.selectors.ts
+++ apps/ngrx-1/src/app/store/activity/activity.selectors.ts
@@ -1,5 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { activityFeatureKey, ActivityState } from './activity.reducer';
+import { selectStatusState } from '../status/status.selectors';
+import { ActivityWithTeachers } from './activity.model';
 
 export const selectActivityState =
   createFeatureSelector<ActivityState>(activityFeatureKey);
@@ -7,4 +9,18 @@ export const selectActivityState =
 export const selectActivities = createSelector(
   selectActivityState,
   (state) => state.activities
+);
+
+export const selectActivitiesWithTeachers = createSelector(
+  selectActivities,
+  selectStatusState,
+  (activities, statusState) => {
+    return activities.map(
+      (activity) =>
+        ({
+          teachers: statusState.teachersMap.get(activity.type) ?? [],
+          ...activity,
+        } as ActivityWithTeachers)
+    );
+  }
 );
