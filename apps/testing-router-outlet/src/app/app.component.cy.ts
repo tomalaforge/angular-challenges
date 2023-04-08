6cc4ff7991a6b9c0db77e6cd5d059ae572cd4561
thomas <thomas.ext@squadracer.com>
2023-04-08 13:40:32 +0200

    feat(cy): add cypress test for router outlet testing

--- apps/testing-router-outlet/src/app/app.component.cy.ts
+++ apps/testing-router-outlet/src/app/app.component.cy.ts
@@ -1,33 +1,71 @@
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 describe(AppComponent.name, () => {
+  const setup = () => {
+    cy.mount(AppComponent, {
+      providers: [provideRouter(appRoutes)],
+    });
+    cy.get('nav a:first').click();
+  };
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', () => {
-      //todo
+      setup();
+
+      cy.get('input[name=bookName]').as('bookControl').clear();
+      cy.contains('Search criteria is required!');
+
+      cy.get('@bookControl').type('kill');
+      cy.contains('Search criteria is required!').should('not.exist');
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', () => {
-      //todo
+      setup();
+
+      cy.get('input[name=bookName]').clear().type('sddfs');
+
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.contains('No book found for this search');
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('input[name=bookName]').clear().type('kill');
+
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('input[name=bookName]').clear().type('KiLl');
+
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
     it('Then shows a list of books', () => {
-      //todo
+      setup();
+
+      cy.get('input[name=bookName]').clear().type('Tolkien');
+
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length', 2);
     });
   });
 });
