2bebca2c3e71c8b008215e0a97ece709c7150fa9
thomas <thomas.ext@squadracer.com>
2023-04-25 16:07:40 +0200

    feat(challenge20): add solution for cypress

--- apps/testing-modal/src/app/app.component.cy.ts
+++ apps/testing-modal/src/app/app.component.cy.ts
@@ -1,19 +1,60 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
-describe(AppComponent.name, () => {
-  const setup = () => {
-    cy.mount(AppComponent);
-  };
+describe('AppComponent', () => {
+  describe('When nothing has been typed inside the name input, and When clicking on Confirm', () => {
+    test('Then display error modal', async () => {
+      cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
 
-  test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
-    setup();
+      cy.contains('button', 'Confirm').click();
+
+      cy.get('mat-dialog-container').contains('h1', 'Error');
+    });
   });
 
-  test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', () => {
-    setup();
-  });
+  describe('Given a name and When clicking on Confim', () => {
+    test('Then display Profil modal', async () => {
+      cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
 
-  test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', () => {
-    setup();
+      cy.get('input').type('toto');
+
+      cy.contains('button', 'Confirm').click();
+
+      cy.get('mat-dialog-container').contains('h1', 'Profil');
+    });
+
+    describe('When clicking on Cancel in the profile modal', () => {
+      test('Then show "Name is invalid" error message', async () => {
+        cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
+
+        cy.get('input').type('toto');
+
+        cy.contains('button', 'Confirm').click();
+
+        cy.get('mat-dialog-container').contains('h1', 'Profil');
+        cy.get('mat-dialog-container').contains('Name: toto');
+
+        cy.contains('button', 'Cancel').click();
+
+        cy.contains('Name is invalid !!');
+      });
+    });
+
+    describe('When clicking on Confirm in the profile modal', () => {
+      test('Then show "Name has been submitted" message', async () => {
+        cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
+
+        cy.get('input').type('toto');
+
+        cy.contains('button', 'Confirm').click();
+
+        cy.get('mat-dialog-container').contains('h1', 'Profil');
+        cy.get('mat-dialog-container').contains('Name: toto');
+
+        cy.contains('button', 'Confirmation').click();
+
+        cy.contains('Name has been submitted');
+      });
+    });
   });
 });
