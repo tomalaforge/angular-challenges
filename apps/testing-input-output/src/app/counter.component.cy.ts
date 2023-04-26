1d84071292ceb36de634180fee38d5e32712d90d
Pascal Küsgen <pascalkuesgen@gmail.com>
2023-04-26 14:54:18 +0200

    feat: solve problem 19

--- apps/testing-input-output/src/app/counter.component.cy.ts
+++ apps/testing-input-output/src/app/counter.component.cy.ts
@@ -1,11 +1,36 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
 
 describe(CounterComponent.name, () => {
+  function clickOnIncrementControl() {
+    cy.contains('button', 'Increment').click();
+  }
+
+  function clickSendControl() {
+    cy.contains('button', 'Send').click();
+  }
+
   it('using createOutputSpy', () => {
-    cy.mount(CounterComponent);
+    cy.mount(CounterComponent, {
+      componentProperties: {
+        send: createOutputSpy('sendSpy'),
+      },
+    });
+
+    clickOnIncrementControl();
+    clickSendControl();
+
+    cy.get('@sendSpy').should('have.been.calledWith', 1);
   });
 
   it('using autoSpyOutputs', () => {
-    cy.mount(CounterComponent);
+    cy.mount(CounterComponent, {
+      autoSpyOutputs: true,
+    });
+
+    clickOnIncrementControl();
+    clickSendControl();
+
+    cy.get('@sendSpy').should('have.been.calledWith', 1);
   });
 });
--- apps/testing-input-output/src/app/counter.component.spec.ts
+++ apps/testing-input-output/src/app/counter.component.spec.ts
@@ -1,8 +1,36 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
   test('set input and listen to output', async () => {
-    await render(CounterComponent);
+    const initialValue = 4711;
+    const sendValue = jest.fn();
+
+    await render(CounterComponent, {
+      componentProperties: {
+        initialValue,
+      },
+      componentOutputs: {
+        send: {
+          emit: sendValue,
+        } as any,
+      },
+    });
+
+    const incrementControl = screen.getByRole('button', { name: /increment/i });
+    const decrementControl = screen.getByRole('button', { name: /decrement/i });
+    const sendControl = screen.getByRole('button', { name: /send/i });
+
+    expect(screen.getByText(`Counter: ${initialValue}`)).toBeVisible();
+
+    fireEvent.click(incrementControl);
+    expect(screen.getByText(`Counter: ${initialValue + 1}`)).toBeVisible();
+
+    fireEvent.click(decrementControl);
+    fireEvent.click(decrementControl);
+    expect(screen.getByText(`Counter: ${initialValue - 1}`)).toBeVisible();
+
+    fireEvent.click(sendControl);
+    expect(sendValue).toHaveBeenCalledWith(initialValue - 1);
   });
 });
