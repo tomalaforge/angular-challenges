2cfa41d5ecbd09c5358908466a74e1b0dd67d275
Paul Chesa <paulchesa1@gmail.com>
2023-05-05 05:19:30 +0300

    fix(nav-button): remove conflicting href

--- apps/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,12 +1,11 @@
-/* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <div>
       <ng-content></ng-content>
-    </a>
+    </div>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
