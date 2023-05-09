85797ae153e3bf496fb2cea9b8c234c268e27d4e
ronderksen <ron.derksen@trivento.nl>
2023-05-09 14:30:44 +0200

    feat: implement smooth scrolling and routerlink

--- apps/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,17 +1,40 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="href.split('#')[0]" [fragment]="href.split('#')[1]">
       <ng-content></ng-content>
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
-export class NavButtonComponent {
+export class NavButtonComponent implements OnInit, AfterViewInit {
   @Input() href = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    document.body.scrollIntoView();
+  }
+
+  ngAfterViewInit() {
+    this.route.fragment.subscribe((fragment) => {
+      try {
+        if (fragment) {
+          document
+            .getElementById(fragment)
+            ?.scrollIntoView({ behavior: 'smooth' });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  }
 }
