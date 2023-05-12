3f22a7b34178c46b5f32eed252d026ad1f6bbb06
Felie Bento <felipe.16costa@gmail.com>
2023-05-11 23:13:48 -0300

    refactor: answer 1

--- apps/projection/src/app/component/city-card/city-card.component.ts
+++ apps/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CardComponent } from '../../ui/card/card.component';
+import { City } from '../../model/city.model';
+import { CardType } from '../../model/card.model';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { CityStore } from '../../data-access/city.store';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: ` <app-card [list]="cities" [type]="cardType"> </app-card>`,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  styles: [
+    `
+      app-city-card div {
+        background-color: orange;
+      }
+    `,
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  cardType = CardType.CITY;
 
-  ngOnInit(): void {}
+  constructor(private http: FakeHttpService, private store: CityStore) {}
+
+  ngOnInit(): void {
+    this.fetchCities();
+  }
+
+  fetchCities(): void {
+    this.http.fetchCities$.subscribe((cities: City[]) =>
+      this.store.addAll(cities)
+    );
+    this.store.cities$.subscribe((cities: City[]) => (this.cities = cities));
+  }
 }
