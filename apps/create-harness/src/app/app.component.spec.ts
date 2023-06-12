ec10564245228f60fececf8d3e9c44bbb66308f5
thomas <thomas.ext@squadracer.com>
2023-06-12 21:55:57 +0200

    feat(Answer:24): answer to create harness challenge

--- apps/create-harness/src/app/app.component.spec.ts
+++ apps/create-harness/src/app/app.component.spec.ts
@@ -1,22 +1,57 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import { MySliderHarness } from './slider.harness';
 
 describe('AppComponent', () => {
+  async function setup() {
+    const { fixture } = await render(AppComponent);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    return loader;
+  }
+
   describe('When clicking 2 times on plus button of first slider', () => {
     test('Then value is 16', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+      const sliders = await loader.getAllHarnesses(MySliderHarness);
+      const slider = sliders[0];
+
+      await slider.clickPlus();
+      await slider.clickPlus();
+      expect(await slider.getValue()).toEqual(16);
     });
   });
 
   describe('When clicking 1 time on plus button and two times on minus button of first slider', () => {
     test('Then value is still 10', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+      const sliders = await loader.getAllHarnesses(MySliderHarness);
+      const slider = sliders[0];
+
+      await slider.clickPlus();
+      await slider.clickMinus();
+      await slider.clickMinus();
+      expect(await slider.getValue()).toEqual(10);
     });
   });
 
   describe('When clicking 4 times on plus button of slider 1', () => {
     test('Then slider 2 is enabled', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+      const slider1 = await loader.getHarness(
+        MySliderHarness.with({ minValue: 10 })
+      );
+      const slider2 = await loader.getHarness(
+        MySliderHarness.with({ minValue: 0 })
+      );
+
+      expect(await slider2.disabled()).toBe(true);
+
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      expect(await slider2.disabled()).toBe(false);
     });
   });
 });
--- apps/create-harness/src/app/slider.harness.ts
+++ apps/create-harness/src/app/slider.harness.ts
@@ -1,5 +1,72 @@
-import { ComponentHarness } from '@angular/cdk/testing';
+import {
+  AsyncFactoryFn,
+  BaseHarnessFilters,
+  ComponentHarness,
+  ComponentHarnessConstructor,
+  HarnessPredicate,
+} from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatSliderHarness } from '@angular/material/slider/testing';
+
+export interface SliderHarnessFilters extends BaseHarnessFilters {
+  minValue?: number;
+}
 
 export class MySliderHarness extends ComponentHarness {
   static hostSelector = 'app-slider';
+
+  static with<T extends MySliderHarness>(
+    this: ComponentHarnessConstructor<T>,
+    options: SliderHarnessFilters = {}
+  ): HarnessPredicate<T> {
+    return new HarnessPredicate(this, options).addOption(
+      'minValue',
+      options.minValue,
+      async (harness, value) => {
+        return (await harness.getMinValue()) === value;
+      }
+    );
+  }
+
+  protected getPlusButton: AsyncFactoryFn<MatButtonHarness> = this.locatorFor(
+    MatButtonHarness.with({ selector: '#plusButton' })
+  );
+
+  protected getMinusButton: AsyncFactoryFn<MatButtonHarness> = this.locatorFor(
+    MatButtonHarness.with({ selector: '#minusButton' })
+  );
+
+  protected getSlider: AsyncFactoryFn<MatSliderHarness> =
+    this.locatorFor(MatSliderHarness);
+
+  protected async getThumb() {
+    return (await this.getSlider()).getEndThumb();
+  }
+
+  async clickPlus(): Promise<void> {
+    return (await this.getPlusButton()).click();
+  }
+
+  async clickMinus(): Promise<void> {
+    return (await this.getMinusButton()).click();
+  }
+
+  async getValue(): Promise<number> {
+    const thumb = await this.getThumb();
+    return thumb.getValue();
+  }
+
+  async getMinValue(): Promise<number> {
+    const thumb = await this.getThumb();
+    return thumb.getMinValue();
+  }
+
+  async disabled(): Promise<boolean> {
+    return (await this.getSlider()).isDisabled();
+  }
+
+  async setValue(value: number): Promise<void> {
+    const thumb = await this.getThumb();
+    thumb.setValue(value);
+  }
 }
