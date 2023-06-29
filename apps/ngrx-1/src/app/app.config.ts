c64c1f7283393f9e300d4b416e6e4b9874aac023
Alan Dragicevich <alan@geneious.com>
2023-06-29 15:24:38 +1200

    fix(ngrx-1): reducers missing from config
    Due to a recent refactor of upgrading to Angular v16, the reducers weren't
    copied over to the new `app.config.ts` file for the `ngrx-1` module.
    
    Also fixed a minor spelling mistake in the related README.md file.

--- apps/ngrx-1/src/app/app.config.ts
+++ apps/ngrx-1/src/app/app.config.ts
@@ -4,6 +4,21 @@ import { provideEffects } from '@ngrx/effects';
 import { ActivityEffects } from './store/activity/activity.effects';
 import { UserEffects } from './store/user/user.effects';
 import { StatusEffects } from './store/status/status.effects';
+import {
+  activityFeatureKey,
+  activityReducer,
+} from './store/activity/activity.reducer';
+
+import { statusFeatureKey, statusReducer } from './store/status/status.reducer';
+
+import { userFeatureKey, userReducer } from './store/user/user.reducer';
+
+const reducers = {
+  [statusFeatureKey]: statusReducer,
+  [activityFeatureKey]: activityReducer,
+  [userFeatureKey]: userReducer,
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideStore(reducers),
