96f2a470e26cbd158bf3b285dcc71b1c01ab3874
ThomasL <thomasl@sphinxonline.com>
2023-07-05 14:35:09 +0200

    feat(solution28): answer 28 simple checkbox test

--- apps/testing-checkbox/src/app/app.component.spec.ts
+++ apps/testing-checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,22 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  const setup = async () => {
+    const user = userEvent.setup();
+    const result = await render(AppComponent);
+    return { result, user };
+  };
+
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
-      await render(AppComponent);
+      const { user } = await setup();
+
+      const checkbox = screen.getByLabelText('Agreed');
+      user.click(checkbox);
+      const button = screen.getByRole('button', { name: 'Submit' });
+      expect(button).toBeDisabled();
     });
   });
 });
