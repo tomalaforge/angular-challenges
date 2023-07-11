31366de7a6d7d41d24e26a16a536a96c5ccffdea
thomas <thomas.ext@squadracer.com>
2023-07-11 20:43:01 +0200

    fix(crud): fix crud

--- apps/crud/src/main.ts
+++ apps/crud/src/main.ts
@@ -2,11 +2,6 @@ import { appConfig } from './app/app.config';
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
