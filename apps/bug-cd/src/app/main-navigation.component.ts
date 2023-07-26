d27876f2c9769fd095640fca3076fce5cd7803e0
Getsov <valsvetget@abv.bg>
2023-07-26 20:21:56 +0300

    from imperative to declarative

--- apps/bug-cd/src/app/main-navigation.component.ts
+++ apps/bug-cd/src/app/main-navigation.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FakeServiceService } from './fake.service';
-import { tap } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 interface MenuItem {
   path: string;
@@ -35,45 +35,37 @@ interface MenuItem {
   },
 })
 export class NavigationComponent {
-  @Input() menus: MenuItem[] | undefined;
+  @Input() menus: MenuItem[] | undefined | null;
 }
 
 @Component({
   standalone: true,
   imports: [NavigationComponent, NgIf, AsyncPipe],
   template: `
-    <ng-container *ngIf="info$ | async as info">
-      <ng-container *ngIf="info !== null; else noInfo">
-        <!-- Switching to a variable, instead of calling a function everytime a CD hits -->
-        <app-nav [menus]="menus" />
-      </ng-container>
+    <ng-container *ngIf="menus$ | async as menus; else noInfo">
+      <!-- Switching to Observable, instead of calling a function everytime a CD hits -->
+      <app-nav [menus]="menus" />
     </ng-container>
 
     <ng-template #noInfo>
-      <!-- Switching to a variable, instead of calling a function everytime a CD hits -->
-      <app-nav [menus]="menus" />
+      <!-- Switching to Observable, instead of calling a function everytime a CD hits -->
+      <app-nav [menus]="emptyMenu$ | async" />
     </ng-template>
   `,
   host: {},
 })
 export class MainNavigationComponent {
   private fakeBackend = inject(FakeServiceService);
-  info$: any;
-  menus: MenuItem[] = [];
+  menus$!: Observable<MenuItem[]>;
+  emptyMenu$!: Observable<MenuItem[]>;
   constructor() {
-    this.info$ = this.fakeBackend.getInfoFromBackend().pipe(
-      // Using tap operator to update menus via the function every time info$ changes
-      tap((info) => {
-        if (info !== null) {
-          this.menus = this.getMenu(info);
-        } else {
-          this.menus = this.getMenu('');
-        }
-      })
-    );
+    this.menus$ = this.fakeBackend
+      .getInfoFromBackend()
+      .pipe(map((info) => this.getMenu(info || '')));
+    this.emptyMenu$ = of(this.getMenu(''));
   }
 
-  getMenu(prop: string) {
+  getMenu(prop: string): MenuItem[] {
     return [
       { path: '/foo', name: `Foo ${prop}` },
       { path: '/bar', name: `Bar ${prop}` },
