7c060c425bc939751d823008a2cb3d916ade64a5
Mathis Vester <m.vester@forstify.de>
2023-08-23 16:16:42 +0200

    fix(challenge11): fix chaining operators

--- apps/rxjs-pipe-bug/src/app/app.service.ts
+++ apps/rxjs-pipe-bug/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { merge, mergeMap, Observable, of, take } from 'rxjs';
+import { concatMap, map, mergeAll, Observable, of, take, toArray } from 'rxjs';
 import { LocalDBService, TopicType } from './localDB.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,11 +11,14 @@ export class AppService {
   deleteOldTopics(type: TopicType): Observable<boolean> {
     return this.dbService.searchByType(type).pipe(
       take(1),
-      mergeMap((topicToDelete) =>
+      concatMap((topicToDelete) =>
         topicToDelete.length > 0
-          ? topicToDelete
-              .map((t) => this.dbService.deleteOneTopic(t.id))
-              .reduce((acc, curr) => merge(acc, curr), of(true))
+          ? of(topicToDelete).pipe(
+              mergeAll(),
+              concatMap((t) => this.dbService.deleteOneTopic(t.id)),
+              toArray(),
+              map((t) => t.reduce((acc, curr) => (!curr ? curr : acc), true))
+            )
           : of(true)
       )
     );
