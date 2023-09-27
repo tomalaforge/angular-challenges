dadd18a473c20b49e2559d9150393dee64499a3b
jdegand <70610011+jdegand@users.noreply.github.com>
2023-09-26 21:25:04 -0400

    feat: add tests

--- apps/testing-nested/src/app/child.component.cy.ts
+++ apps/testing-nested/src/app/child.component.cy.ts
@@ -4,15 +4,46 @@ describe('ChildComponent', () => {
   const setup = () => {
     cy.mount(ChildComponent);
   };
+
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
       setup();
+
+      cy.window()
+        .its('console')
+        .then((console) => {
+          cy.spy(console, 'log').as('log');
+        });
+
+      cy.get(`[data-cy="input"]`).clear();
+
+      cy.get('button').click();
+
+      cy.get('p').should('have.text', 'Title is required !!!');
+
+      cy.get('@log').should('not.have.been.called'); // really not necessary ?
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
       setup();
+
+      cy.window()
+        .its('console')
+        .then((console) => {
+          cy.spy(console, 'log').as('log');
+        });
+
+      cy.get(`[data-cy="input"]`).clear();
+
+      cy.get(`[data-cy="input"]`).type('Good');
+
+      cy.get('p').should('have.text', 'Title is Good');
+
+      cy.get('button').click();
+
+      cy.get('@log').should('be.calledWith', 'Good has been sent !!!');
     });
   });
 });
--- apps/testing-nested/src/app/child.component.spec.ts
+++ apps/testing-nested/src/app/child.component.spec.ts
@@ -1,16 +1,66 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 import { ChildComponent } from './child.component';
+import userEvent from '@testing-library/user-event';
+import { Mock, provideMock } from '@testing-library/angular/jest-utils';
+import { HttpService } from './http.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('ChildComponent', () => {
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
-      await render(ChildComponent);
+      const user = userEvent.setup();
+
+      const component = await render(ChildComponent, {
+        componentProviders: [provideMock(HttpService)],
+      });
+
+      const input = component.getByRole('textbox');
+
+      await user.clear(input);
+
+      const button = component.getByRole('button');
+
+      await user.click(button);
+
+      const error = component.getByText('Title is required !!!');
+
+      expect(error).toBeInTheDocument();
+
+      const httpService = TestBed.inject<HttpService>(
+        HttpService
+      ) as Mock<HttpService>;
+
+      expect(httpService.sendTitle).not.toHaveBeenCalled(); //toHaveBeenCalledTimes(0)
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
-      await render(ChildComponent);
+      const component = await render(ChildComponent, {
+        componentProviders: [provideMock(HttpService)],
+      });
+
+      const user = userEvent.setup();
+
+      const input = component.getByRole('textbox');
+
+      await user.clear(input);
+
+      await user.type(input, 'Good');
+
+      const button = component.getByRole('button');
+
+      await user.click(button);
+
+      const error = component.queryByText('Title is required !!!');
+
+      expect(error).toBe(null);
+
+      const httpService = TestBed.inject<HttpService>(
+        HttpService
+      ) as Mock<HttpService>;
+
+      expect(httpService.sendTitle).toHaveBeenCalledTimes(1);
     });
   });
 });
