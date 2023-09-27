c7cfe5f74bb96a72a4481beb4d5dcc4441eca931
thomas <thomas.ext@squadracer.com>
2023-09-27 10:07:57 +0200

    fix(color): fix color for light mode

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,15 +1,8 @@
 import starlight from '@astrojs/starlight';
-import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [
     starlight({
       title: 'Angular Challenges',
