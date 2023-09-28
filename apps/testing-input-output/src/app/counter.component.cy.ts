6a2ec4a063d659cad2a032c70cdce3d04e01b05a
jdegand <70610011+jdegand@users.noreply.github.com>
2023-09-28 19:05:52 -0400

    feat: add cypress and testing library tests

--- apps/testing-input-output/src/app/counter.component.cy.ts
+++ apps/testing-input-output/src/app/counter.component.cy.ts
@@ -1,13 +1,35 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
 
 describe(CounterComponent.name, () => {
   describe('Given an initualValue of 10', () => {
     it('listen to output using createOutputSpy', () => {
-      cy.mount(CounterComponent);
+      cy.mount(CounterComponent, {
+        componentProperties: {
+          initialValue: 10,
+          send: createOutputSpy<number>('emit'),
+        },
+      });
+
+      cy.get(`[data-cy="increment"]`).click();
+      cy.get(`[data-cy="send"]`).click();
+      cy.get('@emit').should('have.been.calledWith', 11);
     });
 
-    it('listen to output  using autoSpyOutputs', () => {
-      cy.mount(CounterComponent);
+    it('listen to output using autoSpyOutputs', () => {
+      cy.mount(CounterComponent, {
+        autoSpyOutputs: true,
+        componentProperties: {
+          initialValue: 10,
+          send: createOutputSpy<number>('emit'),
+        },
+      });
+
+      cy.get(`[data-cy="decrement"]`).click();
+      cy.get(`[data-cy="decrement"]`).click();
+      cy.get(`[data-cy="send"]`).click();
+
+      cy.get('@sendSpy').should('have.been.calledWith', 8);
     });
   });
 });
--- apps/testing-input-output/src/app/counter.component.spec.ts
+++ apps/testing-input-output/src/app/counter.component.spec.ts
@@ -1,25 +1,105 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 import { CounterComponent } from './counter.component';
+import userEvent from '@testing-library/user-event';
 
 describe('CounterComponent', () => {
   describe('Given an initualValue of 10', () => {
     test('Then counterValue is 10', async () => {
-      await render(CounterComponent);
+      await render(CounterComponent, {
+        componentProperties: { initialValue: 10 },
+      });
+
+      const counter = screen.getByTestId('counter');
+
+      expect(counter).toHaveTextContent('Counter: 10');
     });
 
     describe('When clicking 5 times on increment button', () => {
       test('Then counterValue is 15', async () => {
-        await render(CounterComponent);
+        await render(CounterComponent, {
+          componentProperties: { initialValue: 10 },
+        });
+
+        const counter = screen.getByTestId('counter');
+
+        const button = screen.getAllByRole('button')[0]; //increment is first button
+
+        // need to look up -> any way to do multiple clicks at once?
+        await userEvent.click(button);
+        await userEvent.click(button);
+        await userEvent.click(button);
+        await userEvent.click(button);
+        await userEvent.click(button);
+
+        expect(counter).toHaveTextContent('Counter: 15');
       });
     });
 
     describe('When clicking 2 times on decrement button', () => {
       test('Then counterValue is 8', async () => {
-        await render(CounterComponent);
+        await render(CounterComponent, {
+          componentProperties: { initialValue: 10 },
+        });
+
+        const counter = screen.getByTestId('counter');
+
+        const button = screen.getByText('Decrement');
+
+        await userEvent.click(button);
+        await userEvent.click(button);
+
+        expect(counter).toHaveTextContent('Counter: 8');
       });
+
       describe('When clicking on Send ', () => {
         test('Then emitted value is 8', async () => {
-          await render(CounterComponent);
+          /*
+          const mockSend = jest.fn();
+
+          await render(CounterComponent, {
+            componentProperties: { initialValue: 10, send: mockSend as any }, // this can't work because send.emit property ?
+          })
+ 
+          const counter = screen.getByTestId("counter");
+ 
+          const button = screen.getByText("Decrement");
+          const sendButton = screen.getByText("Send");
+ 
+          await userEvent.click(button);
+          await userEvent.click(button);
+ 
+          expect(counter).toHaveTextContent('Counter: 8');
+
+          await userEvent.click(sendButton);
+
+          expect(mockSend).toHaveBeenCalledWith(8);    
+          */
+
+          const mockSend = jest.fn();
+
+          await render(CounterComponent, {
+            componentInputs: {
+              initialValue: 10,
+            },
+            componentOutputs: {
+              send: {
+                emit: mockSend,
+              } as any,
+            },
+          });
+          const counter = screen.getByTestId('counter');
+
+          const button = screen.getByText('Decrement');
+          const sendButton = screen.getByText('Send');
+
+          await userEvent.click(button);
+          await userEvent.click(button);
+
+          expect(counter).toHaveTextContent('Counter: 8');
+
+          await userEvent.click(sendButton);
+
+          expect(mockSend).toHaveBeenCalledWith(8);
         });
       });
     });
