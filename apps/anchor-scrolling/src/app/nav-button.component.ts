6d5997a0b12830a79d8a6b1ce991cd74656eccfb
Solracort <solracort@gmail.com>
2023-09-28 13:11:38 +0200

    feat(Anchor Navigation): ready and working

--- apps/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,10 +1,11 @@
-/* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
+  imports: [RouterModule],
   template: `
-    <a [href]="href">
+    <a [routerLink]="['/', href]" (click)="scrollTo(anchor)">
       <ng-content></ng-content>
     </a>
   `,
@@ -14,4 +15,32 @@ import { Component, Input } from '@angular/core';
 })
 export class NavButtonComponent {
   @Input() href = '';
+  @Input() anchor?: string | undefined;
+
+  scrollTo(anchor: string | undefined) {
+    if (anchor) {
+      const element = document.getElementById(anchor);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  }
 }
+
+// import { Component, Input } from '@angular/core';
+// @Component({
+//   selector: 'nav-button',
+//   standalone: true,
+//   template: `
+//     <a [routerLink]="['/', href]" fragment="{{ anchor }}">
+//       <ng-content></ng-content>
+//     </a>
+//   `,
+//   host: {
+//     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
+//   },
+// })
+// export class NavButtonComponent {
+//   @Input() href = '';
+//   @Input() anchor?: string | undefined;
+// }
