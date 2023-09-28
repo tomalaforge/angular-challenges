5554156e886ca9fa7565ce49215dbf0800632b2c
jdegand <70610011+jdegand@users.noreply.github.com>
2023-09-28 19:43:24 -0400

    feat: add tests with fireEvent and userEvent

--- apps/testing-checkbox/src/app/app.component.spec.ts
+++ apps/testing-checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,57 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import userEvent from '@testing-library/user-event';
 
 describe('AppComponent', () => {
   describe('When checking the checkbox', () => {
-    it('Then button is enabled', async () => {
+    it('FireEvent', async () => {
       await render(AppComponent);
+
+      const input = screen.getByLabelText('Agreed'); //screen.getByRole('checkbox');
+
+      fireEvent.click(input);
+
+      const submit = screen.getByRole('button', {
+        name: 'Submit',
+      }) as HTMLButtonElement;
+
+      expect(submit.disabled).toBeFalsy();
+    });
+
+    it('UserEvent', async () => {
+      const user = userEvent.setup();
+
+      await render(AppComponent);
+
+      const input = screen.getByLabelText('Agreed');
+      const submit = screen.getByRole('button', {
+        name: 'Submit',
+      }) as HTMLButtonElement;
+
+      await user.click(input);
+
+      expect(submit.disabled).toBeFalsy();
+    });
+
+    it('UserEvent 2', async () => {
+      const user = userEvent.setup();
+
+      await render(AppComponent);
+
+      const checkbox = screen.getByLabelText('Agreed');
+      user.click(checkbox);
+      const button = screen.getByRole('button', { name: 'Submit' });
+      expect(button).toBeDisabled();
+    });
+
+    it('UserEvent 3', async () => {
+      const user = userEvent.setup();
+
+      await render(AppComponent);
+
+      const checkbox = screen.getByLabelText('Agreed');
+      await user.click(checkbox);
+      expect(checkbox).toBeChecked();
     });
   });
 });
