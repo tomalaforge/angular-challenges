f79f8b32dd2c700429d2f2277fe052d4616d96c5
reedanj <james.r@unitaskr.com>
2023-09-29 15:58:08 +0100

    feat: add pipe

--- apps/pipe-easy/src/app/app.component.ts
+++ apps/pipe-easy/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'heavyComputation',
+  standalone: true,
+})
+export class HeavyComputationPipe implements PipeTransform {
+  transform(value: string, index: number): string {
+    return `${value} - ${index}`;
+  }
+}
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, HeavyComputationPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
+      {{ person | heavyComputation : index }}
     </div>
   `,
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
 }
