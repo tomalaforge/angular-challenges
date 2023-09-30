dc413b580900cabb79ec30a43e89e06cc18568dc
arthur <a.lannelucq@gmail.com>
2023-09-30 13:41:17 +0200

    feat: display message only when all items have been deleted

--- apps/rxjs-pipe-bug/src/app/app.service.ts
+++ apps/rxjs-pipe-bug/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { merge, mergeMap, Observable, of, take } from 'rxjs';
+import { forkJoin, map, mergeMap, Observable, take } from 'rxjs';
 import { LocalDBService, TopicType } from './localDB.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,13 +11,10 @@ export class AppService {
   deleteOldTopics(type: TopicType): Observable<boolean> {
     return this.dbService.searchByType(type).pipe(
       take(1),
-      mergeMap((topicToDelete) =>
-        topicToDelete.length > 0
-          ? topicToDelete
-              .map((t) => this.dbService.deleteOneTopic(t.id))
-              .reduce((acc, curr) => merge(acc, curr), of(true))
-          : of(true)
-      )
+      mergeMap((topics) =>
+        forkJoin(topics.map(({ id }) => this.dbService.deleteOneTopic(id)))
+      ),
+      map((deletions) => deletions.every(Boolean))
     );
   }
 }
