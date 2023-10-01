620ac8b42381243bde7242ba273a0f85057d892a
Devesh <deveshachaudhari@gmail.com>
2023-10-01 16:36:34 +0530

    feat: angular 34 onPush

--- apps/performance/default-onpush/src/app/random.component.ts
+++ apps/performance/default-onpush/src/app/random.component.ts
@@ -1,10 +1,11 @@
 import { CDFlashingDirective } from '@angular-challenges/shared/directives';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-random',
   standalone: true,
   template: `<div cd-flash>I do nothing but I'm here</div>`,
   imports: [CDFlashingDirective],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RandomComponent {}
