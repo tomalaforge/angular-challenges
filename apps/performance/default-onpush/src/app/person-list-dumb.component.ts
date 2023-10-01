b5f5a7dfa3be781fc9944bb61478c0b76dc91716
Jesc6417 <sebastien.clauzel@gmail.com>
2023-10-01 23:42:03 +0200

    refactor: answer to default vs OnPush challenge

---
+++ apps/performance/default-onpush/src/app/person-list-dumb.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { CDFlashingDirective } from '@angular-challenges/shared/directives';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-person-list-dumb',
+  template: ` <mat-list-item cd-flash class="text-orange-500">
+    <div MatListItemLine class="flex justify-between">
+      <h3 title="Name">
+        {{ name }}
+      </h3>
+    </div>
+  </mat-list-item>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatListModule, CDFlashingDirective, CommonModule],
+  host: {
+    class: 'w-full flex flex-col items-center',
+  },
+  standalone: true,
+})
+export class PersonListDumbComponent {
+  @Input() name!: string;
+}
--- apps/performance/default-onpush/src/app/person-list.component.ts
+++ apps/performance/default-onpush/src/app/person-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { CDFlashingDirective } from '@angular-challenges/shared/directives';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,8 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { InputComponent } from './input.component';
+import { PersonListDumbComponent } from './person-list-dumb.component';
 
 @Component({
   selector: 'app-person-list',
@@ -19,50 +21,34 @@ import { MatListModule } from '@angular/material/list';
     MatInputModule,
     MatChipsModule,
     CDFlashingDirective,
+    InputComponent,
+    PersonListDumbComponent,
   ],
   template: `
     <h1 cd-flash class="font-semibold text-center" title="Title">
       {{ title | titlecase }}
     </h1>
 
-    <mat-form-field class="w-4/5" cd-flash>
-      <input
-        placeholder="Add one member to the list"
-        matInput
-        type="text"
-        [(ngModel)]="label"
-        (keydown)="handleKey($event)" />
-    </mat-form-field>
+    <app-input (changeLabel)="onChangeLabel($event)"></app-input>
 
     <mat-list class="flex w-full">
       <div *ngIf="names?.length === 0" class="empty-list-label">Empty list</div>
-      <mat-list-item
+      <app-person-list-dumb
         *ngFor="let name of names"
-        cd-flash
-        class="text-orange-500">
-        <div MatListItemLine class="flex justify-between">
-          <h3 title="Name">
-            {{ name }}
-          </h3>
-        </div>
-      </mat-list-item>
+        [name]="name"></app-person-list-dumb>
       <mat-divider *ngIf="names?.length !== 0"></mat-divider>
     </mat-list>
   `,
   host: {
     class: 'w-full flex flex-col items-center',
   },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonListComponent {
   @Input() names: string[] = [];
   @Input() title = '';
 
-  label = '';
-
-  handleKey(event: any) {
-    if (event.keyCode === 13) {
-      this.names?.unshift(this.label);
-      this.label = '';
-    }
+  onChangeLabel(label: string) {
+    this.names = [label, ...this.names];
   }
 }
