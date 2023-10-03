3a43b342d4cbdc2f10bfd75adbb49cd0e2e3e8ae
Hossein Salmanian <Salmanian.Hossein@gmail.com>
2023-10-03 21:03:02 +0330

    feat: use router link and fragment

--- apps/anchor-scrolling/src/app/app.config.ts
+++ apps/anchor-scrolling/src/app/app.config.ts
@@ -1,6 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+      })
+    ),
+  ],
 };
