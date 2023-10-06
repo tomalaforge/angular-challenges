b45bac863e87a9fc5f26e622c380a76230ab7524
gayatri-l-rajput <rajputlalit591@gmail.com>
2023-10-06 23:55:04 +0530

    feat: ui improvement and loader addition

--- apps/crud/src/app/app.component.ts
+++ apps/crud/src/app/app.component.ts
@@ -2,31 +2,91 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { TodoConfig } from './core/Interface/todo';
 import { GetToDoService } from './services/getTodo.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div *ngFor="let todo of todos | async">
-      {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
+    <div class="container">
+      <div class="spinner" *ngIf="isShowSpinner | async; else showData">
+        <mat-spinner></mat-spinner>
+      </div>
+      <ng-template #showData>
+        <div class="show-view" *ngFor="let todo of todos | async">
+          <div class="title-view">
+            {{ todo.title }}
+          </div>
+          <div class="button-view">
+            <button (click)="update(todo)">Update</button>
+            <button class="delete" (click)="delete(todo.id)">Delete</button>
+          </div>
+        </div>
+      </ng-template>
     </div>
   `,
-  styles: [],
+  styles: [
+    `
+      html,
+      body {
+        height: 100vh;
+      }
+      .container {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      }
+      .show-view {
+        border: 1px solid gray;
+      }
+      .title-view {
+        text-align: center;
+        font-size: 20px;
+      }
+      .button-view {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+      }
+      button {
+        width: 80px;
+        height: 30px;
+        background-color: green;
+        color: white;
+        font-size: 20px;
+        margin: 5px 10px;
+      }
+      .spinner {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 50%;
+        width: 100%;
+        top: 50%;
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
   todos!: Observable<TodoConfig[]>;
+  isShowSpinner: Observable<boolean> = of(false);
 
   constructor(private getTodoService: GetToDoService) {}
 
   ngOnInit(): void {
+    this.isShowSpinner = this.getTodoService.isDataFetchRunning;
     this.todos = this.getTodoService.getTodoData();
   }
 
   update(todo: TodoConfig) {
     this.todos = this.getTodoService.updateTodo(todo);
+  }
+  delete(id: number) {
+    this.todos = this.getTodoService.deleteTodo(id);
   }
 }
--- apps/crud/src/app/services/getTodo.service.ts
+++ apps/crud/src/app/services/getTodo.service.ts
@@ -9,19 +9,30 @@ import { BehaviorSubject } from 'rxjs';
 export class GetToDoService {
   private getUrl = 'https://jsonplaceholder.typicode.com/todos';
   private updateUrl = 'https://jsonplaceholder.typicode.com/todos/';
+  private deleteUrl = 'https://jsonplaceholder.typicode.com/todos/';
   private dataObservable = new BehaviorSubject<TodoConfig[]>([]);
+  public isDataFetchRunning = new BehaviorSubject<boolean>(false);
   private headers = {
     'Content-type': 'application/json; charset=UTF-8',
   };
 
   constructor(private http: HttpClient) {}
   getTodoData() {
+    this.turnOnSpinner();
     this.http.get<TodoConfig[]>(this.getUrl).subscribe((data: TodoConfig[]) => {
       this.dataObservable.next(data);
+      this.turnOffSpinner();
     });
     return this.dataObservable;
   }
+  turnOnSpinner() {
+    this.isDataFetchRunning.next(true);
+  }
+  turnOffSpinner() {
+    this.isDataFetchRunning.next(false);
+  }
   updateTodo(todo: TodoConfig) {
+    this.turnOnSpinner();
     this.http
       .put<TodoConfig>(
         `${this.updateUrl}${todo.id}`,
@@ -42,8 +53,20 @@ export class GetToDoService {
           }
           return toDoObj;
         });
+        this.turnOffSpinner();
         this.dataObservable.next(newData);
       });
+    return this.dataObservable;
+  }
+  deleteTodo(id: number) {
+    this.turnOnSpinner();
+    this.http.delete(`${this.deleteUrl}${id}`).subscribe(() => {
+      const newData = this.dataObservable.value.filter((item: TodoConfig) => {
+        return id !== item.id;
+      });
+      this.turnOffSpinner();
+      this.dataObservable.next(newData);
+    });
     return this.dataObservable;
   }
 }
