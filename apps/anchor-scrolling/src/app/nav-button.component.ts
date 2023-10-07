7de7d966dda55174d469347f09274f57033e573c
reedanj <james.r@unitaskr.com>
2023-10-07 13:26:46 +0100

    feat: update anchor nav-button

--- apps/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,17 +1,34 @@
 /* eslint-disable @angular-eslint/component-selector */
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
-      <ng-content></ng-content>
+    <a *ngIf="internalLink" [routerLink]="internalLink ? internalLink : null">
+      <ng-container *ngTemplateOutlet="content"></ng-container>
     </a>
+    <a *ngIf="externalLink" (click)="onClickScroll()">
+      <ng-container *ngTemplateOutlet="content"></ng-container>
+    </a>
+
+    <ng-template #content>
+      <ng-content></ng-content>
+    </ng-template>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterModule, CommonModule],
 })
 export class NavButtonComponent {
-  @Input() href = '';
+  constructor(private viewPortScroller: ViewportScroller) {}
+
+  @Input() internalLink = '';
+  @Input() externalLink = '';
+
+  onClickScroll() {
+    this.viewPortScroller.scrollToAnchor(this.externalLink);
+  }
 }
