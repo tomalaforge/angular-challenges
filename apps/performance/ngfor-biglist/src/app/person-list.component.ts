c7801ecccf000ad9c90f2aa8fff3bdda747c6048
thomas <toma.laforge@gmail.com>
2023-10-07 21:08:58 +0200

    feat(challenge37): solution for big list

--- apps/performance/ngfor-biglist/src/app/person-list.component.ts
+++ apps/performance/ngfor-biglist/src/app/person-list.component.ts
@@ -1,24 +1,24 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { NgForTrackByModule } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule, NgForTrackByModule],
+  imports: [ScrollingModule],
   template: `
-    <div class="h-[300px] relative overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
-        <div
-          *ngFor="let person of persons; trackByProp: 'email'"
-          class="flex justify-between items-center border-b h-9">
-          <h3>{{ person.name }}</h3>
-          <p>{{ person.email }}</p>
-        </div>
+    <cdk-virtual-scroll-viewport
+      appendOnly
+      itemSize="36"
+      class="h-[300px] border-2 border-black">
+      <div
+        *cdkVirtualFor="let person of persons"
+        class="flex justify-between items-center border-b h-9">
+        <h3>{{ person.name }}</h3>
+        <p>{{ person.email }}</p>
       </div>
-    </div>
+    </cdk-virtual-scroll-viewport>
   `,
   host: {
     class: 'w-full flex flex-col',
