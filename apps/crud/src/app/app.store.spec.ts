594cba9795da7fda3179099cbd99b1138ab37fe5
jdegand <70610011+jdegand@users.noreply.github.com>
2023-10-07 16:27:22 -0400

    fix: update app store tests

--- apps/crud/src/app/app.store.spec.ts
+++ apps/crud/src/app/app.store.spec.ts
@@ -6,140 +6,92 @@ import { TodoService } from './service/todo.service';
 import { createMockWithValues } from '@testing-library/angular/jest-utils';
 import { AppComponent } from './app.component';
 import { provideComponentStore } from '@ngrx/component-store';
-
-/*
-describe('AppStore', () => {
-  let appStore: AppStore;
-  let todoServiceSpy: jasmine.SpyObj<TodoService>;
-
-  beforeEach(() => {
-    const todoServiceSpyObj = jasmine.createSpyObj('TodoService', [
-      'getTodos',
-      'updateTodo',
-      'deleteTodo'
-    ]);
-
-    TestBed.configureTestingModule({
-      providers: [
-        AppStore,
-        { provide: TodoService, useValue: todoServiceSpyObj },
-      ],
-    });
-
-    appStore = TestBed.inject(AppStore);
-    todoServiceSpy = TestBed.inject(
-      TodoService
-    ) as jasmine.SpyObj<TodoService>;
-    appStore.setState({ todos: [], callState: 'Loading' });
-  });
-
-  it('should be created', () => {
-    expect(appStore).toBeTruthy();
-  });
-
-  describe('todos$ selector', () => {
-    it('should return todos from state', (done: DoneFn) => {
-      // Given
-      const todos: Todo[] = getFakeTodos();
-      appStore.patchState({ todos });
-
-      // Then
-      appStore.todos$.pipe().subscribe({
-        next: (todos: Todo[]) => {
-          expect(todos.length).toBe(todos.length);
-          done();
-        },
-      });
-    });
-  });
-
-  describe('callState$ selector', () => {
-    it('should return callState from state', (done: DoneFn) => {
-      // Given
-      const callState = "Loading";
-      appStore.patchState({ callState });
-
-      // Then
-      appStore.callState$.pipe().subscribe({
-        next: (callState: string) => {
-          expect(callState).toBe("Loading");
-          done();
-        },
-      });
-    });
-  });
-
-  describe('fetchTodo() method', () => {
-    it('should update state with returned todos', (done: DoneFn) => {
-
-      appStore.patchState({ todos: [] });
-
-      // Given
-      const todos: Todo[] = getFakeTodos();
-      todoServiceSpy.getTodos.and.returnValue(of(todos));
-
-      // Then
-      appStore.state$.pipe(skip(1), take(1)).subscribe({
-        next: (state: AppState) => {
-          expect(state.todos).toEqual(todos);
-          done();
-        },
-      });
-
-      // When
-      appStore.fetchTodo();
-    });
-
-    it('should update the state with an error message if an error occurs', (done: DoneFn) => {
-      // Given
-      const error = 'An error occurred';
-      todoServiceSpy.getTodos.and.returnValue(
-        throwError(() => new Error(error))
-      );
-
-      // Then
-      appStore.state$.pipe(skip(1), take(1)).subscribe({
-        next: (state: AppState) => {
-          expect(state.callState).toEqual(error);
-          done();
-        },
-      });
-
-      // When
-      appStore.fetchTodo();
-    });
-  });
-
-})
-
-export function getFakeTodos(): Todo[] {
-  return [
-    {
-      userId: 1,
-      id: 1,
-      title: 'Test todo 1',
-      completed: false,
-      body: 'Test todo 1'
-    },
-    {
-      userId: 2,
-      id: 2,
-      title: 'Test todo 2',
-      completed: true,
-      body: 'Test todo 2'
-    },
-  ]
-}
-
-*/
+import { Todo } from './interfaces/Todo';
+import { lastValueFrom, take, throwError } from 'rxjs';
 
 describe('AppStore', () => {
   it('truthy', async () => {
     const { store } = await setup();
 
-    store.setState({ todos: [], callState: 'Loading' });
+    store.patchState({ todos: getFakeTodos(), callState: 'LOADED' });
 
     expect(store).toBeTruthy();
+    const callState = await lastValueFrom(store.callState$.pipe(take(1)));
+    expect(callState).toEqual('LOADED');
+  });
+
+  describe('todos$ selector', () => {
+    it('should return todos from state', async () => {
+      const todos: Todo[] = getFakeTodos();
+      const { store } = await setup();
+
+      store.patchState({ todos: getFakeTodos(), callState: 'LOADED' });
+
+      const t = await lastValueFrom(store.todos$.pipe(take(1)));
+
+      expect(t.length).toEqual(todos.length);
+    });
+  });
+
+  // can't do success and failure
+  // setup function has only one mocked returned value
+  // and you can't overwrite it inside a test
+  it('fetchTodo() failure', async () => {
+    const { store } = await setup();
+
+    store.fetchTodo();
+
+    const todos = await lastValueFrom(store.todos$.pipe(take(1)));
+
+    expect(todos.length).toEqual(0);
+
+    const callState = await lastValueFrom(store.callState$.pipe(take(1)));
+
+    expect(callState).toEqual('An error occurred');
+  });
+
+  /*
+  it('updateTodo', async () => {
+    const { store } = await setup();
+
+    store.patchState({todos: getFakeTodos(), callState: 'LOADED'});
+
+    store.updateTodo({
+      userId: 1,
+      id: 1,
+      title: 'Test todo 1',
+      completed: false,
+      body: 'Test todo 1'
+    });
+
+    const todos = await lastValueFrom(store.todos$.pipe(take(1)));
+
+    expect(todos).toEqual([
+      {
+        id: 1,
+        title: 'UPDATED todo 1',
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: 'Test todo 2',
+        completed: true,
+        body: 'Test todo 2'
+      }
+    ]);
+  })
+  */
+
+  it('deleteTodo', async () => {
+    const { store } = await setup();
+
+    store.patchState({ todos: getFakeTodos(), callState: 'LOADED' });
+
+    store.deleteTodoState(2);
+
+    const todos = await lastValueFrom(store.todos$.pipe(take(1)));
+
+    expect(todos.length).toEqual(1);
   });
 });
 
@@ -148,6 +100,23 @@ const setup = async () => {
     getTodos: jest.fn(),
     updateTodo: jest.fn(),
     deleteTodo: jest.fn(),
+  });
+
+  mockTodoService.getTodos.mockReturnValue(
+    throwError(() => new Error('An error occurred'))
+  );
+
+  mockTodoService.updateTodo.mockReturnValue({
+    id: 1,
+    title: 'UPDATED todo 1',
+  });
+
+  mockTodoService.deleteTodo.mockReturnValue({
+    userId: 2,
+    id: 2,
+    title: 'Test todo 2',
+    completed: true,
+    body: 'Test todo 2',
   });
 
   const fixture = await render(AppComponent, {
@@ -159,5 +128,26 @@ const setup = async () => {
 
   const store = TestBed.inject(AppStore);
 
+  store.setState({ todos: [], callState: 'Loading' }); // initial state
+
   return { fixture, store, mockTodoService };
 };
+
+export function getFakeTodos(): Todo[] {
+  return [
+    {
+      userId: 1,
+      id: 1,
+      title: 'Test todo 1',
+      completed: false,
+      body: 'Test todo 1',
+    },
+    {
+      userId: 2,
+      id: 2,
+      title: 'Test todo 2',
+      completed: true,
+      body: 'Test todo 2',
+    },
+  ];
+}
