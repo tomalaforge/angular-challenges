62b8f9cd0e4eacfb01a27a589a0363172236ac09
jdegand <70610011+jdegand@users.noreply.github.com>
2023-10-07 20:08:12 -0400

    fix: todo item component tests

--- apps/crud/src/app/components/todo-item/todo-item.component.spec.ts
+++ apps/crud/src/app/components/todo-item/todo-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { TodoItemComponent } from './todo-item.component';
 import { provideComponentStore } from '@ngrx/component-store';
 import { TodoItemStore } from '../../todoItem.store';
@@ -7,25 +7,80 @@ import { AppStore } from '../../app.store';
 import { TodoService } from '../../service/todo.service';
 import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
 import { createMockWithValues } from '@testing-library/angular/jest-utils';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 describe('TodoItemComponent', () => {
   test('should render', async () => {
-    const mockTodoService = createMockWithValues(TodoService, {
-      getTodos: jest.fn(),
-      updateTodo: jest.fn(),
-      deleteTodo: jest.fn(),
-    });
+    const { fixture } = await setup();
+    expect(fixture).toBeTruthy();
+  });
 
-    const component = await render(TodoItemComponent, {
-      imports: [TodoItemComponent, LoadingSpinnerComponent],
-      providers: [
-        provideComponentStore(TodoItemStore),
-        provideComponentStore(AppStore),
-        { provide: TodoService, useValue: mockTodoService },
-        HttpClient,
-        HttpHandler,
-      ],
-    });
-    expect(component).toBeTruthy();
+  test('update', async () => {
+    const { mockTodoService, fixture } = await setup();
+
+    fixture.detectChanges();
+
+    const button = screen.getAllByRole('button')[0];
+
+    fireEvent.click(button);
+
+    expect(mockTodoService.updateTodo).toHaveBeenCalledTimes(1);
+  });
+
+  test('delete', async () => {
+    const { mockTodoService, fixture } = await setup();
+
+    fixture.detectChanges();
+
+    const button = screen.getAllByRole('button')[1];
+
+    fireEvent.click(button);
+
+    expect(mockTodoService.deleteTodo).toHaveBeenCalledTimes(1);
   });
 });
+
+const setup = async () => {
+  const mockTodoService = createMockWithValues(TodoService, {
+    getTodos: jest.fn(),
+    updateTodo: jest.fn(),
+    deleteTodo: jest.fn(),
+  });
+
+  mockTodoService.getTodos.mockReturnValue(of([]));
+
+  const fixture = await render(TodoItemComponent, {
+    componentInputs: {
+      todo: {
+        userId: 2,
+        id: 2,
+        title: 'Test todo 2',
+        completed: true,
+        body: 'Test todo 2',
+      },
+    },
+    imports: [TodoItemComponent, LoadingSpinnerComponent],
+    providers: [
+      provideComponentStore(TodoItemStore),
+      provideComponentStore(AppStore),
+      { provide: TodoService, useValue: mockTodoService },
+      HttpClient,
+      HttpHandler,
+    ],
+  });
+
+  /*
+  fixture.fixture.componentInstance.todo = {
+    userId: 2,
+    id: 2,
+    title: 'Test todo 2',
+    completed: true,
+    body: 'Test todo 2',
+  };
+  */
+
+  const store = TestBed.inject(TodoItemStore);
+
+  return { mockTodoService, fixture, store };
+};
