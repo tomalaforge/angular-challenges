12b0746e66b02dee70542814b7827ef8cb184d88
gayatri-l-rajput <rajputlalit591@gmail.com>
2023-10-11 00:53:57 +0530

    feat: type casting added

--- apps/pipe-hard/src/app/util.pipe.ts
+++ apps/pipe-hard/src/app/util.pipe.ts
@@ -3,10 +3,19 @@ import { PersonUtils } from './person.utils';
 
 @Pipe({ name: 'utilFunc', standalone: true })
 export class UtilFunction implements PipeTransform {
-  transform(functionName: string, arg1: any, arg2: any, arg3?: any) {
+  transform(
+    functionName: string,
+    arg1: string | number,
+    arg2: boolean | number,
+    arg3?: number
+  ) {
     if (functionName === 'showName') {
-      return PersonUtils.showName(arg1, arg2);
+      return PersonUtils.showName(arg1 as string, arg2 as number);
     }
-    return PersonUtils.isAllowed(arg1, arg2, arg3);
+    return PersonUtils.isAllowed(
+      arg1 as number,
+      arg2 as boolean,
+      arg3 as number
+    );
   }
 }
