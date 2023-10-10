94b6462947120848ed97deb5877b2d02c0922c16
Devesh <deveshachaudhari@gmail.com>
2023-10-10 16:57:38 +0530

    feat: step 1

--- apps/crud/src/app/app.component.ts
+++ apps/crud/src/app/app.component.ts
@@ -1,51 +1,29 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { randText } from '@ngneat/falso';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.model';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   selector: 'app-root',
   template: `
-    <div *ngFor="let todo of todos">
+    <div *ngFor="let todo of todos$ | async">
       {{ todo.title }}
       <button (click)="update(todo)">Update</button>
     </div>
   `,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
-
-  constructor(private http: HttpClient) {}
+  todos$ = this.todoService.todo$;
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
-      .subscribe((todos) => {
-        this.todos = todos;
-      });
+    this.todoService.getAllTodo();
   }
-
-  update(todo: any) {
-    this.http
-      .put<any>(
-        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-        JSON.stringify({
-          todo: todo.id,
-          title: randText(),
-          body: todo.body,
-          userId: todo.userId,
-        }),
-        {
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        }
-      )
-      .subscribe((todoUpdated: any) => {
-        this.todos[todoUpdated.id - 1] = todoUpdated;
-      });
+  update(todo: Todo) {
+    this.todoService.update(todo);
   }
 }
---
+++ apps/crud/src/app/todo.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { randText } from '@ngneat/falso';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Todo } from './todo.model';
+
+@Injectable({ providedIn: 'root' })
+export class TodoService {
+  private http = inject(HttpClient);
+  private todoSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>(
+    []
+  );
+  public todo$: Observable<Todo[]> = this.todoSubject.asObservable();
+
+  getAllTodo() {
+    this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe({
+        next: (value: Todo[]) => {
+          return this.todoSubject.next(value);
+        },
+        error: (error: unknown) => {
+          console.error('Error fetching data:', error);
+        },
+      });
+  }
+
+  update(todo: Todo) {
+    this.http
+      .put<Todo>(
+        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+        JSON.stringify({
+          id: todo.id,
+          title: randText(),
+        }),
+        {
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        }
+      )
+      .subscribe((todoUpdated: Todo) => {
+        let currentTodos = this.todoSubject.value;
+        const updatedTodos = currentTodos.map((t) =>
+          t.id === todoUpdated.id ? { ...t, ...todoUpdated } : t
+        );
+        this.todoSubject.next(updatedTodos);
+      });
+  }
+}
