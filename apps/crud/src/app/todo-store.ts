aa9265c5e4c6c99c0a0bf15090fe3e2e8f97fe83
Prince Darko <princedarko@Princes-MacBook-Pro.local>
2023-10-12 16:39:50 +0000

    feat: added store state to the app

---
+++ apps/crud/src/app/todo-store.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+import { ComponentStore } from '@ngrx/component-store';
+
+interface TodoState {
+  todos: Todo[];
+  loadingAllTodos: boolean;
+  loadingSingleTodo: boolean;
+}
+
+const initialTodoState: TodoState = {
+  todos: [],
+  loadingAllTodos: false,
+  loadingSingleTodo: false,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoStore extends ComponentStore<TodoState> {
+  constructor() {
+    super(initialTodoState);
+  }
+
+  getTodos = this.updater((state, todos: Todo[]) => ({
+    ...state,
+    todos,
+  }));
+
+  updateTodos = this.updater((state, todo: Todo) => ({
+    ...state,
+    todos: state.todos.map((t) => (t.id === todo.id ? todo : t)),
+  }));
+
+  deleteTodos = this.updater((state, todo: Todo) => ({
+    ...state,
+    todos: state.todos.filter((t) => t.id !== todo.id),
+  }));
+
+  setLoadingAllTodos = this.updater((state, value: boolean) => ({
+    ...state,
+    loadingAllTodos: value,
+  }));
+
+  setLoadingSingleTodo = this.updater((state, value: boolean) => ({
+    ...state,
+    loadingSingleTodo: value,
+  }));
+}
