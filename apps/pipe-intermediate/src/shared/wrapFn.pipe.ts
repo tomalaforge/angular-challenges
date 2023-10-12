b1a041fb4b90149e2dae622515bec9649095edc0
gayatri-l-rajput <rajputlalit591@gmail.com>
2023-10-12 23:23:50 +0530

    fix: type matching corrected

--- apps/pipe-intermediate/src/shared/wrapFn.pipe.ts
+++ apps/pipe-intermediate/src/shared/wrapFn.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-type CallbackFunction = (arg1: any, arg2: any) => any;
+type CallbackFunction = (arg1: any, arg2: any) => string;
 @Pipe({ name: 'wrapFn', standalone: true })
 export class WrapFunction implements PipeTransform {
   transform(
     callback: CallbackFunction,
-    arg1: string | number | boolean,
-    arg2: string | number | boolean
+    arg1: string | number,
+    arg2: number | boolean
   ) {
-    return callback(arg1, arg2);
+    return callback(arg1 as string | number, arg2 as number | boolean);
   }
 }
