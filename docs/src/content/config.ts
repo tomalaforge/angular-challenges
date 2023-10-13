304c423c8763f170402514a1dbbb1a900ff63849
thomas <toma.laforge@gmail.com>
2023-10-13 22:58:07 +0200

    docs(comment section): add custom config

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,7 +1,13 @@
-import { defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: (ctx) =>
+      docsSchema()(ctx).extend({
+        // Add a new optional field to the schema.
+        noCommentSection: z.boolean().optional(),
+      }),
+  }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
