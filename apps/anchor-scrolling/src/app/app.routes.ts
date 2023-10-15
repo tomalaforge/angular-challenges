0864494759ed8ee4f450a5e87abb4938fc06ab4c
reedanj <james.r@unitaskr.com>
2023-10-15 10:31:06 +0100

    feat: refactor component with router link

--- apps/anchor-scrolling/src/app/app.routes.ts
+++ apps/anchor-scrolling/src/app/app.routes.ts
@@ -3,7 +3,10 @@ import { FooComponent } from './foo.component';
 import { HomeComponent } from './home.component';
 
 export const appRoutes: Route[] = [
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   { path: 'foo', component: FooComponent },
   { path: '**', redirectTo: 'home' },
 ];
