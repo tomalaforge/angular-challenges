493da6f1d2b1fb1c222cf21fb402011a5f078597
reedanj <james.r@unitaskr.com>
2023-10-16 07:15:50 +0100

    feat: refactor application to standalone

--- apps/module-to-standalone/src/main.ts
+++ apps/module-to-standalone/src/main.ts
@@ -1,6 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { MainShellModule } from '@angular-challenges/module-to-standalone/shell';
+import { importProvidersFrom } from '@angular/core';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(MainShellModule)],
+}).catch((err) => console.log(err));
