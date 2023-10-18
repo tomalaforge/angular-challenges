b945521dbf4481f37f9fe7e778f9f051173c9830
thomas <toma.laforge@gmail.com>
2023-10-18 11:01:17 +0200

    fix(generator): fix generator

--- libs/cli/src/generators/challenge/schema.d.ts
+++ libs/cli/src/generators/challenge/schema.d.ts
@@ -2,9 +2,8 @@ export interface Schema {
   title: string;
   author: string;
   challengeDifficulty: string;
-  docRepository: string;
+  category: string;
   name: string;
-  directory?: string;
   addTest?: boolean;
   skipPackageJson?: boolean;
   rootProject?: boolean;
