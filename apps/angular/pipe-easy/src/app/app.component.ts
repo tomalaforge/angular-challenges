c0f0d384d658fea1fb54aeca60fda9d50ff71e07
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-10-18 22:58:05 +0530

    feat(challenge8): pipe easy

--- apps/angular/pipe-easy/src/app/app.component.ts
+++ apps/angular/pipe-easy/src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyComputationPipe } from '../helpers/heaveyComputation.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, HeavyComputationPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
+      <!-- {{ heavyComputation(person, index) }} -->
+      {{ person | heavyComputation : index }}
     </div>
   `,
 })
 export class AppComponent {
-  persons = ['toto', 'jack'];
+  persons = ['toto', 'jack', 'jill', 'john', 'jane'];
 
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
+  // heavyComputation(name: string, index: number) {
+  //   // very heavy computation
+  //   return `${name} - ${index}`;
+  // }
 }
