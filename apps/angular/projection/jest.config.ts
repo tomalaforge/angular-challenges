5d76fdfa89f3b62f52f33577ae005ee92990aa30
thomas <toma.laforge@gmail.com>
2023-10-18 10:03:54 +0200

    feat(doc): move projection

--- apps/angular/projection/jest.config.ts
+++ apps/angular/projection/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'projection',
-  preset: '../../jest.preset.js',
+  displayName: 'angular-projection',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {},
-  coverageDirectory: '../../coverage/apps/projection',
+  coverageDirectory: '../../../coverage/apps/angular/projection',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
