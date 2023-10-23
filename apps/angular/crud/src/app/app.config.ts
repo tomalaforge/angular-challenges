da448fae652a4718c4a9b713908e8c03e1ddb118
Paul Chesa <paulchesa1@gmail.com>
2023-10-23 10:08:56 +0300

    feat: Added interceptor to handle errors globally

--- apps/angular/crud/src/app/app.config.ts
+++ apps/angular/crud/src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpErrorInterceptor } from './interceptor/http-error-interceptor';
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [importProvidersFrom(HttpClientModule),   {
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpErrorInterceptor,
+    multi: true,
+  },],
 };
---
+++ apps/angular/crud/src/app/interceptor/http-error-interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          console.error('HTTP Error:', error);
+        }
+        window.alert("There was an error processing. Please try again")
+        return of(error);
+      })
+    );
+  }
+}
