878d9e85b92afe770a5bf06bbf0ba69b84a27e25
Paul Chesa <paulchesa1@gmail.com>
2023-10-23 09:21:53 +0300

    feat: Use service  for Todos

--- apps/angular/crud/src/app/app.component.ts
+++ apps/angular/crud/src/app/app.component.ts
@@ -1,51 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { randText } from '@ngneat/falso';
+import { Component, OnInit, inject } from '@angular/core';
 import { ITodo } from './models/todo.model';
+import { TodoService } from './data-access/todo.service';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   selector: 'app-root',
   template: `
+  <ng-container *ngIf="this.todoService.todos$ | async as todos">
     <div *ngFor="let todo of todos">
       {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
+      <button (click)="this.todoService.update(todo)">Update</button>
     </div>
+  </ng-container>
   `,
   styles: [],
 })
 export class AppComponent implements OnInit {
   todos: ITodo[] = [];
-
-  constructor(private http: HttpClient) {}
+  todoService = inject(TodoService);
 
   ngOnInit(): void {
-    this.http
-      .get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
-      .subscribe((todos) => {
-        this.todos = todos;
-      });
+    this.todoService.get()
   }
 
-  update(todo: ITodo) {
-    this.http
-      .put<ITodo>(
-        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-        JSON.stringify({
-          todo: todo.id,
-          title: randText(),
-          userId: todo.userId,
-        }),
-        {
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        }
-      )
-      .subscribe((todoUpdated: ITodo) => {
-        this.todos[todoUpdated.id - 1] = todoUpdated;
-      });
-  }
+
 }
