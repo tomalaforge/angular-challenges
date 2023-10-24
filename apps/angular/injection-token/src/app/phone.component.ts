d3a7e3dbe0a4a563bc3fdcee2e36624d1bcc6ef1
jdegand <70610011+jdegand@users.noreply.github.com>
2023-10-24 15:23:16 -0400

    fix: add selectors for phone & video

--- apps/angular/injection-token/src/app/phone.component.ts
+++ apps/angular/injection-token/src/app/phone.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TimerContainerComponent } from './timer-container.component';
 
 @Component({
+  selector: 'app-phone',
   standalone: true,
   imports: [TimerContainerComponent],
   template: `<div class="flex gap-2">
