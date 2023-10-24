9053187a242caac9b2f8d02f3edf09215014351e
Paul Chesa <paulchesa1@gmail.com>
2023-10-24 05:14:23 +0300

    feat: Moved loading and fetching todos to TodoComponent

--- apps/angular/crud/src/app/app.component.ts
+++ apps/angular/crud/src/app/app.component.ts
@@ -1,44 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Component } from '@angular/core';
 import { TodosComponent } from './components/todos/todos.component';
-import { LoadingService } from './data-access/loading.service';
-import { TodoService } from './data-access/todo.service';
 
 
 @Component({
   standalone: true,
-  imports: [CommonModule, TodosComponent, MatProgressSpinnerModule],
+  imports: [CommonModule, TodosComponent],
 
   selector: 'app-root',
   template: `
-  <ng-container *ngIf="loadingService.isLoading$ | async as isLoading; else loadingTemplate">
-    <div class="spinner-container">
-      <mat-spinner></mat-spinner>
-    </div>
-  </ng-container>
-  <ng-template #loadingTemplate>
-   <app-todos></app-todos>
-  </ng-template>
+    <app-todos></app-todos>
    `,
-  styles: [
-    `.spinner-container {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }`
-
-  ],
 
 })
 export class AppComponent {
-  loadingService = inject(LoadingService)
-  todoService = inject(TodoService);
-
-  ngOnInit(): void {
-    this.todoService.get()
-  }
 
 }
--- apps/angular/crud/src/app/components/todos/todos.component.ts
+++ apps/angular/crud/src/app/components/todos/todos.component.ts
@@ -1,22 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TodoService } from '../../data-access/todo.service';
+import { LoadingService } from '../../data-access/loading.service';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-todos',
   template: `
-  <ng-container *ngIf="this.todoService.todos$ | async as todos">
-    <div class="single-todo" *ngFor="let todo of todos">
-      <span> {{ todo.title }} </span>
-      <span class="actions">
-        <button (click)="this.todoService.update(todo)">Update</button>
-        <button (click)="this.todoService.delete(todo)">Delete</button>
-      </span>
+  <ng-container *ngIf="loadingService.isLoading$ | async as isLoading; else isLoadedTemplate">
+    <div class="spinner-container">
+      <mat-spinner></mat-spinner>
     </div>
   </ng-container>
+  
+  <ng-template #isLoadedTemplate >
+    <ng-container *ngIf="this.todoService.todos$ | async as todos" >
+      <div class="single-todo" *ngFor="let todo of todos">
+        <span> {{ todo.title }} </span>
+        <span class="actions">
+          <button (click)="this.todoService.update(todo)">Update</button>
+          <button (click)="this.todoService.delete(todo)">Delete</button>
+        </span>
+      </div>
+    </ng-container>
+  </ng-template>
   `,
   styles: [
     `
@@ -38,12 +48,26 @@ import { TodoService } from '../../data-access/todo.service';
         margin: 5px;
         padding: 5px 10px;
       }
+
+      .spinner-container {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
     `
   ],
 })
 
-export class TodosComponent  {
+export class TodosComponent implements OnInit {
   todoService = inject(TodoService);
+  loadingService = inject(LoadingService);
+  ngOnInit(): void {
+    this.todoService.get()
+  }
+
+  
 
 
 }
