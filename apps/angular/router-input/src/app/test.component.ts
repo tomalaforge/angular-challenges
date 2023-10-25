591d52f59734b542cb38ae09c94ad3da14abaf47
Mohamed GONTARA <mohamedgontara@MAC843.local>
2023-10-25 15:17:06 +0200

    Resolve router-input challenge

--- apps/angular/router-input/src/app/test.component.ts
+++ apps/angular/router-input/src/app/test.component.ts
@@ -1,21 +1,16 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [AsyncPipe],
   template: `
-    <div>TestId: {{ testId$ | async }}</div>
-    <div>Permission: {{ permission$ | async }}</div>
-    <div>User: {{ user$ | async }}</div>
+    <div>TestId: {{ testId }}</div>
+    <div>Permission: {{ permission }}</div>
+    <div>User: {{ user }}</div>
   `,
 })
 export default class TestComponent {
-  private activatedRoute = inject(ActivatedRoute);
-
-  testId$ = this.activatedRoute.params.pipe(map((p) => p['testId']));
-  permission$ = this.activatedRoute.data.pipe(map((d) => d['permission']));
-  user$ = this.activatedRoute.queryParams.pipe(map((q) => q['user']));
+  @Input() testId!: string;
+  @Input() permission!: string;
+  @Input() user!: string;
 }
