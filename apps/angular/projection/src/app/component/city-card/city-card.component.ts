9be1d8866d804c22b743563ccd69ac404a30e42d
Paul Chesa <paulchesa1@gmail.com>
2023-10-26 14:40:12 +0300

    refactor: fget city observable in a more declarative way

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,38 +1,48 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CardComponent } from '../../ui/card/card.component';
 import { CityStore } from '../../data-access/city.store';
-import { FakeHttpService, randomCity } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
 import { City } from '../../model/city.model';
+import { CommonModule, NgIf } from '@angular/common';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-city-card',
   template: `
-    <app-card [list]="cities"
-            (delete)="delete($event)"
-            customClass="bg-light-blue">
-    <img alt="student"
-         cardImage
-         src="assets/img/city.jpg"
-         width="200px" />
-    <button addNewItemBtn
+    <ng-container *ngIf="fetchCities$ | async as fetchedCities">
+      <ng-container *ngIf="cities$ | async as cities">
+        <app-card
+          [list]="cities"
+          (delete)="delete($event)"
+          customClass="bg-light-blue">
+          <img
+            alt="student"
+            cardImage
+            src="assets/img/city.jpg"
+            width="200px" />
+          <button
+            addNewItemBtn
             class="border border-blue-500 bg-blue-300 p-2 rounded-sm"
             (click)="addNewItem()">
-           Add
-    </button>
-  </app-card>
+            Add
+          </button>
+        </app-card>
+      </ng-container>
+    </ng-container>
   `,
   standalone: true,
-  imports: [CardComponent],
+  imports: [CommonModule, CardComponent, NgIf],
 })
-export class CityCardComponent implements OnInit {
+export class CityCardComponent {
   private store = inject(CityStore);
   private http = inject(FakeHttpService);
-  cities: City[] = [];
-
-  ngOnInit(): void {
-    this.http.fetchCities$.subscribe((t) => this.store.addAll(t));
-    this.store.cities$.subscribe((c) => (this.cities = c));
-  }
+  cities$: Observable<City[]> = this.store.cities$;
+  fetchCities$: Observable<City[]> = this.http.fetchCities$.pipe(
+    tap((t) => this.store.addAll(t))
+  );
 
   addNewItem() {
     this.store.addOne(randomCity());
@@ -41,5 +51,4 @@ export class CityCardComponent implements OnInit {
   delete(id: number) {
     this.store.deleteOne(id);
   }
-
 }
--- apps/angular/projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/projection/src/app/ui/list-item/list-item.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   template: `
     <div class="border border-grey-300 py-1 px-2 flex justify-between">
       <ng-content select="[name]"></ng-content>
-      <ng-content select="[deleteBtn]"></ng-content>
+      <button (click)="delete.emit()">
+        <img alt="delete-icon" class="h-5" src="assets/svg/trash.svg" />
+      </button>
     </div>
   `,
   standalone: true,
 })
-export class ListItemComponent {}
+export class ListItemComponent {
+  @Output() delete: EventEmitter<any> = new EventEmitter();
+}
