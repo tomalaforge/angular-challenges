f40388ae1386f3c750be1b0c49c8783e89eb39f0
jdegand <70610011+jdegand@users.noreply.github.com>
2023-10-26 16:12:29 -0400

    refactor: use inject function

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,5 +1,8 @@
 import { InjectionToken } from '@angular/core';
 
-export const TIMER_TOKEN = new InjectionToken<number>('TIMER');
+export const TIMER_TOKEN = new InjectionToken<number>('TIMER', {
+  factory: () => 1000,
+});
 
-//export const DEFAULT_TIMER = 1000;
+// export const DEFAULT_TIMER = 1000;
+// could use factory: () => DEFAULT_TIMER
--- apps/angular/injection-token/src/app/video.component.ts
+++ apps/angular/injection-token/src/app/video.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TimerContainerComponent } from './timer-container.component';
 import { TIMER_TOKEN } from './data';
 
@@ -6,11 +6,23 @@ import { TIMER_TOKEN } from './data';
   selector: 'app-video',
   standalone: true,
   imports: [TimerContainerComponent],
-  providers: [{ provide: TIMER_TOKEN, useValue: 1000 }],
   template: `<div class="flex gap-2">
       Video Call Timer:
-      <p class="italic">(should be the default 1000s)</p>
+      <p class="italic">(should be the default {{ time }}s)</p>
     </div>
     <timer-container />`,
 })
-export default class VideoComponent {}
+export default class VideoComponent {
+  // if injection token does not have a default value, you could use providers array
+  // providers: [{ provide: TIMER_TOKEN, useValue: 1000 }],
+  // no constructor or inject necessary
+
+  // with default value, you could use inject function or Inject decorator
+  // Inject decorator uses constructor
+  // constructor(@Inject(TIMER_TOKEN) public timer: Number) {}
+
+  // Benefit of using the inject function -> you can interpolate the time in the html
+  // vs it being hard-coded with the other options
+
+  time = inject(TIMER_TOKEN);
+}
