a3ebe3a67c944774ffc8d1cb2260b1554786c13b
Paul Chesa <paulchesa1@gmail.com>
2023-10-26 12:04:24 +0300

    feat: convert href tagto routerlink + use fragment

--- apps/angular/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/angular/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
+  imports: [RouterLink],
   template: `
-    <a [href]="href">
+    <a [routerLink]="href" [fragment]="fragment">
       <ng-content></ng-content>
     </a>
   `,
@@ -14,4 +16,5 @@ import { Component, Input } from '@angular/core';
 })
 export class NavButtonComponent {
   @Input() href = '';
+  @Input() fragment = '';
 }
