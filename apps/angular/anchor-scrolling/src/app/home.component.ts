0519190199c3d0f9080ed03866f5c4328ef37b21
Paul Chesa <paulchesa1@gmail.com>
2023-10-26 12:05:27 +0300

    feat: use fragment to schroll to specific div

--- apps/angular/anchor-scrolling/src/app/home.component.ts
+++ apps/angular/anchor-scrolling/src/app/home.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, ScrollingModule],
   selector: 'app-home',
+  styles: [
+    `
+      #bottom,
+      #top {
+        scroll-behavior: smooth;
+      }
+    `,
+  ],
   template: `
     <nav-button href="/foo" class="fixed top-3 left-1/2">Foo Page</nav-button>
     <div id="top" class="h-screen bg-gray-500">
       Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+      <nav-button href="" fragment="bottom">Scroll Bottom</nav-button>
     </div>
     <div id="bottom" class="h-screen bg-blue-300">
       I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+      <nav-button href="" fragment="top">Scroll Top</nav-button>
     </div>
   `,
 })
