23269144f28adb3121c94b0a1df50a2b6d9bc27b
devanandTech <149224555+devanandTech@users.noreply.github.com>
2023-10-28 16:02:37 +0530

    feat: added ngcontent for btn to add card item
    subject:This is an optional longer description of the changes you made in this commit.

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import { CardType } from '../../model/card.model';
+import { CityStore } from '../../data-access/city.store.';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: ` <app-card
+    [list]="cities"
+    [type]="cardType"
+    customClass="bg-light-green">
+    <img class="header-image" src="assets/img/city.avif" width="200px" />
+  </app-card>`,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  constructor(private http: FakeHttpService, private store: CityStore) {}
 
-  ngOnInit(): void {}
+  cities: City[] = [];
+  cardType = CardType.CITY;
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((cities) => this.store.addAll(cities));
+    this.store.cities$.subscribe((cities) => (this.cities = cities));
+  }
 }
