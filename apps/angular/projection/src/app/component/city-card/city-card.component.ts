12551d0bda855c77359a15304d38ed8cd194aa7b
Brack0 <9846217+Brack0@users.noreply.github.com>
2023-11-10 21:45:25 +0100

    feat(components): add city card

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,58 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { ListItemTemplateDirective } from '../../directive/list-item-template.directive';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="(store.cities$ | async) ?? []"
+      class="bg-light-blue"
+      (addItem)="addItem()">
+      <img src="assets/img/angular-challenge.webp" width="200px" />
+      <ng-template let-item list-item-template>
+        <app-list-item [name]="item.name" (deleteItem)="deleteItem(item.id)">
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
+  styles: [
+    `
+      .bg-light-blue {
+        background-color: rgba(0, 0, 250, 0.1);
+      }
+    `,
+  ],
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent,
+    ListItemComponent,
+    ListItemTemplateDirective,
+    AsyncPipe,
+  ],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private readonly http: FakeHttpService,
+    public readonly store: CityStore
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  addItem() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteItem(id: number) {
+    this.store.deleteOne(id);
+  }
 }
