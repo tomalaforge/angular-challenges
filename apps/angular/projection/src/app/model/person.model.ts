00e36e63599669733f33132b443f0c71dbf15cfe
Allan Juan <allanjuansil@gmail.com>
2023-11-11 11:13:06 -0300

    refactor: add person typing

---
+++ apps/angular/projection/src/app/model/person.model.ts
@@ -0,0 +1,4 @@
+export interface Person {
+  id: number;
+  firstname: string;
+}
--- apps/angular/projection/src/app/ui/card/card.component.ts
+++ apps/angular/projection/src/app/ui/card/card.component.ts
@@ -1,20 +1,20 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { Store } from '../../data-access/store';
 import { CardType } from '../../model/card.model';
+import { Person } from '../../model/person.model';
 import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, ListItemComponent],
+  imports: [NgFor, ListItemComponent],
 })
 export class CardComponent {
   @Input({ required: true }) store!: Store;
-  @Input() list: any[] | null = null;
-  @Input() type!: CardType;
+  @Input() list: Person[] = [];
   @Input() customClass = '';
 
   CardType = CardType;
@@ -23,7 +23,7 @@ export class CardComponent {
     this.store.addRandom();
   }
 
-  delete(item: { id: number }) {
-    this.store.deleteOne(item.id);
+  delete(person: Person) {
+    this.store.deleteOne(person.id);
   }
 }
