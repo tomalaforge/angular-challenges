3d4c6c8c048938241dcc60ed2e9c5333b78207f9
Clarchik <clarchik.services@gmail.com>
2023-11-13 18:07:02 +0100

    feat(Answer 22): router input solution

--- apps/angular/router-input/src/app/home.component.ts
+++ apps/angular/router-input/src/app/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   template: `<div>Home</div>`,
 })
 export default class HomeComponent {}
