28a3134d6b5ce5a646c6df34daba8c9963469d57
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-16 16:23:07 +0530

    Added class with host-context, removed inputs from components

--- apps/angular/styling/src/app/text.component.ts
+++ apps/angular/styling/src/app/text.component.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'text',
   standalone: true,
   template: `
-    <p style="font-size: {{ font }}px; color: {{ color }}">
+    <p>
       <ng-content></ng-content>
     </p>
   `,
+  styles: [
+    `
+      p {
+        font-size: var(--text-font-size, 10px);
+        color: var(--text-color, black);
+      }
+    `,
+  ],
 })
-export class TextComponent {
-  @Input() font = 10;
-  @Input() color = 'black';
-}
+export class TextComponent {}
