5a5027aad5a4f489543a9bc01e255df2b5f437fc
Hector Giraldo <hectorgiraldo0210@gmail.com>
2023-11-16 19:51:29 +0100

    refactor:  city component

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap, tap } from 'rxjs';
+import { CitiesStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import {
+  CardComponent,
+  CardListItemDirective,
+} from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  standalone: true,
-  imports: [],
-})
-export class CityCardComponent implements OnInit {
-  constructor() {}
+  template: `<app-card
+    [list]="cities()"
+    class="bg-light-blue"
+    (add)="addCity()">
+    <img card-header src="assets/img/city.jpg" width="200px" />
 
-  ngOnInit(): void {}
+    <ng-template card-list-item let-city>
+      <app-list-item (deleted)="deleteCity(city.id)">
+        {{ city.name }} - {{ city.country }}
+      </app-list-item>
+    </ng-template>
+  </app-card>`,
+
+  styles: [
+    `
+      .bg-light-blue {
+        background-color: rgba(0, 96, 250, 0.1);
+      }
+    `,
+  ],
+  standalone: true,
+  imports: [CardComponent, ListItemComponent, CardListItemDirective],
+})
+export class CityCardComponent {
+  http = inject(FakeHttpService);
+  store = inject(CitiesStore);
+
+  cities: Signal<City[]> = toSignal(
+    this.http.fetchCities$.pipe(
+      tap((cities: City[]) => this.store.addAll(cities)),
+      switchMap(() => this.store.cities$)
+    ),
+    {
+      initialValue: [] as City[],
+    }
+  );
+
+  addCity() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
