3ad1343166d0c27001d852fa0449101c4d3a06e4
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-17 20:53:44 +0530

    Added TestingLib(unit) and Cypress(component) tests

--- apps/testing/router-outlet/src/app/app.component.cy.ts
+++ apps/testing/router-outlet/src/app/app.component.cy.ts
@@ -1,33 +1,61 @@
+import { appRoutes } from './app.routes';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe(AppComponent.name, () => {
+  const setup = () => {
+    cy.mount(AppComponent, {
+      providers: [provideRouter(appRoutes)],
+    });
+    cy.get('nav a:first').click();
+  };
   describe('Given no search criteria', () => {
-    it('Then shows error message and disabled button', () => {
+    it('Then shows error message and disabled button', async () => {
+      await setup();
       //todo
+      cy.get('input[id=bookName]').clear();
+      cy.contains('Search criteria is required!');
+      cy.get('data-cy="borrow-btn"').should('be.disabled');
     });
   });
 
   describe('Given a search criteria with no book match', () => {
-    it('Then shows No book found', () => {
+    it('Then shows No book found', async () => {
+      await setup();
       //todo
+      cy.get('input[id=bookName]').clear().type('asdasdasd');
+      cy.get('data-cy="borrow-btn"').click();
+      cy.contains('No book found for this search');
     });
   });
 
   describe('Given a search criteria with one book match', () => {
-    it('Then shows One book and no error', () => {
+    it('Then shows One book and no error', async () => {
+      await setup();
       //todo
+      cy.get('input[id=bookName]').clear().type('harper');
+      cy.get('data-cy="borrow-btn"').click();
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
-    it('Then shows One book and no error', () => {
+    it('Then shows One book and no error', async () => {
+      await setup();
       //todo
+      cy.get('input[id=bookName]').clear().type('HARPER');
+      cy.get('data-cy="borrow-btn"').click();
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
-    it('Then shows a list of books', () => {
+    it('Then shows a list of books', async () => {
+      await setup();
       //todo
+      cy.get('input[id=bookName]').clear().type('Tolkien');
+      cy.get('data-cy="borrow-btn"').click();
+      cy.get('li').should('have.length', 2);
     });
   });
 });
--- apps/testing/router-outlet/src/app/app.component.spec.ts
+++ apps/testing/router-outlet/src/app/app.component.spec.ts
@@ -1,31 +1,98 @@
+import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/angular';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 describe('AppComponent', () => {
+  const setup = async () => {
+    await render(AppComponent, {
+      routes: appRoutes,
+    });
+    await userEvent.click(screen.getByRole('link', { name: /borrow a book/i }));
+  };
+
+  const searchInput = async (value: string) => {
+    const input = await screen.getByRole('textbox');
+    const borrowBtn = await screen.getByRole('button', { name: 'Borrow' });
+    await userEvent.type(input, value);
+    await userEvent.click(borrowBtn);
+  };
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', async () => {
       //todo
+      await setup();
+      const input = screen.getByRole('textbox');
+      await userEvent.clear(input);
+      expect(
+        await screen.queryByText('Search criteria is required!')
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.getByRole('button', { name: 'Borrow' })
+      ).toBeDisabled();
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', async () => {
       //todo
+      await setup();
+      await searchInput('asdasd');
+      expect(
+        await screen.queryByText('No book found for this search')
+      ).toBeInTheDocument();
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', async () => {
       //todo
+      await setup();
+      await searchInput('harper');
+      expect(
+        await screen.queryByText(
+          'Borrowed Book: To Kill a Mockingbird by Harper Lee'
+        )
+      ).toBeInTheDocument();
+      expect(
+        await screen.queryByText('Borrowed Book: Animal Farm by George Orwell')
+      ).not.toBeInTheDocument();
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', async () => {
       //todo
+      await setup();
+      await searchInput('HARPER');
+      expect(
+        await screen.queryByText(
+          'Borrowed Book: To Kill a Mockingbird by Harper Lee'
+        )
+      ).toBeInTheDocument();
+      expect(
+        await screen.queryByText('Borrowed Book: Animal Farm by George Orwell')
+      ).not.toBeInTheDocument();
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
     it('Then shows a list of books', async () => {
       //todo
+      await setup();
+      await searchInput('Tolkien');
+      expect(
+        await screen.queryByText('Borrowed Book: The Hobbit by J.R.R. Tolkien')
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.queryByText(
+          'Borrowed Book: The Lord of the Rings by J.R.R. Tolkien'
+        )
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.queryByText('Borrowed Book: Animal Farm by George Orwell')
+      ).not.toBeInTheDocument();
     });
   });
 });
