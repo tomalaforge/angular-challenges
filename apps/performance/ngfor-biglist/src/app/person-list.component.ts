e1e596860ca188486faf03e54088514cf7159a82
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-17 17:20:07 +0530

    Modified for to angular material cdk Virtualfor and viewport

--- apps/performance/ngfor-biglist/src/app/person-list.component.ts
+++ apps/performance/ngfor-biglist/src/app/person-list.component.ts
@@ -1,22 +1,23 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NgForTrackByModule } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule, NgForTrackByModule],
+  imports: [NgForTrackByModule, ScrollingModule],
   template: `
-    <div class="h-[300px] relative overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
-        <div
-          *ngFor="let person of persons; trackByProp: 'email'"
-          class="flex justify-between items-center border-b h-9">
-          <h3>{{ person.name }}</h3>
-          <p>{{ person.email }}</p>
-        </div>
+    <div class="h-[500px] relative overflow-hidden">
+      <div class="absolute inset-0 overflow-hidden">
+        <cdk-virtual-scroll-viewport appendOnly itemSize="50" class="h-full">
+          <div
+            *cdkVirtualFor="let person of persons"
+            class="flex justify-between items-center border-b h-9 z-50">
+            <h3>{{ person.name }}</h3>
+            <p>{{ person.email }}</p>
+          </div>
+        </cdk-virtual-scroll-viewport>
       </div>
     </div>
   `,
