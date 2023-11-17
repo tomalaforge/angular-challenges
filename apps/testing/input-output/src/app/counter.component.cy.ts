c665bed3e075fbab02e909016b7fa1098643c83b
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-17 22:22:32 +0530

    added cypress and testinglibrary test cases

--- apps/testing/input-output/src/app/counter.component.cy.ts
+++ apps/testing/input-output/src/app/counter.component.cy.ts
@@ -1,13 +1,40 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
 
 describe(CounterComponent.name, () => {
   describe('Given an initualValue of 10', () => {
     it('listen to output using createOutputSpy', () => {
-      cy.mount(CounterComponent);
+      cy.mount(CounterComponent, {
+        componentProperties: {
+          initialValue: 10,
+          send: createOutputSpy<boolean>('sendSpy'),
+        },
+      });
+
+      cy.get('[data-testid="counter"]').should('contain', '10');
+      cy.get('button:contains("Increment")').click().click().click();
+      cy.get('[data-testid="counter"]').should('contain', '13');
+      cy.get('button:contains("Decrement")').click().click();
+      cy.get('[data-testid="counter"]').should('contain', '11');
+      cy.get('button:contains("Send")').click();
+      cy.get('@sendSpy').should('have.been.called.with', '11');
     });
 
     it('listen to output  using autoSpyOutputs', () => {
-      cy.mount(CounterComponent);
+      cy.mount(CounterComponent, {
+        autoSpyOutputs: true,
+        componentProperties: {
+          initialValue: 10,
+        },
+      });
+
+      cy.get('[data-testid="counter"]').should('contain', '10');
+      cy.get('button:contains("Increment")').click().click().click();
+      cy.get('[data-testid="counter"]').should('contain', '13');
+      cy.get('button:contains("Decrement")').click().click();
+      cy.get('[data-testid="counter"]').should('contain', '11');
+      cy.get('button:contains("Send")').click();
+      cy.get('@sendSpy').should('have.been.called.with', '11');
     });
   });
 });
--- apps/testing/input-output/src/app/counter.component.spec.ts
+++ apps/testing/input-output/src/app/counter.component.spec.ts
@@ -1,25 +1,62 @@
-import { render } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/angular';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
+  const setup = async () => {
+    const mockSend = jest.fn();
+    await render(CounterComponent, {
+      componentInputs: {
+        initialValue: 10,
+      },
+      componentOutputs: {
+        send: {
+          emit: mockSend,
+        },
+      } as any,
+    });
+
+    return mockSend;
+  };
   describe('Given an initualValue of 10', () => {
     test('Then counterValue is 10', async () => {
-      await render(CounterComponent);
+      await setup();
+      expect(await screen.getByText('Counter: 10')).toBeInTheDocument();
     });
 
     describe('When clicking 5 times on increment button', () => {
       test('Then counterValue is 15', async () => {
-        await render(CounterComponent);
+        await setup();
+        const incrementBtn = await screen.findByRole('button', {
+          name: 'Increment',
+        });
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        expect(await screen.getByText('Counter: 15')).toBeInTheDocument();
       });
     });
 
     describe('When clicking 2 times on decrement button', () => {
+      let mockSend: jest.Mock<any, any, any>;
+      beforeEach(async () => {
+        mockSend = await setup();
+        const decrementBtn = await screen.findByRole('button', {
+          name: 'Decrement',
+        });
+        await userEvent.click(decrementBtn);
+        await userEvent.click(decrementBtn);
+      });
       test('Then counterValue is 8', async () => {
-        await render(CounterComponent);
+        expect(await screen.getByText('Counter: 8')).toBeInTheDocument();
       });
       describe('When clicking on Send ', () => {
         test('Then emitted value is 8', async () => {
-          await render(CounterComponent);
+          await userEvent.click(screen.getByRole('button', { name: 'Send' }));
+          expect(mockSend).toBeCalledWith(8);
+          expect(mockSend).toBeCalledTimes(1);
         });
       });
     });
