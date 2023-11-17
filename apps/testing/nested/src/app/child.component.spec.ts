305cb8cbf5ecac2a5be7448691a20da9593c7b61
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-17 21:36:54 +0530

    added testcases with cypress and testing library

--- apps/testing/nested/src/app/child.component.spec.ts
+++ apps/testing/nested/src/app/child.component.spec.ts
@@ -1,16 +1,50 @@
-import { render } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/angular';
 import { ChildComponent } from './child.component';
+import { createMockWithValues } from '@testing-library/angular/jest-utils';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
+  const setup = async () => {
+    const httpServiceMock = createMockWithValues(HttpService, {
+      sendTitle: jest.fn(),
+    });
+    await render(ChildComponent, {
+      componentProviders: [
+        {
+          provide: HttpService,
+          useValue: httpServiceMock,
+        },
+      ],
+    });
+    return { httpServiceMock };
+  };
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
-      await render(ChildComponent);
+      const { httpServiceMock } = await setup();
+      const validateButton = await  screen.getByRole('button', {
+        name: /validate/i,
+      });
+      const textBox = await screen.getByRole('textbox');
+      await userEvent.clear(textBox);
+      await userEvent.click(validateButton);
+      expect(await screen.queryByText('Title is required !!!')).toBeInTheDocument();
+      expect(httpServiceMock.sendTitle).not.toHaveBeenCalled();
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
-      await render(ChildComponent);
+      const { httpServiceMock } = await setup();
+      const validateButton = await screen.getByRole('button', {
+        name: /validate/i,
+      });
+      const textBox = await screen.getByRole('textbox');
+      await userEvent.clear(textBox);
+      await userEvent.type(textBox, 'Good');
+      expect(await screen.queryByText('Title is Good')).toBeInTheDocument();
+      await userEvent.click(validateButton);
+      expect(httpServiceMock.sendTitle).toHaveBeenCalled();
     });
   });
 });
