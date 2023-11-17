5a54a2cec2929f52093d6a4b1751e39f5fd2f9c7
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-17 19:31:08 +0530

    added test case and tried out the debug funcitons

--- apps/testing/checkbox/src/app/app.component.spec.ts
+++ apps/testing/checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import userEvent from '@testing-library/user-event';
 
 describe('AppComponent', () => {
+  const setup = async () => {
+    const user = userEvent.setup();
+    const result = await render(AppComponent);
+    return { result, user };
+  };
+
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
-      await render(AppComponent);
+      const { user } = await setup();
+      const checkbox = await screen.getByLabelText('Agreed');
+      await user.click(checkbox);
+      const submitBtn = await screen.getByRole('button', { name: 'Submit' });
+      expect(submitBtn).toBeEnabled();
     });
   });
 });
--- apps/testing/checkbox/src/app/app.component.ts
+++ apps/testing/checkbox/src/app/app.component.ts
@@ -1,27 +1,43 @@
 import { Component } from '@angular/core';
-
+import { screen, logRoles } from '@testing-library/angular';
+import {prettyDOM} from '@testing-library/dom'
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: ` <label for="agree">Agreed</label>
-    <input
-      class="ml-2"
-      id="agree"
-      type="checkbox"
-      [value]="check"
-      (input)="toggleCheck()" />
-    <button
-      class="border p-2 rounded-lg ml-10"
-      [class.bg-gray-500]="!check"
-      [class.text-white]="!check"
-      [disabled]="!check">
-      Submit
-    </button>`,
+  template: `
+    <div id="policy">
+      <label for="agree">Agreed</label>
+      <input
+        class="ml-2"
+        id="agree"
+        type="checkbox"
+        [value]="check"
+        (input)="toggleCheck()" />
+      <button
+        class="border p-2 rounded-lg ml-10"
+        [class.bg-gray-500]="!check"
+        [class.text-white]="!check"
+        [disabled]="!check">
+        Submit
+      </button>
+    </div>
+  `,
 })
 export class AppComponent {
   check = false;
 
-  toggleCheck() {
+  async toggleCheck() {
     this.check = !this.check;
+
+    // for testing purposes (working in console)
+
+    const checkbox = await screen.getByLabelText('Agreed');
+    const submitBtn = await screen.getByRole('button', { name: 'Submit' });
+    const container = document.querySelector('#policy') ?? undefined;
+    screen.debug(checkbox);
+    screen.debug();
+    logRoles(submitBtn);
+    screen.logTestingPlaygroundURL(container);
+    console.log(prettyDOM(div))
   }
 }
