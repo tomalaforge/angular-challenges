c2d925195fab557e8a2f7acfb2b5cb0b7ad07fd5
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-18 13:49:22 +0530

    Added cypress and testing library test cases for modal

--- apps/testing/modal/src/app/app.component.cy.ts
+++ apps/testing/modal/src/app/app.component.cy.ts
@@ -1,19 +1,35 @@
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe(AppComponent.name, () => {
-  const setup = () => {
-    cy.mount(AppComponent);
-  };
-
-  test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
-    setup();
+  beforeEach(() => {
+    cy.mount(AppComponent, {
+      imports: [BrowserAnimationsModule],
+    });
   });
 
-  test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', () => {
-    setup();
+  test('After confirm button is clicked with no input a error modal should pop up', () => {
+    cy.contains('button', 'Confirm').click();
+    cy.get('mat-dialog-container').contains('h1', 'Error');
+    cy.contains('button', 'OK').click();
   });
 
-  test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', () => {
-    setup();
+  describe('After entering input and click confirm button a profile modal should pop up', () => {
+    beforeEach(() => {
+      cy.get('input').type('Testing');
+      cy.contains('button', 'Confirm').click();
+      cy.get('mat-dialog-container').contains('h1', 'Profil');
+      cy.get('mat-dialog-container').contains('Name: Testing');
+    });
+
+    test('After clicking cancel there should be error message displayed in HTML', () => {
+      cy.contains('button', 'Cancel').click();
+      cy.contains('Name is invalid !!');
+    });
+
+    test('After clicking confirmation there should be success message displayed in HTML', () => {
+      cy.contains('button', 'Confirmation').click();
+      cy.contains('Name has been submitted');
+    });
   });
 });
--- apps/testing/modal/src/app/app.component.spec.ts
+++ apps/testing/modal/src/app/app.component.spec.ts
@@ -1,16 +1,56 @@
-import { render } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+  test('After confirm button is clicked with no input a error modal should pop up', async () => {
     await render(AppComponent);
+    const confirmBtn = await screen.getByRole('button', { name: /confirm/i });
+    await userEvent.click(confirmBtn);
+    await screen.findByRole('dialog');
+    expect(
+      await screen.getByRole('heading', {
+        name: /error/i,
+        level: 1,
+      })
+    ).toBeInTheDocument();
+
+    const okBtn = await screen.getByRole('button', { name: /ok/i });
+    await userEvent.click(okBtn);
   });
 
-  test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
-  });
+  describe('After entering input and click confirm button a profile modal should pop up', () => {
+    beforeEach(async () => {
+      await render(AppComponent);
+      const input = await screen.getByRole('textbox');
+      await userEvent.type(input, 'Testing');
+      const confirmBtn = await screen.getByRole('button', { name: /confirm/i });
+      await userEvent.click(confirmBtn);
+      await screen.findByRole('dialog');
+      expect(
+        await screen.getByRole('heading', {
+          name: /profil/i,
+          level: 1,
+        })
+      ).toBeInTheDocument();
+    });
 
-  test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    test('After clicking cancel there should be error message displayed in HTML', async () => {
+      const cancelBtn = await screen.getByRole('button', { name: /cancel/i });
+      await userEvent.click(cancelBtn);
+      expect(
+        await screen.queryByText('Name is invalid !!')
+      ).toBeInTheDocument();
+    });
+
+    test('After clicking confirmation there should be success message displayed in HTML', async () => {
+      const confirmationBtn = await screen.getByRole('button', {
+        name: /confirmation/i,
+      });
+      await userEvent.click(confirmationBtn);
+      expect(
+        await screen.queryByText('Name has been submitted')
+      ).toBeInTheDocument();
+    });
   });
 });
