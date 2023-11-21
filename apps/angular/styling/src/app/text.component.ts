2aef13d9f708537b7e4d0dfebdd4fce3ff9c26b2
Davi Batista <DaviBatista.DECSKILL@edp.com>
2023-11-21 12:43:00 -0300

    feat: done highly customizable CSS

--- apps/angular/styling/src/app/text.component.ts
+++ apps/angular/styling/src/app/text.component.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'text',
   standalone: true,
+  styles: [
+    `
+      p {
+        font-size: var(--text-font-size, 10px);
+        color: var(--text-color, black);
+      }
+    `,
+  ],
   template: `
-    <p style="font-size: {{ font }}px; color: {{ color }}">
+    <p>
       <ng-content></ng-content>
     </p>
   `,
 })
-export class TextComponent {
-  @Input() font = 10;
-  @Input() color = 'black';
-}
+export class TextComponent {}
