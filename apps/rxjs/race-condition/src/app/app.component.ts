4980172ebce4a1f26990c8be06283b41d7f9258e
Egor Volvachev <axe.onegaimy@gmail.com>
2023-11-22 09:18:10 +0300

    feat(Answer:14): solution race condition

--- apps/rxjs/race-condition/src/app/app.component.ts
+++ apps/rxjs/race-condition/src/app/app.component.ts
@@ -1,37 +1,53 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
+  ElementRef,
   inject,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { take } from 'rxjs';
+import { AsyncSubject, fromEvent, switchMap, take } from 'rxjs';
 import { TopicModalComponent } from './topic-dialog.component';
 import { TopicService, TopicType } from './topic.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: ` <button (click)="openTopicModal()">Open Topic</button> `,
+  template: ` <button #btn>Open Topic</button> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'rxjs-race-condition';
   dialog = inject(MatDialog);
   topicService = inject(TopicService);
-  topics: TopicType[] = [];
+  destroyRef = inject(DestroyRef);
+  topics$ = new AsyncSubject<TopicType[]>();
 
-  ngOnInit(): void {
-    this.topicService
-      .fakeGetHttpTopic()
-      .pipe(take(1))
-      .subscribe((topics) => (this.topics = topics));
+  @ViewChild('btn', { static: true }) public btnRef!: ElementRef;
+
+  public ngOnInit(): void {
+    this.topicService.fakeGetHttpTopic().pipe(take(1)).subscribe(this.topics$);
   }
 
-  openTopicModal() {
+  public ngAfterViewInit(): void {
+    fromEvent(this.btnRef.nativeElement, 'click')
+      .pipe(
+        switchMap(() => this.topics$),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((topics: TopicType[]) => {
+        this.openTopicModal(topics);
+      });
+  }
+
+  public openTopicModal(topics: TopicType[]) {
     this.dialog.open(TopicModalComponent, {
       data: {
-        topics: this.topics,
+        topics,
       },
     });
   }
