0160ebed47d2b07adad5d4ca7b3f7be5a1704838
Davi Batista <DaviBatista.DECSKILL@edp.com>
2023-11-24 08:56:21 -0300

    feat: challenge 39 implement injectionToken

--- apps/angular/injection-token/src/app/phone.component.ts
+++ apps/angular/injection-token/src/app/phone.component.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_TIMER } from './data';
 import { Component } from '@angular/core';
 import { TimerContainerComponent } from './timer-container.component';
 
@@ -5,6 +6,12 @@ import { TimerContainerComponent } from './timer-container.component';
   selector: 'app-phone',
   standalone: true,
   imports: [TimerContainerComponent],
+  providers: [
+    {
+      provide: DEFAULT_TIMER,
+      useValue: 2000,
+    },
+  ],
   template: `<div class="flex gap-2">
       Phone Call Timer:
       <p class="italic">(should be 2000s)</p>
