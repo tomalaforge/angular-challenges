ebf5bfd268f4f18f361f2a141115d1db29f99e89
Egor Volvachev <axe.onegaimy@gmail.com>
2023-11-25 13:02:39 +0300

    feat(Answer:10): solution `Utility Wrapper Pipe`

--- apps/angular/pipe-hard/src/app/person.utils.ts
+++ apps/angular/pipe-hard/src/app/person.utils.ts
@@ -1,3 +1,5 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
 const showName = (name: string, index: number) => {
   // very heavy computation
   return `${name} - ${index}`;
@@ -15,3 +17,29 @@ export const PersonUtils = {
   showName,
   isAllowed,
 };
+
+export type PersonUtilFunctionName = keyof typeof PersonUtils;
+
+export type MappedType<T extends PersonUtilFunctionName> =
+  (typeof PersonUtils)[T];
+
+export type Params<T extends PersonUtilFunctionName> = Parameters<
+  MappedType<T>
+>;
+
+export type FirstArg<T> = T extends [infer First, ...unknown[]] ? First : never;
+export type LastArgs<T> = T extends [unknown, ...infer Rest] ? Rest : never;
+
+@Pipe({
+  name: 'person',
+  standalone: true,
+})
+export class PersonUtilsPipe implements PipeTransform {
+  public transform<T extends PersonUtilFunctionName>(
+    value: FirstArg<Params<T>>,
+    functionName: T,
+    ...args: LastArgs<Params<T>>
+  ): ReturnType<MappedType<T>> {
+    return (PersonUtils[functionName] as Function)(value, ...args);
+  }
+}
