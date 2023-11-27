4d74e2bad23fedc4c2fe67ad36c8e8a1ba00f7d0
ronald.pacheco <ronald.pacheco@es.dachser.com>
2023-11-27 16:24:25 +0100

    refactor(any-type): create todo.interface and remove any type in appComponent

--- apps/angular/crud/src/app/app.component.ts
+++ apps/angular/crud/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { randText } from '@ngneat/falso';
+import { Todo } from './model/todo.interface';
 
 @Component({
   standalone: true,
@@ -16,35 +17,35 @@ import { randText } from '@ngneat/falso';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
+  todos!: Todo[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
       .subscribe((todos) => {
         this.todos = todos;
       });
   }
 
-  update(todo: any) {
+  update(todo: Todo) {
     this.http
-      .put<any>(
+      .put<Todo>(
         `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
         JSON.stringify({
           todo: todo.id,
           title: randText(),
-          body: todo.body,
+          body: '',
           userId: todo.userId,
         }),
         {
           headers: {
             'Content-type': 'application/json; charset=UTF-8',
           },
-        }
+        },
       )
-      .subscribe((todoUpdated: any) => {
+      .subscribe((todoUpdated: Todo) => {
         this.todos[todoUpdated.id - 1] = todoUpdated;
       });
   }
