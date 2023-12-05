0e5d943d98728c353dd63294be98b8348d165340
arditdomi <ardit.domi@turntown.digital>
2023-12-05 16:37:54 +0000

    feat: answer:1

--- apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,35 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
-  template: `<app-card
-    [list]="teachers"
-    [type]="cardType"
-    customClass="bg-light-red"></app-card>`,
+  template: `
+    <app-card
+      [list]="teachers"
+      [imgUrl]="'assets/img/teacher.png'"
+      (newItem)="onNewItem()">
+      <ng-template #rowTemplate let-teacher>
+        <app-list-item
+          [name]="teacher.firstname + ' ' + teacher.lastname"
+          [id]="teacher.id"
+          (itemDelete)="onItemDelete($event)" />
+      </ng-template>
+    </app-card>
+  `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
+      :host {
+        --card-background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class TeacherCardComponent implements OnInit {
   teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
 
-  constructor(private http: FakeHttpService, private store: TeacherStore) {}
+  constructor(
+    private http: FakeHttpService,
+    private store: TeacherStore,
+  ) {}
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
 
     this.store.teachers$.subscribe((t) => (this.teachers = t));
+  }
+
+  onItemDelete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  onNewItem() {
+    this.store.addOne(randTeacher());
   }
 }
--- apps/angular/projection/src/app/ui/card/card.component.ts
+++ apps/angular/projection/src/app/ui/card/card.component.ts
@@ -1,34 +1,35 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { ListItemComponent } from '../list-item/list-item.component';
+import { NgFor, NgTemplateOutlet } from '@angular/common';
+import {
+  Component,
+  ContentChild,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, ListItemComponent],
+  imports: [NgFor, NgTemplateOutlet],
+  styles: [
+    `
+      .card-container {
+        background-color: var(--card-background-color, lightblue);
+      }
+    `,
+  ],
 })
 export class CardComponent {
-  @Input() list: any[] | null = null;
-  @Input() type!: CardType;
-  @Input() customClass = '';
+  @Input() list: any[] = [];
+  @Input() imgUrl: string = '';
+  @Output() newItem: EventEmitter<void> = new EventEmitter<void>();
 
-  CardType = CardType;
+  @ContentChild('rowTemplate', { read: TemplateRef })
+  rowTemplate!: TemplateRef<{ $implicit: any }>;
 
-  constructor(
-    private teacherStore: TeacherStore,
-    private studentStore: StudentStore
-  ) {}
-
-  addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
+  onAddClick() {
+    this.newItem.emit();
   }
 }
