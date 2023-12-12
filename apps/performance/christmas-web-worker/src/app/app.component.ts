92d2a14b7137bd048710c1737adadfc9479cbf7b
jdegand <70610011+jdegand@users.noreply.github.com>
2023-12-12 15:21:29 -0500

    refactor: add web worker

--- apps/performance/christmas-web-worker/src/app/app.component.ts
+++ apps/performance/christmas-web-worker/src/app/app.component.ts
@@ -3,19 +3,20 @@ import { Component, inject } from '@angular/core';
 import { HeavyCalculationService } from './heavy-calculation.service';
 import { UnknownPersonComponent } from './unknown-person/unknown-person.component';
 
+// add OnPush ?
 @Component({
   standalone: true,
   imports: [CommonModule, UnknownPersonComponent],
   providers: [HeavyCalculationService],
   selector: 'app-root',
   template: `
-    <unknown-person [step]="loadingPercentage()" class="relative grow" />
+    <unknown-person [step]="loadingPercentage" class="relative grow" />
     <button
       class="my-3 w-fit self-center rounded-md border border-white px-4 py-2 text-2xl text-white"
       (click)="discover()">
       Discover
     </button>
-    <div class="p-1 text-white">Progress: {{ loadingPercentage() }}%</div>
+    <div class="p-1 text-white">Progress: {{ loadingPercentage }}%</div>
   `,
   host: {
     class: `flex flex-col h-screen w-screen bg-[#1f75c0]`,
@@ -24,9 +25,17 @@ import { UnknownPersonComponent } from './unknown-person/unknown-person.componen
 export class AppComponent {
   private heavyCalculationService = inject(HeavyCalculationService);
 
-  readonly loadingPercentage = this.heavyCalculationService.loadingPercentage;
+  loadingPercentage = 0;
 
   discover() {
-    this.heavyCalculationService.startLoading();
+    if (typeof Worker !== 'undefined') {
+      const worker = new Worker(new URL('./app.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        this.loadingPercentage = data;
+      };
+      worker.postMessage('start'); // what type here doesn't matter -> like a key
+    } else {
+      this.heavyCalculationService.startLoading();
+    }
   }
 }
---
+++ apps/performance/christmas-web-worker/src/app/app.worker.ts
@@ -0,0 +1,34 @@
+/// <reference lib="webworker" />
+
+import { computed, signal } from '@angular/core';
+
+addEventListener('message', () => {
+  const finalLength = 664579;
+  let loadingLength = signal(0);
+  let loadingPercentage = computed(() => (loadingLength() * 100) / finalLength);
+
+  for (let num = 2; num <= 10000000; num++) {
+    let randomFlag = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        randomFlag = false;
+        break;
+      }
+    }
+    if (randomFlag) {
+      loadingLength.update((l) => l + 1);
+      postMessage(loadingPercentage());
+    }
+  }
+});
+
+/*
+- Struggled when I tried to recreate heavy-calculation.service into a class 
+- Then you have add static to all methods 
+- I was able to recreate the same functionality where progress was 0 and then 100
+- only the main thread wasn't blocked
+
+- approach influenced by article below 
+- https://octoperf.com/blog/2023/02/10/angular-performance-web-worker/#extracting-the-prime-number-logic
+
+*/
