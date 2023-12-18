b659d0333520ae49f775f4f6ca4be0c2d3c4f88a
Pascal Küsgen <pascalkuesgen@gmail.com>
2023-12-18 14:56:12 +0100

    feat(form): solution to control-value-accessor challenge

--- apps/forms/control-value-accessor/src/app/rating-control/rating-control.component.ts
+++ apps/forms/control-value-accessor/src/app/rating-control/rating-control.component.ts
@@ -1,25 +1,73 @@
-import { EventEmitter } from '@angular/core';
-import { Output } from '@angular/core';
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-rating-control',
   templateUrl: 'rating-control.component.html',
   styleUrls: ['rating-control.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: RatingControlComponent,
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: RatingControlComponent,
+    },
+  ],
 })
-export class RatingControlComponent {
-  @Output()
-  readonly ratingUpdated: EventEmitter<string> = new EventEmitter<string>();
+export class RatingControlComponent implements ControlValueAccessor, Validator {
+  rating: number | null = null;
 
-  value: number | null = null;
+  disabled = false;
 
-  setRating(index: number): void {
-    this.value = index + 1;
-    this.ratingUpdated.emit(`${this.value}`);
+  writeValue(rating: number): void {
+    this.rating = rating;
+    this.onChange(rating);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onChange = (rating: number) => {};
+  registerOnChange(onChange: never) {
+    this.onChange = onChange;
+  }
+
+  onTouched = () => {};
+  registerOnTouched(onTouched: never) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
+
+  setRating(rating: number): void {
+    this.writeValue(rating);
   }
 
   isStarActive(index: number, value: number | null): boolean {
     return value ? index < value : false;
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    const rating = control.value;
+
+    if (!rating) {
+      return {
+        required: true,
+      };
+    }
+
+    return null;
   }
 }
