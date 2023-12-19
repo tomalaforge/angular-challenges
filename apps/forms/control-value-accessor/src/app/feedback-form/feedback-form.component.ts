bd8d694d04a498c393be7c593c0c7eb8dc25486d
HenryToti <henryokoth3@gmail.com>
2023-12-19 09:02:00 +0300

    feat!: solution for control accessor value 1. use controlaccessorvalue to implement to create a custom form field

--- apps/forms/control-value-accessor/src/app/feedback-form/feedback-form.component.ts
+++ apps/forms/control-value-accessor/src/app/feedback-form/feedback-form.component.ts
@@ -1,10 +1,10 @@
-import { EventEmitter } from '@angular/core';
-import { Output } from '@angular/core';
-import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { FormControl } from '@angular/forms';
-import { FormGroup } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RatingControlComponent } from '../rating-control/rating-control.component';
 
 @Component({
@@ -26,17 +26,16 @@ export class FeedbackFormComponent {
     email: new FormControl('', {
       validators: Validators.required,
     }),
+    rating: new FormControl('', {
+      validators: Validators.required,
+    }),
     comment: new FormControl(),
   });
-
-  rating: string | null = null;
 
   submitForm(): void {
     this.feedBackSubmit.emit({
       ...this.feedbackForm.value,
-      rating: this.rating,
     });
-
     this.feedbackForm.reset();
   }
 }
--- apps/forms/control-value-accessor/src/app/rating-control/rating-control.component.ts
+++ apps/forms/control-value-accessor/src/app/rating-control/rating-control.component.ts
@@ -1,25 +1,52 @@
-import { EventEmitter } from '@angular/core';
-import { Output } from '@angular/core';
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-rating-control',
   templateUrl: 'rating-control.component.html',
   styleUrls: ['rating-control.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: RatingControlComponent,
+      multi: true,
+    },
+  ],
 })
-export class RatingControlComponent {
-  @Output()
-  readonly ratingUpdated: EventEmitter<string> = new EventEmitter<string>();
-
+export class RatingControlComponent implements ControlValueAccessor, Validator {
   value: number | null = null;
+  onChange?: (value: number) => void;
+  onTouched?: () => void;
+  disabled!: boolean;
+  writeValue(value: number): void {
+    this.value = value;
+    this.onChange?.(this.value);
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
   setRating(index: number): void {
-    this.value = index + 1;
-    this.ratingUpdated.emit(`${this.value}`);
+    this.writeValue(index + 1);
   }
 
   isStarActive(index: number, value: number | null): boolean {
     return value ? index < value : false;
+  }
+  validate(control: AbstractControl): ValidationErrors | null {
+    return control.value ? { required: true } : null;
   }
 }
