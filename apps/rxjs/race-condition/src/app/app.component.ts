9b555852d5bab3eb4f8ab82c3aa8130e9f26eac4
Nuttakit <nutphi@outlook.com>
2023-12-28 18:51:47 -0500

    feat: submit answer 14

--- apps/rxjs/race-condition/src/app/app.component.ts
+++ apps/rxjs/race-condition/src/app/app.component.ts
@@ -1,38 +1,22 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { take } from 'rxjs';
 import { TopicModalComponent } from './topic-dialog.component';
-import { TopicService, TopicType } from './topic.service';
+import { TopicService } from './topic.service';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: ` <button (click)="openTopicModal()">Open Topic</button> `,
+  template: `
+    <button (click)="openTopicModal()">Open Topic</button>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'rxjs-race-condition';
   dialog = inject(MatDialog);
   topicService = inject(TopicService);
-  topics: TopicType[] = [];
-
-  ngOnInit(): void {
-    this.topicService
-      .fakeGetHttpTopic()
-      .pipe(take(1))
-      .subscribe((topics) => (this.topics = topics));
-  }
 
   openTopicModal() {
-    this.dialog.open(TopicModalComponent, {
-      data: {
-        topics: this.topics,
-      },
-    });
+    this.dialog.open(TopicModalComponent);
   }
 }
--- apps/rxjs/race-condition/src/app/topic-dialog.component.ts
+++ apps/rxjs/race-condition/src/app/topic-dialog.component.ts
@@ -1,24 +1,33 @@
-import { NgFor } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { TopicService, TopicType } from './topic.service';
 
 @Component({
-  template: ` <h1 mat-dialog-title>Show all Topics</h1>
+  template: `
+    <h1 mat-dialog-title>Show all Topics</h1>
     <div mat-dialog-content>
-      <ul>
-        <li *ngFor="let topic of data.topics">
-          {{ topic }}
-        </li>
-      </ul>
+      @if (data$ | async; as data) {
+        <ul>
+          @for (topic of data; track topic) {
+            <li>
+              {{ topic }}
+            </li>
+          }
+        </ul>
+      }
     </div>
     <div mat-dialog-actions>
       <button mat-button mat-dialog-close>Close</button>
-    </div>`,
-  imports: [MatDialogModule, MatButtonModule, NgFor],
+    </div>
+  `,
+  imports: [MatDialogModule, MatButtonModule, AsyncPipe],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TopicModalComponent {
-  data = inject(MAT_DIALOG_DATA);
+  topicService = inject(TopicService);
+  data$: Observable<TopicType[]> = this.topicService.fakeGetHttpTopic();
 }
