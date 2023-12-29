e665df6e4630e2af3e84f22aa1ef8f0353deb48a
Nuttakit <nutphi@outlook.com>
2023-12-28 21:56:11 -0500

    feat: submit answer 3

--- apps/angular/ngfor-enhancement/src/app/app.component.ts
+++ apps/angular/ngfor-enhancement/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 interface Person {
@@ -7,15 +6,13 @@ interface Person {
 
 @Component({
   standalone: true,
-  imports: [NgFor, NgIf],
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
-    <ng-template #emptyList>The list is empty !!</ng-template>
+    @for (person of persons; track person.name) {
+      <div>{{ person.name }}</div>
+    } @empty {
+      The list is empty !!
+    }
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
