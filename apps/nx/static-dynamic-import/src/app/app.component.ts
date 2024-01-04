dbc6f450941627a2f0a8eb5cc92eefaefbf8ec96
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-03 21:07:48 -0500

    docs: add useful resources

--- apps/nx/static-dynamic-import/src/app/app.component.ts
+++ apps/nx/static-dynamic-import/src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User, UserPipe } from 'user';
 
+/* 
+// Useful Resources
+
+- https://github.com/nrwl/nx/issues/4463
+- https://www.youtube.com/watch?v=m1NTKbHwNJk&t=476s
+- https://stackoverflow.com/questions/48595065/how-to-lazy-load-a-module-from-nrwl-workspace-libs-folder
+- https://github.com/nrwl/nx/issues/18552 
+*/
+
 @Component({
   standalone: true,
   imports: [UserPipe, RouterOutlet],
