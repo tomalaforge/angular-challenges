8b148c120be6fbc6d64bfd45157f600bbb889473
Artur Rozwalak <artur.rozwalak@landbay.co.uk>
2024-01-05 11:15:01 +0000

    feat: anchor scrolling

--- apps/angular/anchor-scrolling/src/app/app.config.ts
+++ apps/angular/anchor-scrolling/src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({ anchorScrolling: 'enabled' }),
+    ),
+  ],
 };
