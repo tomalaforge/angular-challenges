510014b6df3a2f88bde4866c588c9da7ecd72c57
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-05 21:11:03 -0500

    refactor: view model added

--- apps/ngrx/effect-selector/src/app/app.component.ts
+++ apps/ngrx/effect-selector/src/app/app.component.ts
@@ -1,3 +1,88 @@
+import { AsyncPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { Store, createSelector } from '@ngrx/store';
+import { AppActions } from './app.actions';
+import { selectActivities } from './store/activity/activity.reducer';
+import { StatusSelectors } from './store/status/status.selectors';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [AsyncPipe],
+  template: `
+    <h1>Activity Board</h1>
+    @if (vm$ | async; as vm) {
+      <section>
+        @for (activity of vm.activities; track activity.id) {
+          <div class="card">
+            <h2>Activity Name: {{ activity.name }}</h2>
+            <p>Main teacher: {{ activity.teacher.name }}</p>
+            <span>All teachers available for : {{ activity.type }} are</span>
+            <ul>
+              @for (teacher of vm.teachers; track teacher.name) {
+                <li>{{ teacher.name }}</li>
+              }
+            </ul>
+          </div>
+        }
+      </section>
+    }
+  `,
+  styles: [
+    `
+      section {
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        gap: 2px;
+      }
+
+      .card {
+        display: flex;
+        flex-direction: column;
+        border: solid;
+        border-width: 1px;
+        border-color: black;
+        padding: 2px;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent implements OnInit {
+  private store = inject(Store);
+
+  activities$ = this.store.select(selectActivities);
+  getAllTeachersForActivityType$ = this.store.select(
+    StatusSelectors.selectStatuses,
+  );
+
+  // this could be in another file like app.selector.ts
+  #vm = createSelector(
+    selectActivities,
+    StatusSelectors.selectStatuses,
+    (activities, teachers) => ({ activities, teachers }),
+  );
+
+  vm$ = this.store.select(this.#vm);
+
+  // view model helps with testing and mocking
+
+  // Now with one dispatch here -> Are the other load actions useless?
+  // I guess you could use those actions for local loading
+  ngOnInit(): void {
+    this.store.dispatch(AppActions.initApp());
+  }
+}
+
+/*
+
+// without view model implementation
+
 import { AsyncPipe, NgFor } from '@angular/common';
 import {
   ChangeDetectionStrategy,
@@ -57,13 +142,10 @@ export class AppComponent implements OnInit {
     StatusSelectors.selectStatuses,
   );
 
-  // use a view model ?
-  // works without it
-  // view model helps with testing and mocking
-
   // Now with one dispatch here -> Are the other load actions useless?
   // I guess you could use those actions for local loading
   ngOnInit(): void {
     this.store.dispatch(AppActions.initApp());
   }
 }
+*/
