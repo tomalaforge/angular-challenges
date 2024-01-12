fb9b1a30185f9a1aca6e2c9d84df503e0066e9fa
Nuttakit <nutphi@outlook.com>
2024-01-12 14:06:56 -0500

    fix(answer-30): modify State type

--- apps/angular/interop-rxjs-signal/src/app/list/photos.model.ts
+++ apps/angular/interop-rxjs-signal/src/app/list/photos.model.ts
@@ -4,15 +4,10 @@ export const STATE_KEYS = ['photos', 'total', 'page', 'term'] as const;
 export type StateKey = (typeof STATE_KEYS)[number];
 
 export type State = {
-  [key in StateKey]: key extends 'photos'
-    ? Photo[]
-    : key extends 'total'
-      ? number
-      : key extends 'page'
-        ? number
-        : key extends 'term'
-          ? string
-          : never;
+  photos: Photo[];
+  total: number;
+  page: number;
+  term: string;
 };
 
 export type APIState = 'init' | 'loading' | 'loaded' | { error: string };
