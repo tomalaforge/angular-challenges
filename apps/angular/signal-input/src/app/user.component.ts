dfc924a499b33aa3dfc9781ae66b87d5b183e0ae
GRDtechlab <grdtechlab@gmail.com>
2024-01-19 19:45:20 +0530

    feat: Answer 43 signal input

---
+++ apps/angular/signal-input/src/app/user.component.ts
@@ -0,0 +1,43 @@
+import { TitleCasePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+} from '@angular/core';
+
+type Category = 'Youth' | 'Junior' | 'Open' | 'Senior';
+const ageToCategory = (age: number): Category => {
+  if (age < 10) return 'Youth';
+  else if (age < 18) return 'Junior';
+  else if (age < 35) return 'Open';
+  return 'Senior';
+};
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [TitleCasePipe],
+  template: `
+    {{ fullName | titlecase }} plays tennis in the {{ category }} category!!
+  `,
+  host: {
+    class: 'text-xl text-green-800',
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserComponent implements OnChanges {
+  name = input.required<string>();
+  lastName = input<string>('');
+  age= input<string,number>(0, {
+    transform:(v:string) => Number(v)
+  });
+
+  fullName = '';
+  category: Category = 'Junior';
+
+  ngOnChanges(): void {
+    this.fullName = `${this.name()} ${this.lastName()}`;
+    this.category = ageToCategory(this.age());
+  }
+}
