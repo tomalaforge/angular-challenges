9a928ab0c290fbce4dfcc5a8c0bee4ee002620be
GRDtechlab <grdtechlab@gmail.com>
2024-01-19 15:57:21 +0530

    Answer:43 input branch signal

--- apps/angular/signal-input/src/app/user.component.ts
+++ apps/angular/signal-input/src/app/user.component.ts
@@ -2,8 +2,8 @@ import { TitleCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
   OnChanges,
+  input
 } from '@angular/core';
 
 type Category = 'Youth' | 'Junior' | 'Open' | 'Senior';
@@ -27,15 +27,17 @@ const ageToCategory = (age: number): Category => {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent implements OnChanges {
-  @Input({ required: true }) name!: string;
-  @Input() lastName?: string;
-  @Input() age?: string;
+  name = input.required<string>();
+  lastName = input<string>('');
+  age= input<string,number>(0, {
+    transform:(v:string) => Number(v)
+  });
 
   fullName = '';
   category: Category = 'Junior';
 
   ngOnChanges(): void {
-    this.fullName = `${this.name} ${this.lastName ?? ''}`;
-    this.category = ageToCategory(Number(this.age) ?? 0);
+    this.fullName = `${this.name()} ${this.lastName()}`;
+    this.category = ageToCategory(this.age());
   }
 }
