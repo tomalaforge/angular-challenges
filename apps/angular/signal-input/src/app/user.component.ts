7662b8c4c4ec7df142eeca7fb7ceb1b4645eb2f7
GRDtechlab <grdtechlab@gmail.com>
2024-01-20 19:37:15 +0530

    Answer:43 signal input solution updated as per the suggested comments

--- apps/angular/signal-input/src/app/user.component.ts
+++ apps/angular/signal-input/src/app/user.component.ts
@@ -3,7 +3,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   input,
-  OnChanges,
+  computed,
+  numberAttribute,
 } from '@angular/core';
 
 type Category = 'Youth' | 'Junior' | 'Open' | 'Senior';
@@ -19,29 +20,20 @@ const ageToCategory = (age: number): Category => {
   standalone: true,
   imports: [TitleCasePipe],
   template: `
-    {{ fullName | titlecase }} plays tennis in the {{ category }} category!!
+    {{ fullName() | titlecase }} plays tennis in the {{ category() }} category!!
   `,
   host: {
     class: 'text-xl text-green-800',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserComponent implements OnChanges {
-  // @Input({ required: true }) name!: string;
-  // @Input() lastName?: string;
-  // @Input() age?: string;
-
+export class UserComponent {
   name = input.required<string>();
-  lastName = input<string>('')
-  age = input<string,number>(0, {
-    transform: (v:any) => Number(v)
-  })
+  lastName = input<string>('');
+  age = input<number, unknown>(0, {
+    transform: numberAttribute,
+  });
 
-  fullName = '';
-  category: Category = 'Junior';
-
-  ngOnChanges(): void {
-    this.fullName = `${this.name()} ${this.lastName()}`;
-    this.category = ageToCategory(Number(this.age()));
-  }
+  fullName = computed(() => `${this.name()} ${this.lastName()}`);
+  category = computed(() => ageToCategory(this.age()));
 }
