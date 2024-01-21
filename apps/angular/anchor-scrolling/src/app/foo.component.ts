27799540fd449a73947d1f741d4d167bd606619c
Alejandro <alejandro.glv@gmail.com>
2024-01-21 14:22:51 -0400

    refactor: routerlink scroll

--- apps/angular/anchor-scrolling/src/app/foo.component.ts
+++ apps/angular/anchor-scrolling/src/app/foo.component.ts
@@ -7,7 +7,7 @@ import { NavButtonComponent } from './nav-button.component';
   selector: 'app-foo',
   template: `
     Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
+    <nav-button href="/home" class="fixed left-1/2 top-3">Home Page</nav-button>
     <div class="h-screen bg-blue-200">section 1</div>
     <div class="h-screen bg-red-200">section 2</div>
   `,
--- apps/angular/anchor-scrolling/src/app/home.component.ts
+++ apps/angular/anchor-scrolling/src/app/home.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  inject,
+  signal,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
@@ -6,15 +13,68 @@ import { NavButtonComponent } from './nav-button.component';
   imports: [NavButtonComponent],
   selector: 'app-home',
   template: `
-    <nav-button href="/foo" class="fixed left-1/2 top-3">Foo Page</nav-button>
-    <div id="top" class="h-screen bg-gray-500">
-      Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+    <div
+      id="top"
+      class="top-0 flex h-40 flex-col items-end gap-3 bg-red-300 text-center">
+      <div class="fixed flex gap-3">
+        Top
+        <nav-button href="/home" ref="section1" class="">Section 1</nav-button>
+        <nav-button href="/home" ref="section2" class="">Section 2</nav-button>
+        <nav-button href="/home" ref="section3" class="">Section 3</nav-button>
+      </div>
+      <nav-button href="/foo" class="fixed left-1/2 top-3">
+        Go to Foo Page
+      </nav-button>
     </div>
-    <div id="bottom" class="h-screen bg-blue-300">
-      I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+    <div id="section1" class="h-screen bg-gray-500 text-center text-2xl">
+      I am section 1
+    </div>
+    <div id="section2" class="h-screen bg-blue-300 text-center text-2xl">
+      I am section 2
+    </div>
+    <div id="section3" class="h-screen bg-purple-300 text-center text-2xl">
+      I am section 3
+    </div>
+    <div id="bottom" class="bottom-0 h-12 bg-red-300 text-center">Bottom</div>
+    <div class="fixed bottom-0">
+      @switch (section()) {
+        @case ('top') {
+          <nav-button href="/home" ref="bottom">Go bottom</nav-button>
+        }
+        @case ('bottom') {
+          <nav-button href="/home" ref="top">Go top</nav-button>
+        }
+        @default {
+          <nav-button href="/home" ref="bottom">Go bottom</nav-button>
+        }
+      }
     </div>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterContentInit {
+  private activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  private el: ElementRef = inject(ElementRef);
+  section = signal<string>('top');
+
+  ngAfterContentInit(): void {
+    this.activeRoute.fragment.subscribe({
+      next: (res) => {
+        if (res) {
+          this.goToSection(res);
+          this.section.set(res);
+        }
+      },
+    });
+  }
+  goToSection(fragment: string) {
+    //const element = this.el.nativeElement.querySelector(`#${fragment}`);
+    const element = document.getElementById(fragment);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  }
+}
