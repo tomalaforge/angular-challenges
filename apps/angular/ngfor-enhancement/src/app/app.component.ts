9fc97d5fa429887178737979ba60cacabaee5a18
Alejandro <alejandro.glv@gmail.com>
2024-01-23 13:58:00 -0400

    refactor: custom ngfor directive

--- apps/angular/ngfor-enhancement/src/app/app.component.ts
+++ apps/angular/ngfor-enhancement/src/app/app.component.ts
@@ -1,25 +1,71 @@
-import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 interface Person {
   name: string;
 }
 
+class ContextType<T> {
+  public get $implicit() {
+    return this.ngFor;
+  }
+  public ngFor!: T;
+}
+
+@Directive({
+  selector: '[ngFor]',
+  standalone: true,
+})
+export class NgForDirective<T> implements OnInit {
+  @Input('ngForOf') list!: Array<T>;
+  @Input('ngForEmpty') emptyList!: TemplateRef<any>;
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private template: TemplateRef<any>,
+  ) {}
+
+  ngOnInit(): void {
+    if (this.list.length > 0) {
+      this.list.forEach((item) => {
+        const context = new ContextType<T>();
+        context.ngFor = item;
+        this.viewContainerRef.createEmbeddedView(this.template, context);
+      });
+    } else {
+      this.viewContainerRef.createEmbeddedView(this.emptyList);
+    }
+  }
+}
+
 @Component({
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgForDirective],
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
+    <div *ngFor="let mydefault of persons; empty: emptyList">
+      {{ mydefault.name }}
+    </div>
     <ng-template #emptyList>The list is empty !!</ng-template>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  persons: Person[] = [];
+  persons: Person[] = [
+    { name: 'Monica' },
+    { name: 'Chandler' },
+    { name: 'Phoebe' },
+    { name: 'Joey' },
+    { name: 'Rachel' },
+    { name: 'Esther' },
+    { name: 'Ross' },
+  ];
 }
