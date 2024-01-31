96c2156237002e2612b112b1151f6646412919b2
thomas <toma.laforge@gmail.com>
2024-01-31 14:41:04 +0100

    fix: lang

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -47,10 +47,8 @@ const i18n = defineCollection({
         'challenge.footer.communityAnswers': z.string(),
         'challenge.footer.authorAnswer': z.string(),
         'challenge.footer.blogPost': z.string(),
-        'challenge.footer.gettingStarted': z.object({
-          title: z.string(),
-          link: z.string(),
-        }),
+        'challenge.footer.gettingStarted.title': z.string(),
+        'challenge.footer.gettingStarted.link': z.string(),
       })
       .partial(),
   }),
