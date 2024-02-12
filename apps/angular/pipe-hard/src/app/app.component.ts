0ffc4aee98fb3fee56cd84cc6d78414e3acaddde
ahmed-aouinti <awintiahmed2017@gmail.com>
2024-02-12 17:12:44 +0100

    feat: pipe hard challenge solution

--- apps/angular/pipe-hard/src/app/app.component.ts
+++ apps/angular/pipe-hard/src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { PersonUtils } from './person.utils';
+import { CustomPipe } from './shared/custom.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, CustomPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let activity of activities">
       {{ activity.name }} :
       <div
         *ngFor="let person of persons; let index = index; let isFirst = first">
-        {{ showName(person.name, index) }}
-        {{ isAllowed(person.age, isFirst, activity.minimumAge) }}
+        {{ person | custom: index : isFirst : activity.minimumAge }}
       </div>
     </div>
   `,
@@ -29,8 +28,4 @@ export class AppComponent {
     { name: 'hiking', minimumAge: 25 },
     { name: 'dancing', minimumAge: 1 },
   ];
-
-  showName = PersonUtils.showName;
-
-  isAllowed = PersonUtils.isAllowed;
 }
