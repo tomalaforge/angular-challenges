93b526190178285f1a6427a4d992b2d9aa7ee063
ipopa144 <44372517+ipopa144@users.noreply.github.com>
2024-02-15 09:26:56 +0200

    feat(solution): projection

--- apps/angular/projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/projection/src/app/component/student-card/student-card.component.ts
@@ -1,40 +1,62 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Observable } from 'rxjs';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
-import { CardType } from '../../model/card.model';
 import { Student } from '../../model/student.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
     <app-card
-      [list]="students"
-      [type]="cardType"
-      customClass="bg-light-green"></app-card>
+      [list]="students$ | async"
+      (add)="addStudent()"
+      class="bg-light-green">
+      <img src="assets/img/student.webp" width="200px" />
+
+      <ng-template #rowRef let-student>
+        <app-list-item (delete)="deleteStudent(student.id)">
+          {{ student.firstName }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
   `,
   standalone: true,
   styles: [
     `
-      ::ng-deep .bg-light-green {
+      .bg-light-green {
         background-color: rgba(0, 250, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent, AsyncPipe],
 })
 export class StudentCardComponent implements OnInit {
-  students: Student[] = [];
-  cardType = CardType.STUDENT;
+  students$: Observable<Student[]>;
 
   constructor(
     private http: FakeHttpService,
     private store: StudentStore,
-  ) {}
+  ) {
+    this.students$ = this.store.students$;
+  }
 
   ngOnInit(): void {
-    this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+    this.http.fetchStudents$.subscribe((students) =>
+      this.store.addAll(students),
+    );
+  }
 
-    this.store.students$.subscribe((s) => (this.students = s));
+  addStudent() {
+    this.store.addOne(randStudent());
+  }
+
+  deleteStudent(studentId: number): void {
+    this.store.deleteOne(studentId);
   }
 }
--- apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,40 +1,62 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Observable } from 'rxjs';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
     <app-card
-      [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+      [list]="teachers$ | async"
+      (add)="addTeacher()"
+      class="bg-light-red">
+      <img src="assets/img/teacher.png" width="200px" />
+
+      <ng-template #rowRef let-teacher>
+        <app-list-item (delete)="deleteTeacher(teacher.id)">
+          {{ teacher.firstName }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      .bg-light-red {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent, AsyncPipe],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  teachers$: Observable<Teacher[]>;
 
   constructor(
     private http: FakeHttpService,
     private store: TeacherStore,
-  ) {}
+  ) {
+    this.teachers$ = this.store.teachers$;
+  }
 
   ngOnInit(): void {
-    this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+    this.http.fetchTeachers$.subscribe((teachers) =>
+      this.store.addAll(teachers),
+    );
+  }
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+  addTeacher(): void {
+    this.store.addOne(randTeacher());
+  }
+
+  deleteTeacher(teacherId: number): void {
+    this.store.deleteOne(teacherId);
   }
 }
