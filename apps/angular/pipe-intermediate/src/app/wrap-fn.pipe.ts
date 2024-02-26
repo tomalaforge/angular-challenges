589ae84b907d7c8c7243ec5792f5bf478897d101
Sven Brodny <svenbrodny@googlemail.com>
2024-02-26 20:33:42 +0100

    refactor: wrap fn pipe function type

--- apps/angular/pipe-intermediate/src/app/wrap-fn.pipe.ts
+++ apps/angular/pipe-intermediate/src/app/wrap-fn.pipe.ts
@@ -4,8 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'wrapFn',
   standalone: true,
 })
-export class WrapFnPipe implements PipeTransform {
-  transform<R, F extends (...args: never[]) => R>(
+export class WrapFnPipe<T> implements PipeTransform {
+  transform<R, F extends (...args: T[]) => R>(
     func: F,
     ...args: Parameters<F>
   ): R {
