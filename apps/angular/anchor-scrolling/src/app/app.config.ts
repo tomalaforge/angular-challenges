7d866c4509fd1d2fd420dda6c77e10f1118a7875
kunlinl <scu2018lkl@gmail.com>
2024-03-07 16:11:42 +0800

    chore: add scroll behaviour

--- apps/angular/anchor-scrolling/src/app/app.config.ts
+++ apps/angular/anchor-scrolling/src/app/app.config.ts
@@ -1,6 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
+
+const scrollOptions: InMemoryScrollingOptions = {
+  anchorScrolling: 'enabled',
+  scrollPositionRestoration: 'enabled',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes, withInMemoryScrolling(scrollOptions))],
 };
