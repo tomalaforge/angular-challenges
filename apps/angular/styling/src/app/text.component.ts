3983042d24fb744ab640ba05322908bbe3f802b9
kunlinl <scu2018lkl@gmail.com>
2024-03-12 11:07:24 +0800

    refactor: remove @Input which for style usages

--- apps/angular/styling/src/app/text.component.ts
+++ apps/angular/styling/src/app/text.component.ts
@@ -1,16 +1,14 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'text',
   standalone: true,
   template: `
-    <p style="font-size: {{ font }}px; color: {{ color }}">
+    <p>
       <ng-content></ng-content>
     </p>
   `,
+  styleUrls: ['text.component.scss'],
 })
-export class TextComponent {
-  @Input() font = 10;
-  @Input() color = 'black';
-}
+export class TextComponent {}
