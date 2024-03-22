35542618b446be5b925b4b79641a106355dc5508
flex2016 <reloded2004@hotmail.com>
2024-03-22 13:33:36 -0400

    feat: projection added city card

--- apps/angular/projection/src/app/ui/card/card.component.ts
+++ apps/angular/projection/src/app/ui/card/card.component.ts
@@ -1,6 +1,11 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
+import { CityStore } from '../../data-access/city.store';
+import {
+  randStudent,
+  randTeacher,
+  randomCity,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
 import { TeacherStore } from '../../data-access/teacher.store';
 import { CardType } from '../../model/card.model';
@@ -20,11 +25,15 @@ import { ListItemComponent } from '../list-item/list-item.component';
         *ngIf="type === CardType.STUDENT"
         src="assets/img/student.webp"
         width="200px" />
+      <img
+        *ngIf="type === CardType.CITY"
+        src="assets/img/city.png"
+        width="200px" />
 
       <section>
         <app-list-item
           *ngFor="let item of list"
-          [name]="item.firstName"
+          [name]="item.firstName || item.name"
           [id]="item.id"
           [type]="type"></app-list-item>
       </section>
@@ -49,6 +58,7 @@ export class CardComponent {
   constructor(
     private teacherStore: TeacherStore,
     private studentStore: StudentStore,
+    private cityStore: CityStore,
   ) {}
 
   addNewItem() {
@@ -56,6 +66,8 @@ export class CardComponent {
       this.teacherStore.addOne(randTeacher());
     } else if (this.type === CardType.STUDENT) {
       this.studentStore.addOne(randStudent());
+    } else if (this.type === CardType.CITY) {
+      this.cityStore.addOne(randomCity());
     }
   }
 }
