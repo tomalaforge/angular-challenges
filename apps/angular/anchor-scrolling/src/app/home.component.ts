316d72c2ab7789080ab36bac2d24b30829a616bf
s.suyunov <s.suyunov@fincube.uz>
2024-03-27 16:49:59 +0500

    feat: navigate and scroll href with Routerlink Anchor-navigation task done with ViewportScroller methods

--- apps/angular/anchor-scrolling/src/app/home.component.ts
+++ apps/angular/anchor-scrolling/src/app/home.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterLink],
   selector: 'app-home',
   template: `
-    <nav-button href="/foo" class="fixed left-1/2 top-3">Foo Page</nav-button>
+    <nav-button [routerLink]="'/foo'" class="fixed left-1/2 top-3">
+      Foo Page
+    </nav-button>
     <div id="top" class="h-screen bg-gray-500">
       Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+      <nav-button (click)="goDown()">Scroll Bottom</nav-button>
     </div>
     <div id="bottom" class="h-screen bg-blue-300">
       I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+      <nav-button (click)="goUP()">Scroll Top</nav-button>
     </div>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  scroller = inject(ViewportScroller);
+
+  goUP() {
+    //put id value of scrolling section into scrollToAnchor() method as a parameter
+    this.scroller.scrollToAnchor('top');
+  }
+
+  goDown() {
+    //put id value of scrolling section into scrollToAnchor() method as a parameter
+    this.scroller.scrollToAnchor('bottom');
+  }
+}
--- apps/angular/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/angular/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="href">
       <ng-content></ng-content>
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
   @Input() href = '';
