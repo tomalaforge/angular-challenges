b37e47744479c479ecff113500eeb97c82b0ed62
s.suyunov <s.suyunov@fincube.uz>
2024-04-02 16:45:45 +0500

    feat: task anchor navigation Added router link with smooth scroll

--- apps/angular/anchor-scrolling/src/app/app.routes.ts
+++ apps/angular/anchor-scrolling/src/app/app.routes.ts
@@ -1,6 +1,10 @@
-import { Route } from '@angular/router';
+import { ExtraOptions, Route } from '@angular/router';
 import { FooComponent } from './foo.component';
 import { HomeComponent } from './home.component';
+
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+};
 
 export const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
--- apps/angular/anchor-scrolling/src/app/home.component.ts
+++ apps/angular/anchor-scrolling/src/app/home.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterModule],
   selector: 'app-home',
   template: `
-    <nav-button href="/foo" class="fixed left-1/2 top-3">Foo Page</nav-button>
-    <div id="top" class="h-screen bg-gray-500">
+    <nav-button [routerLink]="'/foo'" class="fixed left-1/2 top-3">
+      Foo Page
+    </nav-button>
+    <div id="top" class="h-screen bg-gray-500" (click)="scrollTo('bottom')">
       Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+      <nav-button>Scroll Bottom</nav-button>
     </div>
-    <div id="bottom" class="h-screen bg-blue-300">
+    <div id="bottom" class="h-screen bg-blue-300" (click)="scrollTo('top')">
       I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+      <nav-button>Scroll Top</nav-button>
     </div>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  scroller = inject(ViewportScroller);
+
+  scrollTo(anchor: string) {
+    const element = document.getElementById(anchor);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
+``;
