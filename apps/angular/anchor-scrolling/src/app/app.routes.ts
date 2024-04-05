8a0d00c237a351aaf76b68fac69bfc4505bfefe3
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2024-04-05 17:00:46 +0500

    fix: task anchor navigation removed unused code and imports

--- apps/angular/anchor-scrolling/src/app/app.routes.ts
+++ apps/angular/anchor-scrolling/src/app/app.routes.ts
@@ -1,10 +1,6 @@
-import { ExtraOptions, Route } from '@angular/router';
+import { Route } from '@angular/router';
 import { FooComponent } from './foo.component';
 import { HomeComponent } from './home.component';
-
-const routerOptions: ExtraOptions = {
-  anchorScrolling: 'enabled',
-};
 
 export const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
