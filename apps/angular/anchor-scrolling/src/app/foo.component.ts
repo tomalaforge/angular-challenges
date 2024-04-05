06aeb2793a6218be98483c03d20d978ea6497b68
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2024-04-05 16:58:36 +0500

    fix: task anchor navigation Fixed PR issues on anchor navigation task

--- apps/angular/anchor-scrolling/src/app/foo.component.ts
+++ apps/angular/anchor-scrolling/src/app/foo.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterModule],
   selector: 'app-foo',
   template: `
     Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
-    <div class="h-screen bg-blue-200">section 1</div>
-    <div class="h-screen bg-red-200">section 2</div>
+    <nav-button [routerLink]="'home'" class="fixed left-1/2 top-3">
+      Home Page
+    </nav-button>
+    <div
+      id="section1"
+      class="h-screen bg-blue-200"
+      (click)="scrollTo('section2')">
+      section 1
+      <button class="m-2 block w-fit rounded-md border border-red-500 p-4">
+        Section 2
+      </button>
+    </div>
+    <div
+      id="section2"
+      class="h-screen bg-red-200"
+      (click)="scrollTo('section1')">
+      section 2
+      <button class="m-2 block w-fit rounded-md border border-red-500 p-4">
+        Section 1
+      </button>
+    </div>
   `,
 })
-export class FooComponent {}
+export class FooComponent {
+  scroller = inject(ViewportScroller);
+
+  scrollTo(anchor: string) {
+    const element = document.getElementById(anchor);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
