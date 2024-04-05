bc52f30798861faaa49d0fe26f706a6887d01910
Nguyễn Quốc Hưng <quochung9401@gmail.com>
2024-04-05 15:46:17 +0700

    feat(projection): add host class for list item component

--- apps/angular/projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/projection/src/app/ui/list-item/list-item.component.ts
@@ -3,14 +3,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-list-item',
   template: `
-    <div class="border-grey-300 flex justify-between border px-2 py-1">
-      {{ name }}
-      <button (click)="delete(id)">
-        <img class="h-5" src="assets/svg/trash.svg" />
-      </button>
-    </div>
+    {{ name }}
+    <button (click)="delete(id)">
+      <img class="h-5" src="assets/svg/trash.svg" />
+    </button>
   `,
   standalone: true,
+  host: {
+    class: 'border-grey-300 flex justify-between border px-2 py-1',
+  },
 })
 export class ListItemComponent {
   @Input() id!: number;
