3bb4feea945f02fbd0364503508449df14d6ab05
AzureAD\IoannisTsironis <i.tsironis@epg-financials.com>
2024-04-12 12:48:30 +0300

    feat: fix angular router input challenge

--- apps/angular/router-input/src/app/app.component.ts
+++ apps/angular/router-input/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { RouterLink, RouterModule } from '@angular/router';
     <label for="testId">TestId</label>
     <input id="testId" type="number" [formControl]="testId" />
     <button
+      [disabled]="!testId.value"
       [routerLink]="'subscription/' + testId.value"
       [queryParams]="{ user: userName.value }">
       Test
