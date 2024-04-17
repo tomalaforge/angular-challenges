3e35a29ed1227cb38ec55100b3943f538fb84fcc
Sven Brodny <svenbrodny@googlemail.com>
2024-04-17 09:56:05 +0200

    feat(Answer:48): avoid losing form data solution

--- apps/forms/avoid-losing-form-data/src/app/ui/dialog.component.ts
+++ apps/forms/avoid-losing-form-data/src/app/ui/dialog.component.ts
@@ -1,24 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDialogClose } from '@angular/material/dialog';
 
 // NOTE : this is just the dialog content, you need to implement dialog logic
 
 @Component({
   standalone: true,
+  imports: [MatDialogClose],
   template: `
-    <div role="alert" class="rounded-xl border border-gray-100 bg-white p-5">
-      <h3 class="block text-xl font-medium text-red-600">
-        You have unsaved information!
+    <div
+      role="alertdialog"
+      aria-labelledby="alert-title"
+      aria-describedby="alert-description"
+      class="rounded-xl border border-gray-100 bg-white p-5">
+      <h3 id="alert-title" class="block text-xl font-medium text-red-600">
+        Confirmation
       </h3>
 
-      <p class="mt-1 text-gray-700">Do you want to continue and lose them?</p>
+      <p id="alert-description" class="mt-1 text-gray-700">
+        You have unsaved information! Do you want to continue and lose them?
+      </p>
 
       <div class="mt-4 flex gap-2">
         <button
+          [matDialogClose]="true"
           class="inline-flex items-center gap-2 rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700">
           Yes continue
         </button>
 
         <button
+          [matDialogClose]="false"
           class="block rounded-lg px-4 py-2 text-gray-700 transition hover:bg-gray-50">
           Stay on page
         </button>
