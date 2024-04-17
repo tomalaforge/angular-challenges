41fc261705acc08850ca09475c6bbc3f829bc8bd
thomas <toma.laforge@gmail.com>
2024-04-17 21:45:41 +0200

    feat: add add loabl

--- .github/github-action/index.js
+++ .github/github-action/index.js
@@ -1,15 +1,48 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const core = require('@actions/core');
+const contributors = require('./contributors');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const title = github.context.payload.pull_request.title;
+    const labels = [];
+
+    if(!title.startsWith('Answer:')) {
+      return;
+    }
+    labels.push('answer');
+
+    const match = title.match(/Answer:\s*(\d+)/);
+    if (match) {
+      labels.push(parseInt(match[1], 10));
+    }
+
+    const actor = github.context.actor;
+    if(contributors.includes(actor)) {
+      labels.push('to be reviewed');
+    }
+
+    const githubToken = core.getInput('github_token');
+
+    const [owner, repo] = core.getInput('repo').split('/');
+    const number =
+      core.getInput('number') === ''
+        ? github.context.issue.number
+        : parseInt(core.getInput('number'));
+
+    const octokit = github.getOctokit(githubToken);
+    await octokit.rest.issues.addLabels({
+      labels,
+      owner,
+      repo,
+      issue_number: number
+    });
+  } catch (e) {
+    if (e instanceof Error) {
+      core.error(e);
+      core.setFailed(e.message);
+    }
+  }
 }
+
+run();
