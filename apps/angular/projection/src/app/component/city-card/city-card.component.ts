5697511058f23f37b81808a7801b5027c04ecce5
Hyun Don Moon <hyundonmoon@gmail.com>
2024-04-29 20:02:53 +0900

    feat: create city card component and implement relevant features

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardBgColorDirective } from '../../directive/card-background-color.directive';
+import { CardImageTemplateDirective } from '../../directive/card-image-template.directive';
+import { ListItemTemplateDirective } from '../../directive/list-item-template-directive';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
   standalone: true,
-  imports: [],
+  templateUrl: './city-card.component.html',
+  imports: [
+    CardComponent,
+    CardImageTemplateDirective,
+    ListItemComponent,
+    ListItemTemplateDirective,
+    CardBgColorDirective,
+  ],
 })
-export class CityCardComponent implements OnInit {
-  constructor() {}
+export class CityCardComponent implements OnInit, OnDestroy {
+  private store = inject(CityStore);
+  private http = inject(FakeHttpService);
+  private destroy$ = new Subject<void>();
 
-  ngOnInit(): void {}
+  cities: City[] = [];
+
+  ngOnInit() {
+    this.http.fetchCities$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (cities) => {
+        this.store.addAll(cities);
+      },
+    });
+
+    this.store.cities$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (cities) => {
+        this.cities = cities;
+      },
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  getCityName({ name, country }: City): string {
+    return `${name}, ${country}`;
+  }
+
+  onAddCityItem() {
+    this.store.addOne(randomCity());
+  }
+
+  onDeleteCityItem(id: number) {
+    this.store.deleteOne(id);
+  }
 }
