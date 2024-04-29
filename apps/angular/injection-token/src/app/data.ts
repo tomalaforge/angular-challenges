31416582ae1852ecc19d3ac7d007dbbd84e3a5ec
Sidhartha.Verma <sidharthv91@gmail.com>
2024-04-29 12:42:17 -0300

    feat: injection tokens challenge

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,1 +1,9 @@
-export const DEFAULT_TIMER = 1000;
+import { InjectionToken } from '@angular/core';
+
+const DEFAULT_TIMER = 1000;
+
+export const TIMER_TOKEN = new InjectionToken('timer', {
+  factory: () => DEFAULT_TIMER,
+});
+
+export default TIMER_TOKEN;
--- apps/angular/injection-token/src/app/phone.component.ts
+++ apps/angular/injection-token/src/app/phone.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import TIMER_TOKEN from './data';
 import { TimerContainerComponent } from './timer-container.component';
 
 @Component({
   selector: 'app-phone',
   standalone: true,
   imports: [TimerContainerComponent],
+  providers: [{ provide: TIMER_TOKEN, useValue: '2000' }],
   template: `
     <div class="flex gap-2">
       Phone Call Timer:
