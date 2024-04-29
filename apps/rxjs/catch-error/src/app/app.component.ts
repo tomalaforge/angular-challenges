12d80db061c261efcfa2b3716bc1c549171ddc14
aserdulet <serdulet.a@gmail.com>
2024-04-30 01:30:38 +0300

    feat(challenge-38): added catch solution

--- apps/rxjs/catch-error/src/app/app.component.ts
+++ apps/rxjs/catch-error/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { Subject, concatMap, map } from 'rxjs';
+import { Subject, catchError, concatMap, map, of } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -42,8 +42,14 @@ export class AppComponent implements OnInit {
       .pipe(
         map(() => this.input),
         concatMap((value) =>
-          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`),
+          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`).pipe(
+            catchError((error: HttpErrorResponse) => {
+              console.error(error.message);
+              return of([]);
+            }),
+          ),
         ),
+
         takeUntilDestroyed(this.destroyRef),
       )
       .subscribe({
