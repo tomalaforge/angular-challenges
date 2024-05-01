68e2afcce630f81d522467d42420fa13a62583a2
aserdulet <serdulet.a@gmail.com>
2024-05-01 18:51:31 +0300

    feat(challenge-43): added signal input solution

--- apps/angular/signal-input/src/app/user.component.ts
+++ apps/angular/signal-input/src/app/user.component.ts
@@ -2,8 +2,8 @@ import { TitleCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
-  OnChanges,
+  computed,
+  input,
 } from '@angular/core';
 
 type Category = 'Youth' | 'Junior' | 'Open' | 'Senior';
@@ -14,28 +14,39 @@ const ageToCategory = (age: number): Category => {
   return 'Senior';
 };
 
+export function convertAgeToNumber<T>(value: T): number {
+  if (typeof value === 'string') {
+    return +value;
+  } else if (typeof value === 'number') {
+    return value;
+  } else {
+    return 0;
+  }
+}
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [TitleCasePipe],
   template: `
-    {{ fullName | titlecase }} plays tennis in the {{ category }} category!!
+    {{ fullName() | titlecase }} plays tennis in the {{ category() }} category!!
   `,
   host: {
     class: 'text-xl text-green-800',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserComponent implements OnChanges {
-  @Input({ required: true }) name!: string;
-  @Input() lastName?: string;
-  @Input() age?: string;
+export class UserComponent {
+  name = input.required<string>();
+  lastName = input<string>();
+  age = input(0, {
+    transform: convertAgeToNumber<string>,
+  });
 
-  fullName = '';
-  category: Category = 'Junior';
+  fullName = computed<string>(() => {
+    return `${this.name()} ${this.lastName() ?? ''}`;
+  });
 
-  ngOnChanges(): void {
-    this.fullName = `${this.name} ${this.lastName ?? ''}`;
-    this.category = ageToCategory(Number(this.age) ?? 0);
-  }
+  category = computed<Category>(() => {
+    return ageToCategory(this.age());
+  });
 }
