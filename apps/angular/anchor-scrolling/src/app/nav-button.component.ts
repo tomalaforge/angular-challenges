fedbc0aba877bb8b143b448a8e99ae41cca910af
Lance Finney <lafinney@cisco.com>
2024-05-02 17:06:11 -0500

    feat(Answer:21): anchor scrolling
    Both anchorScrolling and scroll-behavior were new for me. Thanks!

--- apps/angular/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/angular/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="href()" [fragment]="anchor()">
       <ng-content></ng-content>
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
-  @Input() href = '';
+  readonly href = input<string>('');
+  readonly anchor = input<string | undefined>();
 }
