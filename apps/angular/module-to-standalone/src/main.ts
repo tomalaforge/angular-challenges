8aef8d7aa83ee601d201d627019371cfd392c2b0
Lance Finney <lafinney@cisco.com>
2024-05-03 13:51:29 -0500

    feat(Answer:31): remove MainShellModule

--- apps/angular/module-to-standalone/src/main.ts
+++ apps/angular/module-to-standalone/src/main.ts
@@ -1,8 +1,9 @@
-import { MainShellModule } from '@angular-challenges/module-to-standalone/shell';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideToken } from '@angular-challenges/module-to-standalone/core/providers';
+import { appRoutes } from '@angular-challenges/module-to-standalone/shell';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, MainShellModule)],
+  providers: [provideRouter(appRoutes), provideToken('main-shell-token')],
 }).catch((err) => console.error(err));
