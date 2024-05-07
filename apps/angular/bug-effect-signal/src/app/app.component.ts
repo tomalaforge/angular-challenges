1312f3fd1d6d2c83019aaab6fa77f40e93a6b5c7
Sven Brodny <svenbrodny@googlemail.com>
2024-05-07 16:50:51 +0200

    feat(Answer:50): bug in effect solution

--- apps/angular/bug-effect-signal/src/app/app.component.ts
+++ apps/angular/bug-effect-signal/src/app/app.component.ts
@@ -28,7 +28,24 @@ export class AppComponent {
 
   constructor() {
     effect(() => {
-      if (this.name() || this.age() || this.address()) {
+      /* 
+        This effect executes only once when name is true, because the value of name doesn't change.
+        Effects run when a signal inside changes, since name value is the same as before, the effect doesn't trigger.
+        If we click on age first, the effect will run again after click on name. 
+        Because there is no signal value which has the same value as before.
+      */
+      // if (this.name() || this.age() || this.address()) {
+      //   alert('Checkbox was checked');
+      // }
+      /* 
+        This effect should run if any of these 3 values is true, you have to define those values.
+        With this all signal values are interpreted as relevant values for this effect.
+        So if any of these values changes, the effect will run.
+      */
+      const name = this.name();
+      const age = this.age();
+      const address = this.address();
+      if (name || age || address) {
         alert('Checkbox was checked');
       }
     });
