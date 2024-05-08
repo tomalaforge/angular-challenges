2d3509068e87c25cb9bcbbe356f7161b45eb0332
Lance Finney <lafinney@cisco.com>
2024-05-08 12:04:30 -0500

    feat(Answer:50): bug effect signal

--- apps/angular/bug-effect-signal/src/app/app.component.ts
+++ apps/angular/bug-effect-signal/src/app/app.component.ts
@@ -27,8 +27,30 @@ export class AppComponent {
   address = model(false);
 
   constructor() {
+    // Assuming I correctly understand that the goal is to show the alert
+    // whenever any of them is checked (independent of whether any others are
+    // selected), and that we shouldn't show the alert when any of them
+    // unchecked, then each signal needs to be considered separately.
+    //
+    // The problem with the previous implementation is that `||` is
+    // short-circuiting; if the first signal is true, the second and third
+    // signals are never checked, so we don't get the desired effect on
+    // subsequent checkboxes.
+    //
+    // If JavaScript had the '|' operator like Java, then we could use that
+    // instead.
     effect(() => {
-      if (this.name() || this.age() || this.address()) {
+      if (this.name()) {
+        alert('Checkbox was checked');
+      }
+    });
+    effect(() => {
+      if (this.age()) {
+        alert('Checkbox was checked');
+      }
+    });
+    effect(() => {
+      if (this.address()) {
         alert('Checkbox was checked');
       }
     });
