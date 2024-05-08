0a5ccefa7f34c850cfce9c63d346992e1de55611
Lance Finney <lafinney@cisco.com>
2024-05-08 17:00:26 -0500

    feat(Answer:9): function pipe

--- apps/angular/pipe-intermediate/src/app/app.component.ts
+++ apps/angular/pipe-intermediate/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { WrapFnPipe } from './wrap-fn.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [WrapFnPipe],
   selector: 'app-root',
   template: `
-    <div *ngFor="let person of persons; let index = index; let isFirst = first">
-      {{ showName(person.name, index) }}
-      {{ isAllowed(person.age, isFirst) }}
-    </div>
+    @for (person of persons; track person.name) {
+      <div>
+        {{ showName | wrapFn: person.name : $index }}
+        {{ isAllowed | wrapFn: person.age : $first }}
+      </div>
+    }
   `,
 })
 export class AppComponent {
