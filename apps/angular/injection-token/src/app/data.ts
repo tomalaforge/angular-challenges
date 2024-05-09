d16ea74f874f918dfae467d8d57bf1ed724e3319
Lance Finney <lafinney@cisco.com>
2024-05-09 14:04:25 -0500

    feat(Answer:39): injection token

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,1 +1,7 @@
+import { InjectionToken } from '@angular/core';
+
 export const DEFAULT_TIMER = 1000;
+
+export const TIMER_STATE = new InjectionToken<number>('TimerState', {
+  factory: () => DEFAULT_TIMER,
+});
