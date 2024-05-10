b153ad6a0e78b7740cb14b6d9760859dccb91420
Lance Finney <lafinney@cisco.com>
2024-05-10 14:40:40 -0500

    feat(Answer:34): low-hanging fruit

--- apps/performance/default-onpush/src/app/person-list.component.ts
+++ apps/performance/default-onpush/src/app/person-list.component.ts
@@ -1,7 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  model,
+} from '@angular/core';
 
 import { CDFlashingDirective } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,17 +17,17 @@ import { MatListModule } from '@angular/material/list';
   selector: 'app-person-list',
   standalone: true,
   imports: [
-    CommonModule,
+    CDFlashingDirective,
     FormsModule,
-    MatListModule,
+    MatChipsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatChipsModule,
-    CDFlashingDirective,
+    MatListModule,
+    TitleCasePipe,
   ],
   template: `
     <h1 cd-flash class="text-center font-semibold" title="Title">
-      {{ title | titlecase }}
+      {{ title() | titlecase }}
     </h1>
 
     <mat-form-field class="w-4/5" cd-flash>
@@ -35,33 +40,36 @@ import { MatListModule } from '@angular/material/list';
     </mat-form-field>
 
     <mat-list class="flex w-full">
-      <div *ngIf="names?.length === 0" class="empty-list-label">Empty list</div>
-      <mat-list-item
-        *ngFor="let name of names"
-        cd-flash
-        class="text-orange-500">
-        <div MatListItemLine class="flex justify-between">
-          <h3 title="Name">
-            {{ name }}
-          </h3>
-        </div>
-      </mat-list-item>
-      <mat-divider *ngIf="names?.length !== 0"></mat-divider>
+      @for (name of names(); track name) {
+        <mat-list-item cd-flash class="text-orange-500">
+          <div MatListItemLine class="flex justify-between">
+            <h3 title="Name">
+              {{ name }}
+            </h3>
+          </div>
+        </mat-list-item>
+      } @empty {
+        <div class="empty-list-label">Empty list</div>
+      }
+      @if (names().length !== 0) {
+        <mat-divider />
+      }
     </mat-list>
   `,
   host: {
     class: 'w-full flex flex-col items-center',
   },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonListComponent {
-  @Input() names: string[] = [];
-  @Input() title = '';
+  names = model.required<string[]>();
+  title = input.required<string>();
 
   label = '';
 
   handleKey(event: KeyboardEvent) {
     if (event.key === 'Enter') {
-      this.names?.unshift(this.label);
+      this.names.update((names) => [...names, this.label]);
       this.label = '';
     }
   }
