c8f0664c87331f31dcea58129939154a50802eb4
Lance Finney <lafinney@cisco.com>
2024-05-10 16:13:15 -0500

    feat(Answer:34): ngFor
    The important thing is the addition of "track", which is forced by `@for`.
    The rest of the change is irrelevant.

--- apps/performance/ngfor-optimize/src/app/person-list.component.ts
+++ apps/performance/ngfor-optimize/src/app/person-list.component.ts
@@ -1,37 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   template: `
-    <div
-      *ngFor="let person of persons"
-      class="flex items-center justify-between border-b">
-      <h3>{{ person.name }}</h3>
-      <div class="flex gap-10 py-1">
-        <button
-          class="rounded-md border bg-blue-500 p-2 text-white"
-          (click)="update.emit(person.email)">
-          UPDATE
-        </button>
-        <button
-          class="rounded-md border bg-red-500 p-2 text-white"
-          (click)="delete.emit(person.email)">
-          DELETE
-        </button>
+    @for (person of persons(); track person.email) {
+      <div class="flex items-center justify-between border-b">
+        <h3>{{ person.name }}</h3>
+        <div class="flex gap-10 py-1">
+          <button
+            class="rounded-md border bg-blue-500 p-2 text-white"
+            (click)="update.emit(person.email)">
+            UPDATE
+          </button>
+          <button
+            class="rounded-md border bg-red-500 p-2 text-white"
+            (click)="delete.emit(person.email)">
+            DELETE
+          </button>
+        </div>
       </div>
-    </div>
+    }
   `,
   host: {
     class: 'w-full flex flex-col',
   },
 })
 export class PersonListComponent {
-  @Input() persons: Person[] = [];
-  @Output() delete = new EventEmitter<string>();
-  @Output() update = new EventEmitter<string>();
+  persons = input.required<Person[]>();
+  delete = output<string>();
+  update = output<string>();
 }
