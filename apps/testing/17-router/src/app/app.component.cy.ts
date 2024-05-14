07ddda2e49bb9987b0385387c7f716f0ad01c759
Lance Finney <lafinney@cisco.com>
2024-05-14 15:50:48 -0500

    feat(Answer:17): router tests

--- apps/testing/17-router/src/app/app.component.cy.ts
+++ apps/testing/17-router/src/app/app.component.cy.ts
@@ -1,33 +1,76 @@
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 describe(AppComponent.name, () => {
+  const setup = () => {
+    cy.mount(AppComponent, {
+      providers: [provideRouter(appRoutes)],
+    });
+    cy.get('nav a:first').click();
+  };
+
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', () => {
-      //todo
+      setup();
+
+      cy.contains('Search criteria is required!');
+      cy.get('.error').should('exist');
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', () => {
-      //todo
+      setup();
+
+      cy.get('input').type('Not Found');
+
+      cy.get('button').click();
+
+      cy.contains('No book found for this search');
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('input').type('Harry');
+
+      cy.get('button').click();
+
+      cy.contains("Harry Potter and the Philosopher's Stone by J.K. Rowling");
+      cy.get('li').should('have.length', 1);
+      cy.get('.error').should('not.exist');
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('input').type('HARRY');
+
+      cy.get('button').click();
+
+      cy.contains("Harry Potter and the Philosopher's Stone by J.K. Rowling");
+      cy.get('li').should('have.length', 1);
+      cy.get('.error').should('not.exist');
     });
   });
 
-  describe('Given a search criteria with multple books matches', () => {
+  describe('Given a search criteria with multiple books matches', () => {
     it('Then shows a list of books', () => {
-      //todo
+      setup();
+
+      cy.get('input').type('Orwell');
+
+      cy.get('button').click();
+
+      cy.contains('1984 by George Orwell');
+      cy.contains('Animal Farm by George Orwell');
+      cy.get('li').should('have.length', 2);
+      cy.get('.error').should('not.exist');
     });
   });
 });
--- apps/testing/17-router/src/app/app.component.spec.ts
+++ apps/testing/17-router/src/app/app.component.spec.ts
@@ -1,31 +1,111 @@
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+
 describe('AppComponent', () => {
+  async function setup() {
+    await render(AppComponent, { routes: appRoutes });
+  }
+
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', async () => {
-      //todo
+      // Arrange
+      await setup();
+
+      // Act
+      await userEvent.click(
+        screen.getByRole('link', { name: /borrow a book/i }),
+      );
+
+      // Assert
+      expect(screen.getByRole('button', { name: /borrow/i })).toBeDisabled();
+      await screen.findByText(/search criteria is required!/i);
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', async () => {
-      //todo
+      // Arrange
+      await setup();
+      const inputField = screen.getByRole('textbox', {
+        name: /search book by author or title/i,
+      });
+      const borrowButton = screen.getByRole('button', { name: /borrow/i });
+
+      // Act
+      await userEvent.type(inputField, 'Not Found');
+      await userEvent.click(borrowButton);
+
+      // Assert
+      await screen.findByText(/no book found for this search/i);
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', async () => {
-      //todo
+      // Arrange
+      await setup();
+      const inputField = screen.getByRole('textbox', {
+        name: /search book by author or title/i,
+      });
+      const borrowButton = screen.getByRole('button', { name: /borrow/i });
+
+      // Act
+      await userEvent.type(inputField, 'Harry');
+      await userEvent.click(borrowButton);
+
+      // Assert
+      expect(screen.queryByText(/no book found for this search/i)).toBeFalsy();
+      expect(screen.queryByText(/search criteria is required!/i)).toBeFalsy();
+      expect(screen.queryByText(/The Lord of the Rings/)).toBeFalsy();
+      expect(screen.getAllByRole('listitem')).toHaveLength(1);
+      expect(screen.getByRole('listitem')).toHaveTextContent(/Harry Potter/);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', async () => {
-      //todo
+      // Arrange
+      await setup();
+      const inputField = screen.getByRole('textbox', {
+        name: /search book by author or title/i,
+      });
+      const borrowButton = screen.getByRole('button', { name: /borrow/i });
+
+      // Act
+      await userEvent.type(inputField, 'HARRY');
+      await userEvent.click(borrowButton);
+
+      // Assert
+      expect(screen.queryByText(/no book found for this search/i)).toBeFalsy();
+      expect(screen.queryByText(/search criteria is required!/i)).toBeFalsy();
+      expect(screen.queryByText(/The Lord of the Rings/)).toBeFalsy();
+      expect(screen.getAllByRole('listitem')).toHaveLength(1);
+      expect(screen.getByRole('listitem')).toHaveTextContent(/Harry Potter/);
     });
   });
 
-  describe('Given a search criteria with multple books matches', () => {
+  describe('Given a search criteria with multiple books matches', () => {
     it('Then shows a list of books', async () => {
-      //todo
+      // Arrange
+      await setup();
+      const inputField = screen.getByRole('textbox', {
+        name: /search book by author or title/i,
+      });
+      const borrowButton = screen.getByRole('button', { name: /borrow/i });
+
+      // Act
+      await userEvent.type(inputField, 'Orwell');
+      await userEvent.click(borrowButton);
+
+      // Assert
+      expect(screen.queryByText(/no book found for this search/i)).toBeFalsy();
+      expect(screen.queryByText(/search criteria is required!/i)).toBeFalsy();
+      expect(screen.queryByText(/The Lord of the Rings/)).toBeFalsy();
+      expect(screen.getAllByRole('listitem')).toHaveLength(2);
+      await screen.findByText(/Animal Farm/i);
+      await screen.findByText(/1984/i);
     });
   });
 });
