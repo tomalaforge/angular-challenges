f7814148c7789fc3945bd8de5eb313cde5f464d4
Lance Finney <lafinney@cisco.com>
2024-05-14 18:22:39 -0500

    feat(Answer:19): tests

--- apps/testing/19-input-output/src/app/counter.component.cy.ts
+++ apps/testing/19-input-output/src/app/counter.component.cy.ts
@@ -1,13 +1,50 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
 
 describe(CounterComponent.name, () => {
-  describe('Given an initualValue of 10', () => {
+  describe('Given an initialValue of 10', () => {
     it('listen to output using createOutputSpy', () => {
-      cy.mount(CounterComponent);
+      cy.mount(CounterComponent, {
+        componentProperties: {
+          send: createOutputSpy('sendSpy'),
+          initialValue: 10,
+        },
+      });
+
+      cy.get('p').contains('Counter: 10');
+
+      cy.get('button:contains(Increment)').click();
+
+      cy.get('p').contains('Counter: 11');
+
+      cy.get('button:contains(Decrement)').click().click();
+
+      cy.get('p').contains('Counter: 9');
+
+      cy.get('button:contains(Send)').click();
+
+      expect(cy.get('@sendSpy').should('have.been.calledWith', 9));
     });
 
     it('listen to output  using autoSpyOutputs', () => {
-      cy.mount(CounterComponent);
+      cy.mount(CounterComponent, {
+        componentProperties: { initialValue: 10 },
+        autoSpyOutputs: true,
+      });
+
+      cy.get('p').contains('Counter: 10');
+
+      cy.get('button:contains(Increment)').click();
+
+      cy.get('p').contains('Counter: 11');
+
+      cy.get('button:contains(Decrement)').click().click();
+
+      cy.get('p').contains('Counter: 9');
+
+      cy.get('button:contains(Send)').click();
+
+      expect(cy.get('@sendSpy').should('have.been.calledWith', 9));
     });
   });
 });
--- apps/testing/19-input-output/src/app/counter.component.spec.ts
+++ apps/testing/19-input-output/src/app/counter.component.spec.ts
@@ -1,25 +1,79 @@
-import { render } from '@testing-library/angular';
+import { EventEmitter } from '@angular/core';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { createSpyFromClass } from 'jest-auto-spies';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
-  describe('Given an initualValue of 10', () => {
+  async function setup() {
+    const sendSpy = createSpyFromClass(EventEmitter<number>);
+    const result = await render(CounterComponent, {
+      componentProperties: {
+        initialValue: 10,
+      },
+      componentOutputs: {
+        send: sendSpy,
+      },
+    });
+
+    return { sendSpy, ...result };
+  }
+
+  describe('Given an initialValue of 10', () => {
     test('Then counterValue is 10', async () => {
-      await render(CounterComponent);
+      // Arrange
+      await setup();
+
+      // Act
+      // Assert
+      expect(screen.getByText('Counter: 10')).toBeTruthy();
     });
 
     describe('When clicking 5 times on increment button', () => {
       test('Then counterValue is 15', async () => {
-        await render(CounterComponent);
+        // Arrange
+        await setup();
+        const incrementBtn = screen.getByText('Increment');
+
+        // Act
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+        await userEvent.click(incrementBtn);
+
+        // Assert
+        expect(screen.getByText('Counter: 15')).toBeTruthy();
       });
     });
 
     describe('When clicking 2 times on decrement button', () => {
       test('Then counterValue is 8', async () => {
-        await render(CounterComponent);
+        // Arrange
+        await setup();
+        const decrementBtn = screen.getByText('Decrement');
+
+        // Act
+        await userEvent.click(decrementBtn);
+        await userEvent.click(decrementBtn);
+
+        // Assert
+        expect(screen.getByText('Counter: 8')).toBeTruthy();
       });
+
       describe('When clicking on Send ', () => {
         test('Then emitted value is 8', async () => {
-          await render(CounterComponent);
+          // Arrange
+          const { sendSpy } = await setup();
+          const decrementBtn = screen.getByText('Decrement');
+
+          // Act
+          await userEvent.click(decrementBtn);
+          await userEvent.click(decrementBtn);
+          await userEvent.click(screen.getByText('Send'));
+
+          // Assert
+          expect(sendSpy.emit).toHaveBeenCalledWith(8);
         });
       });
     });
