4cd4f0054910d43d31b6df7b3992ff9b30babbcc
Sidhartha.Verma <sidharthv91@gmail.com>
2024-05-14 15:47:50 -0300

    feat(Answer:32): good learning

--- apps/angular/32-change-detection-bug/src/app/main-navigation.component.ts
+++ apps/angular/32-change-detection-bug/src/app/main-navigation.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { map } from 'rxjs';
 import { FakeServiceService } from './fake.service';
 
 interface MenuItem {
@@ -13,7 +14,7 @@ interface MenuItem {
   standalone: true,
   imports: [RouterLink, RouterLinkActive, NgFor],
   template: `
-    <ng-container *ngFor="let menu of menus">
+    <ng-container *ngFor="let menu of menus; trackBy: identify">
       <a
         class="rounded-md border px-4 py-2"
         [routerLink]="menu.path"
@@ -35,16 +36,18 @@ interface MenuItem {
 })
 export class NavigationComponent {
   @Input() menus!: MenuItem[];
+
+  identify(index: number, item: any) {
+    return item.name;
+  }
 }
 
 @Component({
   standalone: true,
   imports: [NavigationComponent, NgIf, AsyncPipe],
   template: `
-    <ng-container *ngIf="info$ | async as info">
-      <ng-container *ngIf="info !== null; else noInfo">
-        <app-nav [menus]="getMenu(info)" />
-      </ng-container>
+    <ng-container>
+      <app-nav [menus]="(menus$ | async) ?? []" />
     </ng-container>
 
     <ng-template #noInfo>
@@ -56,7 +59,9 @@ export class NavigationComponent {
 export class MainNavigationComponent {
   private fakeBackend = inject(FakeServiceService);
 
-  readonly info$ = this.fakeBackend.getInfoFromBackend();
+  readonly menus$ = this.fakeBackend
+    .getInfoFromBackend()
+    .pipe(map((info) => this.getMenu(info ?? '')));
 
   getMenu(prop: string) {
     return [
