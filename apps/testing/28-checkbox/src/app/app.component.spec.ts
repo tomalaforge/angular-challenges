bcdaa83cb34b7bf3d03aa6e7e9386e4b40971d90
Lance Finney <lafinney@cisco.com>
2024-05-14 14:40:21 -0500

    feat(Answer:28): test tools

--- apps/testing/28-checkbox/src/app/app.component.spec.ts
+++ apps/testing/28-checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,31 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  async function setup() {
+    const user = userEvent.setup();
+    const result = await render(AppComponent);
+    return { ...result, user };
+  }
+
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
-      await render(AppComponent);
+      // Arrange
+      const { user, debugElement } = await setup();
+
+      // logRoles(debugElement.nativeElement);
+      // screen.debug(debugElement.nativeElement);
+      // screen.logTestingPlaygroundURL();
+
+      const checkbox = screen.getByRole('checkbox', { name: /agreed/i });
+
+      // Act
+      await user.click(checkbox);
+
+      // Assert
+      const button = screen.getByRole('button', { name: /submit/i });
+      expect(button).toBeEnabled();
     });
   });
 });
