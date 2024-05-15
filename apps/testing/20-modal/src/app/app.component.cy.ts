432dee9db8a123bb9f6124097351bcd526408d77
Lance Finney <lafinney@cisco.com>
2024-05-15 14:21:48 -0500

    feat(Answer:20): tests

--- apps/testing/20-modal/src/app/app.component.cy.ts
+++ apps/testing/20-modal/src/app/app.component.cy.ts
@@ -1,19 +1,41 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 describe(AppComponent.name, () => {
   const setup = () => {
-    cy.mount(AppComponent);
+    cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
   };
 
-  test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
+  test('error modal is displayed if you click on "Confirm" without inputting a name', () => {
     setup();
+
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container div').should(
+      'contain.text',
+      'You must enter a name first!!',
+    );
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', () => {
     setup();
+
+    cy.get('input').type('John Doe');
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container').contains('div', 'Name: John Doe');
+    cy.contains('button', 'Cancel').click();
+    cy.contains('div', 'Name is invalid !!');
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', () => {
     setup();
+
+    cy.get('input').type('John Doe');
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container').contains('div', 'Name: John Doe');
+    cy.contains('button', 'Confirmation').click();
+    cy.contains('div', 'Name has been submitted');
   });
 });
--- apps/testing/20-modal/src/app/app.component.spec.ts
+++ apps/testing/20-modal/src/app/app.component.spec.ts
@@ -1,16 +1,72 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+  async function setup() {
     await render(AppComponent);
+  }
+
+  test('error modal is displayed if you click on "Confirm" without inputting a name', async () => {
+    // Arrange
+    await setup();
+
+    // Act
+    await userEvent.click(screen.getByText(/confirm/i));
+
+    // Assert
+    expect(screen.getByRole('dialog')).toBeTruthy();
+    expect(screen.getByRole('heading', { name: /error/i })).toBeTruthy();
+    expect(screen.getByText(/you must enter a first!!/i)).toBeTruthy();
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    // Arrange
+    await setup();
+
+    // Act
+    await userEvent.type(
+      screen.getByRole('textbox', { name: /name/i }),
+      'John Doe',
+    );
+    await userEvent.click(screen.getByText(/confirm/i));
+
+    // Assert
+    expect(screen.getByRole('dialog')).toBeTruthy();
+    expect(screen.getByRole('heading', { name: /profil/i })).toBeTruthy();
+    expect(screen.getByText(/name: john doe/i)).toBeTruthy();
+
+    // Act
+    await userEvent.click(screen.getByText(/cancel/i));
+
+    // Assert
+    expect(screen.getByText(/name is invalid !!/i)).toBeTruthy();
+    expect(screen.queryByText(/you must enter a first!!/i)).toBeFalsy();
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    // Arrange
+    await setup();
+
+    // Act
+    await userEvent.type(
+      screen.getByRole('textbox', { name: /name/i }),
+      'John Doe',
+    );
+    await userEvent.click(screen.getByText(/confirm/i));
+
+    // Assert
+    expect(screen.getByRole('dialog')).toBeTruthy();
+    expect(screen.getByRole('heading', { name: /profil/i })).toBeTruthy();
+    expect(screen.getByText(/name: john doe/i)).toBeTruthy();
+    expect(screen.queryByText(/you must enter a first!!/i)).toBeFalsy();
+
+    // Act
+    await userEvent.click(screen.getByText(/confirmation/i));
+
+    // Assert
+    expect(screen.getByText(/name has been submitted/i)).toBeTruthy();
+    expect(screen.queryByText(/name is invalid !!/i)).toBeFalsy();
+    expect(screen.queryByText(/you must enter a first!!/i)).toBeFalsy();
   });
 });
