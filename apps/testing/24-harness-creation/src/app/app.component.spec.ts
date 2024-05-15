246f31c228f49f98e2b8dcf2bdd8dafb02e758ef
Lance Finney <lafinney@cisco.com>
2024-05-15 15:07:39 -0500

    feat(Answer:24): custom harness

--- apps/testing/24-harness-creation/src/app/app.component.spec.ts
+++ apps/testing/24-harness-creation/src/app/app.component.spec.ts
@@ -1,22 +1,60 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import { MySliderHarness } from './slider.harness';
 
 describe('AppComponent', () => {
+  async function setup() {
+    const { fixture } = await render(AppComponent);
+    return TestbedHarnessEnvironment.loader(fixture);
+  }
+
   describe('When clicking 2 times on plus button of first slider', () => {
     test('Then value is 16', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+
+      const slider = await loader.getHarness(
+        MySliderHarness.with({ minValue: 10 }),
+      );
+      await slider.clickPlus();
+      await slider.clickPlus();
+
+      expect(await slider.getValue()).toBe(16);
     });
   });
 
   describe('When clicking 1 time on plus button and two times on minus button of first slider', () => {
     test('Then value is still 10', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+
+      const slider = await loader.getHarness(
+        MySliderHarness.with({ minValue: 10 }),
+      );
+      await slider.clickPlus();
+      await slider.clickMinus();
+      await slider.clickMinus();
+
+      expect(await slider.getValue()).toBe(10);
     });
   });
 
   describe('When clicking 4 times on plus button of slider 1', () => {
     test('Then slider 2 is enabled', async () => {
-      await render(AppComponent);
+      const loader = await setup();
+
+      const firstSlider = await loader.getHarness(
+        MySliderHarness.with({ minValue: 10 }),
+      );
+      const secondSlider = await loader.getHarness(
+        MySliderHarness.with({ minValue: 0 }),
+      );
+      expect(await secondSlider.disabled()).toBe(true);
+      await firstSlider.clickPlus();
+      await firstSlider.clickPlus();
+      await firstSlider.clickPlus();
+      await firstSlider.clickPlus();
+
+      expect(await secondSlider.disabled()).toBe(false);
     });
   });
 });
--- apps/testing/24-harness-creation/src/app/slider.harness.ts
+++ apps/testing/24-harness-creation/src/app/slider.harness.ts
@@ -1,5 +1,65 @@
-import { ComponentHarness } from '@angular/cdk/testing';
+import {
+  BaseHarnessFilters,
+  ComponentHarness,
+  ComponentHarnessConstructor,
+  HarnessPredicate,
+} from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatSliderHarness } from '@angular/material/slider/testing';
 
 export class MySliderHarness extends ComponentHarness {
   static hostSelector = 'app-slider';
+
+  static with<T extends MySliderHarness>(
+    this: ComponentHarnessConstructor<T>,
+    options: BaseHarnessFilters & { minValue?: number } = {},
+  ): HarnessPredicate<T> {
+    return new HarnessPredicate(this, options).addOption(
+      'minValue',
+      options.minValue,
+      async (harness, minValue) => {
+        const value = await harness.getMinValue();
+        return value === minValue;
+      },
+    );
+  }
+
+  async clickPlus(): Promise<void> {
+    const plusButton = this.locatorFor(
+      MatButtonHarness.with({ text: 'arrow_forward_ios' }),
+    );
+    return (await plusButton()).click();
+  }
+
+  async clickMinus(): Promise<void> {
+    const minusButton = this.locatorFor(
+      MatButtonHarness.with({ text: 'arrow_back_ios' }),
+    );
+    return (await minusButton()).click();
+  }
+
+  async getValue(): Promise<number> {
+    return (await this.getEndThumb()).getValue();
+  }
+
+  async getMinValue(): Promise<number> {
+    return (await this.getEndThumb()).getMinValue();
+  }
+
+  async disabled(): Promise<boolean> {
+    return (await this.getSlider()).isDisabled();
+  }
+
+  async setValue(value: number): Promise<void> {
+    return (await this.getEndThumb()).setValue(value);
+  }
+
+  private async getSlider() {
+    return await this.locatorFor(MatSliderHarness)();
+  }
+
+  private async getEndThumb() {
+    const slider = await this.getSlider();
+    return await slider.getEndThumb();
+  }
 }
