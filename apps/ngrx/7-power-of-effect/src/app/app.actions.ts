661fe0dca3738e0900eb562caeed0b26627499c4
Lance Finney <lafinney@cisco.com>
2024-05-21 08:22:31 -0500

    feat(Answer:7): pr tweaks
    Change one effect with a switch statement to four effects

--- apps/ngrx/7-power-of-effect/src/app/app.actions.ts
+++ apps/ngrx/7-power-of-effect/src/app/app.actions.ts
@@ -1,9 +1,16 @@
-import { createActionGroup, emptyProps } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
 // This is the global actions.
 export const appActions = createActionGroup({
   source: 'App Component',
   events: {
     'Init App': emptyProps(),
+  },
+});
+
+export const appApiActions = createActionGroup({
+  source: 'App API',
+  events: {
+    Alert: props<{ message: string }>(),
   },
 });
--- apps/ngrx/7-power-of-effect/src/app/app.effects.ts
+++ apps/ngrx/7-power-of-effect/src/app/app.effects.ts
@@ -2,51 +2,63 @@ import { inject, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map, tap } from 'rxjs';
+import { appApiActions } from './app.actions';
 import { schoolActions } from './school/school.actions';
 import { studentActions } from './student/store/student.actions';
 import { teacherActions } from './teacher/store/teacher.actions';
-
-const actionMap = new Map<string, string>();
-actionMap.set(schoolActions.addOneSchool.type, 'School added');
-actionMap.set(studentActions.addOneStudent.type, 'Student added');
-actionMap.set(teacherActions.addOneTeacher.type, 'Teacher added');
 
 @Injectable()
 export class AppEffects {
   private actions$ = inject(Actions);
   private snackbar = inject(MatSnackBar);
 
+  schoolUpdated$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(schoolActions.addOneSchool),
+      map((action) => {
+        if (action.school.version === 0) {
+          return `School added: ${action.school.name}`;
+        } else {
+          return `School updated: ${action.school.name}`;
+        }
+      }),
+      map((message) => appApiActions.alert({ message })),
+    ),
+  );
+
+  studentUpdated$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(studentActions.addOneStudent),
+      map((action) => {
+        if (action.student.version === 0) {
+          return `Student added: ${action.student.firstname} ${action.student.lastname}`;
+        } else {
+          return `Student updated: ${action.student.firstname} ${action.student.lastname}`;
+        }
+      }),
+      map((message) => appApiActions.alert({ message })),
+    ),
+  );
+
+  teacherUpdated$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(teacherActions.addOneTeacher),
+      map((action) => {
+        if (action.teacher.version === 0) {
+          return `Teacher added: ${action.teacher.firstname} ${action.teacher.lastname}`;
+        } else {
+          return `Teacher updated: ${action.teacher.firstname} ${action.teacher.lastname}`;
+        }
+      }),
+      map((message) => appApiActions.alert({ message })),
+    ),
+  );
+
   displayAlerts$ = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(
-          schoolActions.addOneSchool,
-          studentActions.addOneStudent,
-          teacherActions.addOneTeacher,
-        ),
-        map((action) => {
-          switch (action.type) {
-            case schoolActions.addOneSchool.type:
-              if (action.school.version === 0) {
-                return `School added: ${action.school.name}`;
-              } else {
-                return `School updated: ${action.school.name}`;
-              }
-            case studentActions.addOneStudent.type:
-              if (action.student.version === 0) {
-                return `Student added: ${action.student.firstname} ${action.student.lastname}`;
-              } else {
-                return `Student updated: ${action.student.firstname} ${action.student.lastname}`;
-              }
-            default:
-              if (action.teacher.version === 0) {
-                return `Teacher added: ${action.teacher.firstname} ${action.teacher.lastname}`;
-              } else {
-                return `Teacher updated: ${action.teacher.firstname} ${action.teacher.lastname}`;
-              }
-          }
-        }),
-        tap((message) =>
+        ofType(appApiActions.alert),
+        tap(({ message }) =>
           this.snackbar.open(message, 'close', {
             duration: 500,
           }),
