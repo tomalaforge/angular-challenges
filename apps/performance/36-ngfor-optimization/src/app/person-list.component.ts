04948af0864f53097402cb633213ecc66888b4d3
Sidhartha.Verma <sidharthv91@gmail.com>
2024-05-21 15:54:51 -0300

    feat: answer: 36

--- apps/performance/36-ngfor-optimization/src/app/person-list.component.ts
+++ apps/performance/36-ngfor-optimization/src/app/person-list.component.ts
@@ -9,7 +9,7 @@ import { Person } from './person.model';
   imports: [CommonModule],
   template: `
     <div
-      *ngFor="let person of persons"
+      *ngFor="let person of persons; trackBy: email"
       class="flex items-center justify-between border-b">
       <h3>{{ person.name }}</h3>
       <div class="flex gap-10 py-1">
@@ -34,4 +34,8 @@ export class PersonListComponent {
   @Input() persons: Person[] = [];
   @Output() delete = new EventEmitter<string>();
   @Output() update = new EventEmitter<string>();
+
+  public email(index: number, item: Person): string {
+    return item.email;
+  }
 }
