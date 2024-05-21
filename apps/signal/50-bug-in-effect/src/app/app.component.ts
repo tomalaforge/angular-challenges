0e7275262797e788cb2b6ca3cf366c0e6505b3fd
thomas <toma.laforge@gmail.com>
2024-05-21 16:40:00 +0200

    feat: solution of challenge 50

--- apps/signal/50-bug-in-effect/src/app/app.component.ts
+++ apps/signal/50-bug-in-effect/src/app/app.component.ts
@@ -41,11 +41,20 @@ export class AppComponent {
   gpu = model(false);
 
   constructor() {
-    /* 
-      Explain for your junior team mate why this bug occurs ...
-    */
     effect(() => {
-      if (this.drive() || this.ram() || this.gpu()) {
+      if (this.drive()) {
+        alert('Price increased!');
+      }
+    });
+
+    effect(() => {
+      if (this.ram()) {
+        alert('Price increased!');
+      }
+    });
+
+    effect(() => {
+      if (this.gpu()) {
         alert('Price increased!');
       }
     });
