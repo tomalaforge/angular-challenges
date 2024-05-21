94036f62ad40d049ad85f8060d67cee23eeee7dd
jdegand <70610011+jdegand@users.noreply.github.com>
2024-05-20 23:25:44 -0400

    fix: function-call-effect

--- apps/signal/51-function-call-effect/src/app/user.service.ts
+++ apps/signal/51-function-call-effect/src/app/user.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, untracked } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  name = signal('Thomas');
+  name = untracked(signal('Thomas'));
+
+  // name is considered a dependency by Angular
 
   log(message: string) {
-    console.log(`${this.name()}: ${message}`);
+    console.log(`${this.name}: ${message}`);
   }
 }
+
+// https://blog.angular-university.io/angular-signals/
+// https://marmicode.io/blog/angular-signals-and-custom-render-strategies
+
+// This doesn't seem correct but it appears to work fine
+// I envisioned using an effect or using the untracked inside log function
