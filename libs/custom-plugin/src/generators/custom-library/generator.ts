beb96cec2d7a7541081add4a93843aa90c45b9a4
Lance Finney <lafinney@cisco.com>
2024-05-22 13:38:52 -0500

    feat(Answer:25): extended lib
    I was able to copy the schema files from node_modules/@nx/angular/src/generators/library/
    I wouldn't have completed this without the blog post:
    https://medium.com/ngconf/extending-an-existing-nx-generator-4ae0e7b53484

--- libs/custom-plugin/src/generators/custom-library/generator.ts
+++ libs/custom-plugin/src/generators/custom-library/generator.ts
@@ -1,24 +1,57 @@
+import { libraryGenerator, UnitTestRunner } from '@nx/angular/generators';
 import {
-  addProjectConfiguration,
   formatFiles,
   generateFiles,
+  joinPathFragments,
+  names,
+  offsetFromRoot,
+  readProjectConfiguration,
   Tree,
+  updateJson,
 } from '@nx/devkit';
-import * as path from 'path';
+import { Linter } from '@nx/eslint';
+import { join } from 'path';
 import { CustomLibraryGeneratorSchema } from './schema';
 
 export async function customLibraryGenerator(
   tree: Tree,
   options: CustomLibraryGeneratorSchema,
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await libraryGenerator(tree, options);
+  const { fileName } = names(options.name);
+
+  // I really needed https://medium.com/ngconf/extending-an-existing-nx-generator-4ae0e7b53484
+  // to do this.
+
+  // UnitTestRunner.Jest is the default
+  if (options.unitTestRunner === UnitTestRunner.Jest) {
+    const projectConfig = readProjectConfiguration(tree, fileName);
+
+    tree.delete(join(projectConfig.root, './jest-config.ts'));
+
+    generateFiles(
+      tree,
+      joinPathFragments(__dirname, 'jest'),
+      projectConfig.root,
+      {
+        tmpl: '',
+        project: fileName,
+        offsetFromRoot: offsetFromRoot(projectConfig.root),
+      },
+    );
+  }
+
+  // Linter.EsLint is the default
+  if (options.linter === Linter.EsLint) {
+    updateJson(tree, `libs/${fileName}/.eslintrc.json`, (json) => {
+      json.overrides[0].rules = {
+        ...json.overrides[0].rules,
+        '@typescript-eslint/member-ordering': 'off',
+      };
+      return json;
+    });
+  }
+
   await formatFiles(tree);
 }
 
--- libs/custom-plugin/src/generators/custom-library/schema.d.ts
+++ libs/custom-plugin/src/generators/custom-library/schema.d.ts
@@ -1,3 +1,45 @@
+import type { UnitTestRunner } from '@nx/angular/src/utils/test-runners';
+import type { ProjectNameAndRootFormat } from '@nx/devkit/src/generators/project-name-and-root-utils';
+import type { Linter } from '@nx/eslint';
+
+// copied from node_modules/@nx/angular/src/generators/library/schema.d.ts
 export interface CustomLibraryGeneratorSchema {
   name: string;
+  addTailwind?: boolean;
+  skipFormat?: boolean;
+  simpleName?: boolean;
+  addModuleSpec?: boolean;
+  directory?: string;
+  projectNameAndRootFormat?: ProjectNameAndRootFormat;
+  sourceDir?: string;
+  buildable?: boolean;
+  publishable?: boolean;
+  importPath?: string;
+  standaloneConfig?: boolean;
+  spec?: boolean;
+  flat?: boolean;
+  commonModule?: boolean;
+  prefix?: string;
+  routing?: boolean;
+  lazy?: boolean;
+  parent?: string;
+  tags?: string;
+  strict?: boolean;
+  linter?: Linter;
+  unitTestRunner?: UnitTestRunner;
+  compilationMode?: 'full' | 'partial';
+  setParserOptionsProject?: boolean;
+  skipModule?: boolean;
+  skipPackageJson?: boolean;
+  standalone?: boolean;
+  displayBlock?: boolean;
+  inlineStyle?: boolean;
+  inlineTemplate?: boolean;
+  viewEncapsulation?: 'Emulated' | 'None' | 'ShadowDom';
+  changeDetection?: 'Default' | 'OnPush';
+  style?: 'css' | 'scss' | 'sass' | 'less' | 'none';
+  skipTests?: boolean;
+  selector?: string;
+  skipSelector?: boolean;
+  addPlugin?: boolean;
 }
