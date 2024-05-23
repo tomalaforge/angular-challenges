8b3050af07e9bed219144d22409f6ce936eba46d
Lance Finney <lafinney@cisco.com>
2024-05-23 08:18:51 -0500

    feat: revert to NgModule
    Copy of 31

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone: true,
-  imports: [],
   selector: 'app-root',
-  template: ``,
-  styles: [''],
+  template: `
+    <div class="flex gap-2">
+      <button
+        routerLink="home"
+        class="rounded-md border border-blue-400 px-4 py-2">
+        Home
+      </button>
+      <button
+        routerLink="admin"
+        class="rounded-md border border-blue-400 px-4 py-2">
+        Admin
+      </button>
+      <button
+        routerLink="user"
+        class="rounded-md border border-blue-400 px-4 py-2">
+        User
+      </button>
+    </div>
+    <router-outlet></router-outlet>
+  `,
+  host: {
+    class: 'flex flex-col p-4 gap-3',
+  },
 })
 export class AppComponent {}
--- apps/angular/52-lazy-load-component/src/main.ts
+++ apps/angular/52-lazy-load-component/src/main.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
-);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
