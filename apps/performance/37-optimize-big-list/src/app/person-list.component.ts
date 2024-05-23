268476986b430c949b0d58185863f78c3ff9413f
AzureAD\IoannisTsironis <i.tsironis@epg-financials.com>
2024-05-23 15:36:28 +0300

    feat: fix angular performance optimize big list challenge

--- apps/performance/37-optimize-big-list/src/app/person-list.component.ts
+++ apps/performance/37-optimize-big-list/src/app/person-list.component.ts
@@ -1,23 +1,27 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { NgForTrackByModule } from '@angular-challenges/shared/directives';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule, NgForTrackByModule],
+  imports: [CommonModule, ScrollingModule],
   template: `
     <div class="relative h-[300px] overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
+      <cdk-virtual-scroll-viewport
+        class="h-full"
+        itemSize="50"
+        minBufferPx="0"
+        maxBufferPx="50">
         <div
-          *ngFor="let person of persons; trackByProp: 'email'"
-          class="flex h-9 items-center justify-between border-b">
+          *cdkVirtualFor="let person of persons; trackBy: trackByFn"
+          class="flex h-[50px] items-center justify-between border-b">
           <h3>{{ person.name }}</h3>
           <p>{{ person.email }}</p>
         </div>
-      </div>
+      </cdk-virtual-scroll-viewport>
     </div>
   `,
   host: {
@@ -27,4 +31,8 @@ import { Person } from './person.model';
 })
 export class PersonListComponent {
   @Input() persons: Person[] = [];
+
+  protected trackByFn(_index: number, item: Person) {
+    return item.email;
+  }
 }
