9f03d3fac9d3b9bfde2ad1c31b01ff4c83d3ea27
AzureAD\IoannisTsironis <i.tsironis@epg-financials.com>
2024-05-23 12:08:37 +0300

    feat: fix angular performance ngfor optimization challenge

--- apps/performance/36-ngfor-optimization/src/app/person-list.component.ts
+++ apps/performance/36-ngfor-optimization/src/app/person-list.component.ts
@@ -8,23 +8,23 @@ import { Person } from './person.model';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div
-      *ngFor="let person of persons"
-      class="flex items-center justify-between border-b">
-      <h3>{{ person.name }}</h3>
-      <div class="flex gap-10 py-1">
-        <button
-          class="rounded-md border bg-blue-500 p-2 text-white"
-          (click)="update.emit(person.email)">
-          UPDATE
-        </button>
-        <button
-          class="rounded-md border bg-red-500 p-2 text-white"
-          (click)="delete.emit(person.email)">
-          DELETE
-        </button>
+    @for (person of persons; track $index) {
+      <div class="flex items-center justify-between border-b">
+        <h3>{{ person.name }}</h3>
+        <div class="flex gap-10 py-1">
+          <button
+            class="rounded-md border bg-blue-500 p-2 text-white"
+            (click)="update.emit(person.email)">
+            UPDATE
+          </button>
+          <button
+            class="rounded-md border bg-red-500 p-2 text-white"
+            (click)="delete.emit(person.email)">
+            DELETE
+          </button>
+        </div>
       </div>
-    </div>
+    }
   `,
   host: {
     class: 'w-full flex flex-col',
