1a7640749f50d5796b20a06992e98c1ef5bbba21
AzureAD\IoannisTsironis <i.tsironis@epg-financials.com>
2024-05-27 13:33:49 +0300

    feat: fix angular testing checkbox challenge

--- apps/testing/28-checkbox/src/app/app.component.spec.ts
+++ apps/testing/28-checkbox/src/app/app.component.spec.ts
@@ -1,7 +1,26 @@
-import { render } from '@testing-library/angular';
+import { prettyDOM, render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  describe('On init', () => {
+    it('The toggle checkbox should be in the DOM', async () => {
+      await render(AppComponent);
+
+      screen.debug();
+    });
+
+    it('test debug function with an element', async () => {
+      await render(AppComponent);
+
+      screen.debug(screen.getByText('Agreed'));
+    });
+
+    it('test prettyDOM log', () => {
+      const div = document.createElement('div');
+      div.innerHTML = '<div><h1>Hello World</h1></div>';
+      console.log(prettyDOM(div));
+    });
+  });
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
       await render(AppComponent);
