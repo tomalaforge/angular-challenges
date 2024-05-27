1f536327dd31bba189c560a6e586326ffab07e64
Sidhartha.Verma <sidharthv91@gmail.com>
2024-05-27 15:00:14 -0300

    feat(Answer: 14): pr review comments

--- apps/rxjs/14-race-condition/src/app/app.component.ts
+++ apps/rxjs/14-race-condition/src/app/app.component.ts
@@ -1,29 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { MatDialog } from '@angular/material/dialog';
 import { TopicModalComponent } from './topic-dialog.component';
-import { TopicService, TopicType } from './topic.service';
+import { TopicService } from './topic.service';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [CommonModule],
   template: `
-    <ng-container *ngIf="topics$ | async as topics">
-      <button (click)="openTopicModal(topics)">Open Topic</button>
-    </ng-container>
+    @if (topics()) {
+      <button (click)="openTopicModal()">Open Topic</button>
+    } @else {
+      ...Wait for data
+    }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'rxjs-race-condition';
   dialog = inject(MatDialog);
-  topics$ = inject(TopicService).fakeGetHttpTopic();
+  topics = toSignal(inject(TopicService).fakeGetHttpTopic());
 
-  openTopicModal(topics: TopicType[]) {
+  openTopicModal() {
     this.dialog.open(TopicModalComponent, {
       data: {
-        topics: topics,
+        topics: this.topics(),
       },
     });
   }
