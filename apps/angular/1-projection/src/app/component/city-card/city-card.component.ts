ff868051728e7449f3b12fe4118465910560a36c
Jairo Valderrama <jairovg@gmail.com>
2024-06-01 18:02:58 -0500

    fix: add city card

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
+import { RowItemDirective } from '../../ui/card/row-item.directive';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="cities()"
+      class="bg-light-yellow"
+      (addItem)="onAddNewItem()">
+      <img src="assets/img/city.png" width="200px" />
+      <ng-template [rowItem]="cities()" let-item>
+        <app-list-item
+          [name]="item.name"
+          (deleteItem)="onDeleteItem(item.id)"></app-list-item>
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  styles: [
+    `
+      .bg-light-yellow {
+        background-color: rgba(250, 250, 0, 0.1);
+      }
+    `,
+  ],
+  imports: [CardComponent, RowItemDirective, ListItemComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
 
-  ngOnInit(): void {}
+  cities = this.store.cities;
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  onAddNewItem() {
+    this.store.addOne(randomCity());
+  }
+
+  onDeleteItem(id: number) {
+    this.store.deleteOne(id);
+  }
 }
