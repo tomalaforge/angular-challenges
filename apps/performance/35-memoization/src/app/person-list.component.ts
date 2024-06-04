af5185678f3474ec5b6bb0c6cf9b9c964d2cfe3f
Jairo Valderrama <jairovg@gmail.com>
2024-06-03 20:45:24 -0500

    refactor: use built-in flow controls and use input function instead of decorator

--- apps/performance/35-memoization/src/app/person-list.component.ts
+++ apps/performance/35-memoization/src/app/person-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,7 +19,7 @@ const fibonacci = (num: number): number => {
   selector: 'app-person-list',
   standalone: true,
   imports: [
-    CommonModule,
+    TitleCasePipe,
     FormsModule,
     MatListModule,
     MatFormFieldModule,
@@ -28,7 +28,7 @@ const fibonacci = (num: number): number => {
   ],
   template: `
     <h1 class="text-center font-semibold" title="Title">
-      {{ title | titlecase }}
+      {{ title() | titlecase }}
     </h1>
 
     <mat-form-field class="w-4/5">
@@ -40,12 +40,14 @@ const fibonacci = (num: number): number => {
     </mat-form-field>
 
     <mat-list class="flex w-full">
-      <mat-list-item *ngFor="let person of persons">
-        <div MatListItemLine class="flex justify-between">
-          <h3>{{ person.name }}</h3>
-          <mat-chip>{{ calculate(person.fib) }}</mat-chip>
-        </div>
-      </mat-list-item>
+      @for (person of persons(); track $index) {
+        <mat-list-item>
+          <div MatListItemLine class="flex justify-between">
+            <h3>{{ person.name }}</h3>
+            <mat-chip>{{ calculate(person.fib) }}</mat-chip>
+          </div>
+        </mat-list-item>
+      }
     </mat-list>
   `,
   host: {
@@ -53,8 +55,8 @@ const fibonacci = (num: number): number => {
   },
 })
 export class PersonListComponent {
-  @Input() persons: Person[] = [];
-  @Input() title = '';
+  persons = input<Person[]>([]);
+  title = input<string>('');
 
   label = '';
 
