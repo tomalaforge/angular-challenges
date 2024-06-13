abaa3f9402ca4a3fc03b1dd9b7bbc1409153d384
szymonhel <szhelmecki@gmail.com>
2024-06-13 21:37:06 +0200

    feat(answer:41): control value accessor

--- apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
+++ apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
@@ -1,12 +1,53 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Output } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-rating-control',
   templateUrl: 'rating-control.component.html',
   styleUrls: ['rating-control.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RatingControlComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => RatingControlComponent),
+      multi: true,
+    },
+  ],
 })
-export class RatingControlComponent {
+export class RatingControlComponent implements ControlValueAccessor, Validator {
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    if (!control.value) {
+      return { required: 'The value is required' };
+    }
+    return null;
+  }
+  registerOnValidatorChange?(fn: () => void): void {}
+
+  private onChange!: (value: any) => void;
+  private onTouched!: (value: any) => void;
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {}
   @Output()
   readonly ratingUpdated: EventEmitter<string> = new EventEmitter<string>();
 
@@ -14,6 +55,7 @@ export class RatingControlComponent {
 
   setRating(index: number): void {
     this.value = index + 1;
+    this.onChange(this.value);
     this.ratingUpdated.emit(`${this.value}`);
   }
 
