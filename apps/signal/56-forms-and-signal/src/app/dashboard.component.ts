ddb5036af0a43b7744a075923bd63b9eab7e06c4
Laforge Thomas <30832608+tomalaforge@users.noreply.github.com>
2024-07-08 17:11:41 +0200

    Challenge 56 multi step reactive form and signals (#1030)
     feat: challenge 55 form and signal

---
+++ apps/signal/56-forms-and-signal/src/app/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { products } from './products';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterLink],
+  template: `
+    <h2 class="mb-5 w-full bg-gray-400 p-2 text-white">List of Products</h2>
+    <ul class="w-full *:border-b *:border-l *:border-r *:p-4">
+      @for (product of products; track product.id) {
+        <li [class.border-t]="$first">
+          <div class="flex w-full justify-between">
+            {{ product.name }} ({{ product.price }}€)
+            <button
+              class="w-20 rounded-full border bg-blue-500 p-2 text-white"
+              routerLink="/order"
+              [queryParams]="{ productId: product.id }">
+              Buy
+            </button>
+          </div>
+        </li>
+      }
+    </ul>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DashboardComponent {
+  products = products;
+}
---
+++ apps/signal/56-forms-and-signal/src/app/order.component.ts
@@ -0,0 +1,72 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { products } from './products';
+
+@Component({
+  selector: 'app-order',
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
+  template: `
+    <h2 class="mb-5 w-full bg-gray-400 p-2 text-white">Order</h2>
+    <section class="flex flex-col gap-5">
+      <form class="flex items-center justify-between gap-5" [formGroup]="form">
+        <label for="countries" class="mb-2 block text-nowrap text-gray-900">
+          Select a quantity
+        </label>
+        <select
+          formControlName="quantity"
+          id="countries"
+          class="block w-32 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </form>
+      <div class="flex justify-between">
+        <div>SubTotal</div>
+        <div>{{ totalWihoutVat() }} €</div>
+      </div>
+      <div class="flex justify-between">
+        <div>VAT (21%)</div>
+        <div>{{ vat() }} €</div>
+      </div>
+      <div class="flex justify-between">
+        <div>Total</div>
+        <div>{{ total() }} €</div>
+      </div>
+      <button
+        routerLink="/checkout"
+        [queryParams]="{ quantity: quantity() }"
+        queryParamsHandling="merge"
+        class="w-full rounded-full border bg-blue-500 p-2 text-white">
+        Checkout
+      </button>
+    </section>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class OrderComponent {
+  form = new FormGroup({
+    quantity: new FormControl(1, { nonNullable: true }),
+  });
+
+  productId = input('1');
+  price = computed(
+    () => products.find((p) => p.id === this.productId())?.price ?? 0,
+  );
+  quantity = toSignal(this.form.controls.quantity.valueChanges, {
+    initialValue: this.form.getRawValue().quantity,
+  });
+  totalWihoutVat = computed(() => Number(this.price()) * this.quantity());
+  vat = computed(() => this.totalWihoutVat() * 0.21);
+  total = computed(() => this.totalWihoutVat() + this.vat());
+}
