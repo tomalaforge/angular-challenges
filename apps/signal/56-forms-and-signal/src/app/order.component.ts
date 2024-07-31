c36aba5b900e3e8e80a0a1e805d7b881debb2472
Vishnu Prasad <vpsvprasad8@gmail.com>
2024-07-31 13:59:41 +0000

    Answer: 56 - forms and signal

--- apps/signal/56-forms-and-signal/src/app/order.component.ts
+++ apps/signal/56-forms-and-signal/src/app/order.component.ts
@@ -3,25 +3,27 @@ import {
   Component,
   computed,
   input,
+  model,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { products } from './products';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, FormsModule],
   template: `
     <h2 class="mb-5 w-full bg-gray-400 p-2 text-white">Order</h2>
     <section class="flex flex-col gap-5">
-      <form class="flex items-center justify-between gap-5" [formGroup]="form">
+      <form class="flex items-center justify-between gap-5" >
         <label for="countries" class="mb-2 block text-nowrap text-gray-900">
           Select a quantity
         </label>
         <select
-          formControlName="quantity"
+          name="quantity"
+          [(ngModel)]="quantity"
           id="countries"
           class="block w-32 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500">
           <option value="1">1</option>
@@ -55,17 +57,15 @@ import { products } from './products';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrderComponent {
-  form = new FormGroup({
-    quantity: new FormControl(1, { nonNullable: true }),
-  });
 
+  quantity = model<number>(1);
   productId = input('1');
   price = computed(
     () => products.find((p) => p.id === this.productId())?.price ?? 0,
   );
-  quantity = toSignal(this.form.controls.quantity.valueChanges, {
-    initialValue: this.form.getRawValue().quantity,
-  });
+  // quantity = toSignal(this.form.controls.quantity.valueChanges, {
+  //   initialValue: this.form.getRawValue().quantity,
+  // });
   totalWihoutVat = computed(() => Number(this.price()) * this.quantity());
   vat = computed(() => this.totalWihoutVat() * 0.21);
   total = computed(() => this.totalWihoutVat() + this.vat());
