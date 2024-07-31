6924887de469047519b321e9d0c719965386f624
Alejandro Moreno <alejandro.moreno@endava.com>
2024-07-31 15:26:36 -0500

    challengue 21: anchor navigation

--- apps/angular/21-anchor-navigation/src/app/foo.component.ts
+++ apps/angular/21-anchor-navigation/src/app/foo.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterLink],
   selector: 'app-foo',
   template: `
     Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
+    <nav-button routerLink="/home" class="fixed left-1/2 top-3">
+      Home Page
+    </nav-button>
     <div class="h-screen bg-blue-200">section 1</div>
     <div class="h-screen bg-red-200">section 2</div>
   `,
