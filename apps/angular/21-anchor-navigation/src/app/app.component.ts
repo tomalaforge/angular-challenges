6c1597c926a5da8e2f87c7d881cc8d53cc9ea50e
hoanghiep <hiepbh@kaopiz.com>
2024-08-02 11:33:34 +0700

    challenges anchor-navigation

--- apps/angular/21-anchor-navigation/src/app/app.component.ts
+++ apps/angular/21-anchor-navigation/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive],
   selector: 'app-root',
   template: `
     <router-outlet></router-outlet>
