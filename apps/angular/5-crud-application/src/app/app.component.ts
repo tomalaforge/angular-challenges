784a1321f2dd5a4dfcc8d6386bdb5a4b831ac023
Alejandro Moreno <alejandro.moreno@endava.com>
2024-08-08 09:42:16 -0500

    challengue 5

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,51 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { randText } from '@ngneat/falso';
+import { Component, OnInit, Signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoaderService } from './loader.service';
+import { Todo, TodoService } from './todo.service';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   selector: 'app-root',
   template: `
-    <div *ngFor="let todo of todos">
-      {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
-    </div>
+    @if (loader()) {
+      <mat-spinner *ngIf="loader()"></mat-spinner>
+    } @else {
+      @for (todo of todos(); track todo.id) {
+        <div>
+          {{ todo.title }}
+          <button (click)="update(todo)">Update</button>
+          <button (click)="delete(todo.id)">Delete</button>
+        </div>
+      } @empty {
+        <div>No elements on Todo</div>
+      }
+    }
   `,
   styles: [],
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
+  public todos: Signal<Todo[]> = this.todoService.todos;
+  public loader: Signal<boolean> = this.loaderService.loader;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private todoService: TodoService,
+    private loaderService: LoaderService,
+  ) {}
 
   ngOnInit(): void {
-    this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
-      .subscribe((todos) => {
-        this.todos = todos;
-      });
+    this.todoService.getTodos();
   }
 
-  update(todo: any) {
-    this.http
-      .put<any>(
-        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-        JSON.stringify({
-          todo: todo.id,
-          title: randText(),
-          body: todo.body,
-          userId: todo.userId,
-        }),
-        {
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        },
-      )
-      .subscribe((todoUpdated: any) => {
-        this.todos[todoUpdated.id - 1] = todoUpdated;
-      });
+  update(todo: Todo) {
+    this.todoService.update(todo);
+  }
+
+  delete(index: number) {
+    this.todoService.delete(index);
   }
 }
--- apps/angular/5-crud-application/src/app/app.config.ts
+++ apps/angular/5-crud-application/src/app/app.config.ts
@@ -1,6 +1,17 @@
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-
+import { ErrorInterceptor } from './error.interceptor';
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()), // move to withInterceptors: HttpInterceptorFn
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
 };
