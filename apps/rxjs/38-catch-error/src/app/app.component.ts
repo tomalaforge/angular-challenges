9a3e623a6fe9e16ac7b68a4feea3e5b0040e9db9
hoanghiep <hiepbh@kaopiz.com>
2024-08-22 14:17:38 +0700

    catch-error

--- apps/rxjs/38-catch-error/src/app/app.component.ts
+++ apps/rxjs/38-catch-error/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { Subject, concatMap, map } from 'rxjs';
+import { Subject, catchError, concatMap, map, of } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -42,7 +42,9 @@ export class AppComponent implements OnInit {
       .pipe(
         map(() => this.input),
         concatMap((value) =>
-          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`),
+          this.http
+            .get(`https://jsonplaceholder.typicode.com/${value}/1`)
+            .pipe(catchError((err) => of(err))),
         ),
         takeUntilDestroyed(this.destroyRef),
       )
@@ -50,10 +52,6 @@ export class AppComponent implements OnInit {
         next: (value) => {
           console.log(value);
           this.response = value;
-        },
-        error: (error) => {
-          console.log(error);
-          this.response = error;
         },
         complete: () => console.log('done'),
       });
