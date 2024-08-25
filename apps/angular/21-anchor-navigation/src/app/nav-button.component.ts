33fcd768114bc5e89e965ea07064fb08a2cc655d
Fabien MACIP <fabien.macip@gmail.com>
2024-08-25 15:58:13 +0200

    feat: routerLink & anchor scrolling

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="[routerLink]" [fragment]="fragment">
       <ng-content></ng-content>
     </a>
   `,
+  imports: [RouterLinkWithHref],
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
 })
 export class NavButtonComponent {
-  @Input() href = '';
+  @Input() routerLink?: string = '';
+  @Input() fragment?: string = undefined;
 }
