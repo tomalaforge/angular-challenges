7348d00e0008d0499876c2dd8148850be94e2d22
Wamula Bashir Saidi <wamulabash1@gmail.com>
2024-09-01 21:35:17 +0300

    worked on the highlite css

--- apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
@@ -9,25 +9,25 @@ export type StaticTextType = 'normal' | 'warning' | 'error';
   standalone: true,
   imports: [TextComponent],
   template: `
-    <text [font]="font" [color]="color">This is a static text</text>
+    <text [ngClass]="typeClass">This is a static text</text>
   `,
-})
-export class TextStaticComponent {
-  @Input() set type(type: StaticTextType) {
-    switch (type) {
-      case 'error': {
-        this.font = 30;
-        this.color = 'red';
-        break;
+   styles: [
+    `
+      .normal {
+        font-size: 10px;
+        color: black;
       }
-      case 'warning': {
-        this.font = 25;
-        this.color = 'orange';
-        break;
-      }
-    }
-  }
 
-  font = 10;
-  color = 'black';
-}
+      .error {
+        font-size: 30px;
+        color: red;
+      }
+
+      .warning {
+        font-size: 25px;
+        color: orange;
+      }
+    `,
+  ],
+})
+export class TextStaticComponent {}
--- apps/angular/13-highly-customizable-css/src/app/text.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/text.component.ts
@@ -5,12 +5,15 @@ import { Component, Input } from '@angular/core';
   selector: 'text',
   standalone: true,
   template: `
-    <p style="font-size: {{ font }}px; color: {{ color }}">
-      <ng-content></ng-content>
-    </p>
+     <p class="text-content"><ng-content></ng-content></p>
   `,
+    styles: [
+      `
+        .text-content {
+          font-size: var(--font-size, 10px);
+          color: var(--color, black);
+        }
+      `,
+    ],
 })
-export class TextComponent {
-  @Input() font = 10;
-  @Input() color = 'black';
-}
+export class TextComponent {}
