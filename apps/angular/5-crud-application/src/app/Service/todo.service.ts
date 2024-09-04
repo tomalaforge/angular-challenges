9f0c743bb56f71332af00f440e047d3db337e311
Ambinintsoa <amr@alternateeve.com>
2024-09-04 17:28:25 +0300

    feat: add delete button on todoList and handle request error

--- apps/angular/5-crud-application/src/app/Service/todo.service.ts
+++ apps/angular/5-crud-application/src/app/Service/todo.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { randText } from '@ngneat/falso';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { Todo } from '../Model/todo';
 
 @Injectable({
@@ -11,23 +11,49 @@ export class TodoService {
   http = inject(HttpClient);
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
+    return this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          throw 'GET request failed. Details: ' + error.message;
+        }),
+      );
   }
 
   updateTodo(todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>(
-      `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-      JSON.stringify({
-        todo: todo.id,
-        title: randText(),
-        completed: todo.completed,
-        userId: todo.userId,
-      }),
-      {
+    return this.http
+      .put<Todo>(
+        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+        JSON.stringify({
+          todo: todo.id,
+          title: randText(),
+          completed: todo.completed,
+          userId: todo.userId,
+        }),
+        {
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        },
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          throw 'PUT request failed. Details: ' + error.message;
+        }),
+      );
+  }
+
+  deleteTodo(todo: Todo): Observable<void> {
+    return this.http
+      .delete<void>(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      },
-    );
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          throw 'DELETE request failed. Details: ' + error.message;
+        }),
+      );
   }
 }
--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -8,10 +8,15 @@ import { TodoService } from './Service/todo.service';
   imports: [CommonModule],
   selector: 'app-root',
   template: `
-    <div *ngFor="let todo of todos()">
-      {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
-    </div>
+    @for (todo of todos(); track todo.id) {
+      <div>
+        {{ todo.title }}
+        <button (click)="update(todo)">Update</button>
+        <button (click)="deleteTodo(todo)">Delete</button>
+      </div>
+    } @empty {
+      <span>Loading...</span>
+    }
   `,
   styles: [],
 })
@@ -21,17 +26,39 @@ export class AppComponent implements OnInit {
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todoService.getTodos().subscribe((todos$) => {
-      this.todos.set(todos$);
+    this.todoService.getTodos().subscribe({
+      next: (todos$) => {
+        this.todos.set(todos$);
+      },
+      error: (error) => {
+        console.error(error);
+      },
     });
   }
 
   update(todo: Todo) {
-    this.todoService.updateTodo(todo).subscribe((todoUpdated$: Todo) => {
-      const updatedTodos = this.todos().map((t) =>
-        t.id === todoUpdated$.id ? todoUpdated$ : t,
-      );
-      this.todos.set(updatedTodos);
+    this.todoService.updateTodo(todo).subscribe({
+      next: (todoUpdated$: Todo) => {
+        const updatedTodos = this.todos().map((t) =>
+          t.id === todoUpdated$.id ? todoUpdated$ : t,
+        );
+        this.todos.set(updatedTodos);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo).subscribe({
+      next: () => {
+        const updatedTodos = this.todos().filter((t) => t.id !== todo.id);
+        this.todos.set(updatedTodos);
+      },
+      error: (error) => {
+        console.error(error);
+      },
     });
   }
 }
