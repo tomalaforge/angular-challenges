5d2254d719cf3f9780c76fb898a1f201364d7451
Ambinintsoa <amr@alternateeve.com>
2024-09-05 18:04:16 +0300

    refactor: remove all commented line and unecessary code

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Todo } from './Model/todo';
-import { TodoService } from './Service/todo.service';
 import { TodosStore } from './Store/todo.store';
 
 @Component({
@@ -22,48 +21,17 @@ import { TodosStore } from './Store/todo.store';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  // todos: WritableSignal<Todo[]> = signal([]);
   readonly store = inject(TodosStore);
 
-  constructor(private todoService: TodoService) {}
-
   ngOnInit(): void {
-    // this.todoService.getTodos().subscribe({
-    //   next: (todos$) => {
-    //     this.todos.set(todos$);
-    //   },
-    //   error: (error) => {
-    //     console.error(error);
-    //   },
-    // });
     this.store.getTodos();
   }
 
   update(todo: Todo) {
-    // this.todoService.updateTodo(todo).subscribe({
-    //   next: (todoUpdated$: Todo) => {
-    //     const updatedTodos = this.todos().map((t) =>
-    //       t.id === todoUpdated$.id ? todoUpdated$ : t,
-    //     );
-    //     this.todos.set(updatedTodos);
-    //   },
-    //   error: (error) => {
-    //     console.error(error);
-    //   },
-    // });
     this.store.updateTodo(todo);
   }
 
   delete(todo: Todo) {
-    // this.todoService.deleteTodo(todo).subscribe({
-    //   next: () => {
-    //     const updatedTodos = this.todos().filter((t) => t.id !== todo.id);
-    //     this.todos.set(updatedTodos);
-    //   },
-    //   error: (error) => {
-    //     console.error(error);
-    //   },
-    // });
     this.store.deleteTodo(todo);
   }
 }
