119a8d2e54d1966a0fc6bdbd3a44b45f28db0721
ARMEDIA\bojan.mickoski <bojan.mickoski@armedia.com>
2024-09-20 15:08:09 +0200

    feat: answer 50

--- apps/signal/50-bug-in-effect/src/app/app.component.ts
+++ apps/signal/50-bug-in-effect/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   effect,
   model,
 } from '@angular/core';
@@ -40,14 +41,62 @@ export class AppComponent {
   ram = model(false);
   gpu = model(false);
 
+  // For some reason this was not working. When i check first checkbox, check second,
+  // then uncheck first and check again alert was not shown, prev.drive, next.drive
+  // were both true
+  //   effect(() => {
+  //     const { drive, ram, gpu } = componentsComputed();
+  //     if (drive || ram || gpu) {
+  //       alert('Price increased');
+  //     }
+  //   });
+
+  //   const componentsComputed = computed(
+  //     () => ({
+  //       drive: this.drive(),
+  //       ram: this.ram(),
+  //       gpu: this.gpu(),
+  //     }),
+  //     {
+  //       equal: (prev, next) => {
+  //         if (
+  //           (!Object.is(prev.drive, next.drive) && next.drive) ||
+  //           (!Object.is(prev.ram, next.ram) && next.ram) ||
+  //           (!Object.is(prev.gpu, next.gpu) && next.gpu)
+  //         ) {
+  //           return false;
+  //         }
+  //         return !(!next.drive && !next.ram && !next.gpu);
+  //       },
+  //     },
+  //   );
+  // }
+
+  // Define computed values
+  componentsComputed = computed(() => ({
+    drive: this.drive(),
+    ram: this.ram(),
+    gpu: this.gpu(),
+  }));
+
+  // Subscribe to changes
   constructor() {
-    /* 
-      Explain for your junior team mate why this bug occurs ...
-    */
+    let previousState = { drive: false, ram: false, gpu: false };
+
     effect(() => {
-      if (this.drive() || this.ram() || this.gpu()) {
-        alert('Price increased!');
+      const { drive, ram, gpu } = this.componentsComputed();
+
+      // Check if any checkbox is newly selected (going from false to true)
+      if (
+        (!previousState.drive && drive) ||
+        (!previousState.ram && ram) ||
+        (!previousState.gpu && gpu)
+      ) {
+        alert('Price increased');
       }
+
+      // Update previous state to the current state
+      previousState = { drive, ram, gpu };
     });
   }
 }
