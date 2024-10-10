f2083801d712fff1fde3f2c14908abdb65349aab
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-10-10 12:22:04 +0200

    feat: finished

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,21 +1,44 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  Pipe,
+  PipeTransform,
+  signal,
+} from '@angular/core';
+
+@Pipe({
+  name: 'heavyComputation',
+  pure: true,
+  standalone: true,
+})
+export class HeavyComputationPipe implements PipeTransform {
+  transform(name: string, index: number) {
+    console.log(name);
+    console.log(index);
+    console.log('heavy computation');
+    return `${name} - ${index}`;
+  }
+}
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, HeavyComputationPipe],
   selector: 'app-root',
   template: `
-    <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
-    </div>
+    @for (person of computedPersons(); track $index) {
+      <div>{{ person }}</div>
+    }
   `,
 })
 export class AppComponent {
-  persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
+  persons = signal(['toto', 'jack']);
+  computedPersons = computed(() => {
+    return this.persons().map((name, index) => {
+      console.log(name);
+      console.log(index);
+      console.log('heavy computation');
+      return `${name} - ${index}`;
+    });
+  });
 }
