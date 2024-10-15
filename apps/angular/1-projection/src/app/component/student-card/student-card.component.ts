86878e9cf890c710c9ec5598a75e0332a195c258
nmarilag <nickolemarilag@gmail.com>
2024-10-15 10:59:07 +0800

    refactor(student-card.component): refactor card component
    implement the changes applied from card component

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
 import { CardType } from '../../model/card.model';
 import { Student } from '../../model/student.model';
-import { CardComponent } from '../../ui/card/card.component';
+import { CardModule } from '../../ui/card/card.module';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
-    <app-card
-      [list]="students"
-      [type]="cardType"
-      customClass="bg-light-green"></app-card>
+    <app-card class="bg-light-green" [list]="students" (addItem)="addItem()">
+      <img src="assets/img/student.webp" width="200" />
+
+      <ng-template [cardItem]="students" let-item>
+        <app-list-item
+          [id]="item.id"
+          [name]="item.firstName"
+          (deleteItem)="deleteItem(item.id)" />
+      </ng-template>
+    </app-card>
   `,
   standalone: true,
   styles: [
     `
-      ::ng-deep .bg-light-green {
+      .bg-light-green {
         background-color: rgba(0, 250, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardModule, ListItemComponent],
 })
 export class StudentCardComponent implements OnInit {
   students: Student[] = [];
@@ -36,5 +46,13 @@ export class StudentCardComponent implements OnInit {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
 
     this.store.students$.subscribe((s) => (this.students = s));
+  }
+
+  addItem(): void {
+    this.store.addOne(randStudent());
+  }
+
+  deleteItem(index: number): void {
+    this.store.deleteOne(index);
   }
 }
