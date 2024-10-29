777816abb2bc7f588923cad3fc7bd892e87c31b1
rxssula <rxssul.aitkali@gmail.com>
2024-10-29 12:44:07 +0500

    Revert "fix: this commit is an issue"
    This reverts commit 8dc959ce63a8a2775927665ab953cb73dd94addc.

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,41 +1,40 @@
-import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FakeHttpService,
   randStudent,
 } from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
+import { CardType } from '../../model/card.model';
+import { Student } from '../../model/student.model';
 import { CardComponent } from '../../ui/card/card.component';
-import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
     <app-card
-      [list]="students$ | async"
-      (add)="addStudent()"
-      class="bg-light-green">
-      <img src="assets/img/student.webp" width="200px" />
-      <ng-template #rowRef let-student>
-        <app-list-item (delete)="deleteStudent(student.id)">
-          {{ student.firstname }}
-        </app-list-item>
-      </ng-template>
+      [list]="students"
+      [type]="cardType"
+      customClass="bg-light-green"
+      (add)="onAdd()">
+      <img
+        src="assets/img/student.webp"
+        width="200px"
+        ngProjectAs="card-image" />
     </app-card>
   `,
   standalone: true,
   styles: [
     `
-      .bg-light-green {
+      ::ng-deep .bg-light-green {
         background-color: rgba(0, 250, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent, ListItemComponent, AsyncPipe],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CardComponent],
 })
 export class StudentCardComponent implements OnInit {
-  students$ = this.store.students$;
+  students: Student[] = [];
+  cardType = CardType.STUDENT;
 
   constructor(
     private http: FakeHttpService,
@@ -44,13 +43,11 @@ export class StudentCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+
+    this.store.students$.subscribe((s) => (this.students = s));
   }
 
-  addStudent() {
+  onAdd() {
     this.store.addOne(randStudent());
-  }
-
-  deleteStudent(id: number) {
-    this.store.deleteOne(id);
   }
 }
--- apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
@@ -1,23 +1,35 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Output,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StudentStore } from '../../data-access/student.store';
+import { TeacherStore } from '../../data-access/teacher.store';
+import { CardType } from '../../model/card.model';
 
 @Component({
   selector: 'app-list-item',
   template: `
     <div class="border-grey-300 flex justify-between border px-2 py-1">
-      <ng-content></ng-content>
-      <button (click)="delete.next()">
+      {{ name }}
+      <button (click)="delete(id)">
         <img class="h-5" src="assets/svg/trash.svg" />
       </button>
     </div>
   `,
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListItemComponent {
-  @Output() delete = new EventEmitter<void>();
+  @Input() id!: number;
+  @Input() name!: string;
+  @Input() type!: CardType;
+
+  constructor(
+    private teacherStore: TeacherStore,
+    private studentStore: StudentStore,
+  ) {}
+
+  delete(id: number) {
+    if (this.type === CardType.TEACHER) {
+      this.teacherStore.deleteOne(id);
+    } else if (this.type === CardType.STUDENT) {
+      this.studentStore.deleteOne(id);
+    }
+  }
 }
