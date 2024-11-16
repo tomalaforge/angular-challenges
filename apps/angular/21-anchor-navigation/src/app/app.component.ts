72082c353d79f0a8f058ea55508796c9ade2c2dd
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-11-16 04:16:45 +0100

    feat(21-anchor-navigation): implement anchor navigation with smooth scrolling

--- apps/angular/21-anchor-navigation/src/app/app.component.ts
+++ apps/angular/21-anchor-navigation/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   selector: 'app-root',
   template: `
-    <router-outlet></router-outlet>
+    <router-outlet />
   `,
 })
 export class AppComponent {}
