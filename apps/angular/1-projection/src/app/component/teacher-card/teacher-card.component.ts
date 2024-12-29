487593e2f8947bb12d67d314d4a93f3f3593ce12
mohamedamiri12 <amirimohamed1999@gmail.com>
2024-12-29 01:01:07 +0100

    feat: resolve projection challenge

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Component, OnInit, signal, ViewEncapsulation } from '@angular/core';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
-    <app-card
-      [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+    <app-card [list]="teachers()" customClass="bg-light-red">
+      <img ngProjectAs="image" src="assets/img/teacher.png" width="200px" />
+      <ng-template let-item>
+        {{ item.firstName }}
+        <button (click)="delete(item.id)">
+          <img class="h-5" src="assets/svg/trash.svg" />
+        </button>
+      </ng-template>
+      <button
+        ngProjectAs="add"
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      .bg-light-red {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
+  encapsulation: ViewEncapsulation.None,
   imports: [CardComponent],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  teachers = signal<Teacher[]>([]);
 
   constructor(
     private http: FakeHttpService,
@@ -34,6 +47,15 @@ export class TeacherCardComponent implements OnInit {
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+    this.teachers = this.store.teachers;
+    console.log(this.teachers);
+  }
+
+  addNewItem() {
+    this.store.addOne(randTeacher());
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
   }
 }
