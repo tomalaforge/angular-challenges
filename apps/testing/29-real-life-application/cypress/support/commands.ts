5be923c25691f0ea62b5966f118d1a4aa4cd9d2a
thomas <toma.laforge@gmail.com>
2025-01-27 21:07:25 +0100

    feat(*): upgrade cypress

--- apps/testing/29-real-life-application/cypress/support/commands.ts
+++ apps/testing/29-real-life-application/cypress/support/commands.ts
@@ -23,20 +23,3 @@ declare global {
 }
 
 Cypress.Commands.add('mount', mount);
-
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
