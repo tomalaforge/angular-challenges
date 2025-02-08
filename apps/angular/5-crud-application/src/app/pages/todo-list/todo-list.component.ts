9025cf2904dbb46bd0197ad895a94c2da02a1802
charles kato <kcharles52@gmail.com>
2025-02-08 13:50:43 +0300

    feat: display error and loader

--- apps/angular/5-crud-application/src/app/pages/todo-list/todo-list.component.ts
+++ apps/angular/5-crud-application/src/app/pages/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { TodoItemComponent } from '../../components/todo-item/todo-item.component';
 import { Todo } from '../../models/todo.model';
@@ -8,19 +9,25 @@ import { TodoService } from '../../services/todo.service';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, TodoItemComponent],
+  imports: [CommonModule, TodoItemComponent, MatProgressSpinnerModule],
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
   todos!: Signal<Todo[]>;
+  loading!: Signal<boolean>;
+  errorMessage!: Signal<string | null>;
 
   constructor(private readonly todoService: TodoService) {}
 
   ngOnInit(): void {
     this.todos = this.todoService.todos$;
+    this.loading = this.todoService.loading$;
+    this.errorMessage = this.todoService.errorMessage$;
     this.todoService.getTodos().subscribe();
   }
 
   update = (todo: Todo) => this.todoService.updateTodo(todo);
+
+  delete = (todo: Todo) => this.todoService.deleteTodo(todo);
 }
