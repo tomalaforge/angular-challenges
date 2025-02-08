32e951252bb67a2902ff7f6350bfdb73d359f3be
ianbalijawa <ianbalijawa16@gmail.com>
2025-02-08 16:16:19 +0300

    feat(challenge-45): integrate React component in Angular

--- apps/angular/45-react-in-angular/tailwind.config.js
+++ apps/angular/45-react-in-angular/tailwind.config.js
@@ -1,11 +1,11 @@
-const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('path');
+const { createGlobPatternsForDependencies } = require( '@nx/angular/tailwind' );
+const { join } = require( 'path' );
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
+    join( __dirname, 'src/**/!(*.stories|*.spec).{ts,tsx,html}' ),
+    ...createGlobPatternsForDependencies( __dirname ),
   ],
   theme: {
     extend: {},
