e7d2e428e1ca6268913934769f1dd0972bc6d723
charles kato <kcharles52@gmail.com>
2025-02-08 12:05:45 +0300

    feat: todo item component

---
+++ apps/angular/5-crud-application/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../../models/todo.model';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+})
+export class TodoItemComponent {
+  @Input() todo: Todo;
+  @Output() update = new EventEmitter<Todo>();
+
+  updateTodo() {
+    this.update.emit(this.todo);
+  }
+}
