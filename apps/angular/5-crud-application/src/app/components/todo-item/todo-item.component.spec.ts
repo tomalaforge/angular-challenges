3a6958cb122714641f5d74dca40989df9946496d
charles kato <kcharles52@gmail.com>
2025-02-10 01:00:17 +0300

    test: emitting update event on clicking update button

--- apps/angular/5-crud-application/src/app/components/todo-item/todo-item.component.spec.ts
+++ apps/angular/5-crud-application/src/app/components/todo-item/todo-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { Todo } from '../../models/todo.model';
 import { TodoItemComponent } from './todo-item.component';
 
@@ -28,5 +29,23 @@ describe('TodoItemComponent', () => {
 
     const todoText = fixture.nativeElement.querySelector('span').textContent;
     expect(todoText).toContain('Test Todo');
+  });
+
+  it('should emit update event on clicking update button', () => {
+    const mockTodo: Todo = {
+      id: 1,
+      title: 'Test Todo',
+      completed: false,
+      userId: 1,
+    };
+    component.todo = mockTodo;
+    fixture.detectChanges();
+
+    jest.spyOn(component.update, 'emit');
+
+    const button = fixture.debugElement.query(By.css('button:nth-of-type(1)'));
+    button.triggerEventHandler('click', null);
+
+    expect(component.update.emit).toHaveBeenCalledWith(mockTodo);
   });
 });
