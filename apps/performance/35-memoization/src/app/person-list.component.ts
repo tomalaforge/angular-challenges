c9d9509633634046e4e9db8a617325f66c9bfc50
ianbalijawa <ianbalijawa16@gmail.com>
2025-02-11 16:05:56 +0300

    perf(memoization): optimize fibonacci calculation with pure pipe

--- apps/performance/35-memoization/src/app/person-list.component.ts
+++ apps/performance/35-memoization/src/app/person-list.component.ts
@@ -1,22 +1,16 @@
-import { Component, Input } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { FibonacciPipe } from './fibonacci.pipe';
 import { Person } from './person.model';
-
-const fibonacci = (num: number): number => {
-  if (num === 1 || num === 2) {
-    return 1;
-  }
-  return fibonacci(num - 1) + fibonacci(num - 2);
-};
 
 @Component({
   selector: 'app-person-list',
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
@@ -24,40 +18,40 @@ const fibonacci = (num: number): number => {
     MatFormFieldModule,
     MatInputModule,
     MatChipsModule,
+    FibonacciPipe,
   ],
   template: `
-    <h1 class="text-center font-semibold" title="Title">
+    <h1 class="mb-6 text-center text-2xl font-bold text-gray-800">
       {{ title | titlecase }}
     </h1>
 
-    <mat-form-field class="w-4/5">
+    <mat-form-field class="mb-6 w-full">
       <input
         placeholder="Add one member to the list"
         matInput
         type="text"
+        class="text-lg"
         [(ngModel)]="label" />
     </mat-form-field>
 
-    <mat-list class="flex w-full">
-      <mat-list-item *ngFor="let person of persons">
-        <div MatListItemLine class="flex justify-between">
-          <h3>{{ person.name }}</h3>
-          <mat-chip>{{ calculate(person.fib) }}</mat-chip>
+    <mat-list class="w-full divide-y divide-gray-200">
+      <mat-list-item *ngFor="let person of persons" class="py-4">
+        <div MatListItemLine class="flex items-center justify-between">
+          <h3 class="text-lg font-medium text-gray-900">{{ person.name }}</h3>
+          <mat-chip class="bg-blue-100 text-blue-800">
+            Fib({{ person.fib }}) = {{ person.fib | fibonacci }}
+          </mat-chip>
         </div>
       </mat-list-item>
     </mat-list>
   `,
   host: {
-    class: 'w-full flex flex-col items-center',
+    class: 'block w-full',
   },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonListComponent {
   @Input() persons: Person[] = [];
   @Input() title = '';
-
   label = '';
-
-  calculate(num: number) {
-    return fibonacci(num);
-  }
 }
