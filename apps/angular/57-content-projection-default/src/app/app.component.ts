1516c4b67f978eafeb0dd62b572b5d0a926b7c34
Mohamed BEN MAKHLOUF <bc6509@engie.com>
2025-03-04 13:18:54 +0100

    feat: use ng-content instead of input

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CardComponent } from './card.component';
 
 @Component({
   imports: [CardComponent],
+  schemas: [NO_ERRORS_SCHEMA],
   selector: 'app-root',
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <app-card>
+      <card-title>Titre 1</card-title>
+      <card-message>Message1</card-message>
+    </app-card>
+    <app-card>
+      <card-title>Titre 2</card-title>
+    </app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
