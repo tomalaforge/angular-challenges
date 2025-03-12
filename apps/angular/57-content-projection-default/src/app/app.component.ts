e792eeb3d4d1633b9a0bbe734e05a8d698f45237
WhoisBsa <mbscbsjbs@gmail.com>
2025-03-12 17:59:19 -0300

    feat: answer:57 content-projection-default

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -5,8 +5,13 @@ import { CardComponent } from './card.component';
   imports: [CardComponent],
   selector: 'app-root',
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <app-card title="Titre 1" message="Message1">
+      <div ngProjectAs="title">Titre 1</div>
+      <div ngProjectAs="message">Message1</div>
+    </app-card>
+    <app-card title="Titre 2">
+      <div ngProjectAs="title">Titre 2</div>
+    </app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
