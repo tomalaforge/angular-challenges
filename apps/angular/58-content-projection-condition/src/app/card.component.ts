ccfbcfe47891b1d146c0dff3523e769845d3d95d
peggylicious <p.asq@live.com>
2025-03-16 10:43:48 +0100

    fix: answer 58

--- apps/angular/58-content-projection-condition/src/app/card.component.ts
+++ apps/angular/58-content-projection-condition/src/app/card.component.ts
@@ -1,21 +1,31 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   template: `
     @if (small()) {
-      <ng-content select="[title]" />
-      <ng-content select="[message]" />
+      <ng-container [ngTemplateOutlet]="titleTempl" />
+      <ng-container [ngTemplateOutlet]="messageTempl" />
     } @else {
       <div class="p-4">
         <div class="text-2xl">
-          <ng-content select="[title]" />
+          <ng-container [ngTemplateOutlet]="titleTempl" />
         </div>
-        <ng-content select="[message]" />
+        <ng-container [ngTemplateOutlet]="messageTempl" />
       </div>
     }
+
+    <ng-template #titleTempl>
+      <ng-content select="[title]" />
+    </ng-template>
+    <ng-template #messageTempl>
+      <ng-content select="[message]" />
+    </ng-template>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NgTemplateOutlet],
   host: {
     class: 'p-4 border border-grey rounded-sm flex flex-col w-[200px]',
   },
