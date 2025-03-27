ef03cb3d261810890eebea01b89c9e12445d95bc
GIANNIS\tsiro <tsiro1@hotmail.com>
2025-03-27 10:31:09 +0200

    Revert "feat: fix angular content projection default challenge"
    This reverts commit c1925527c4ef6e43c0e7d8a5918ae7677f57d0c3.

--- apps/angular/58-content-projection-condition/src/app/app.component.ts
+++ apps/angular/58-content-projection-condition/src/app/app.component.ts
@@ -1,35 +1,17 @@
-import { ChangeDetectionStrategy, Component, Directive } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CardComponent } from './card.component';
 
-@Directive({
-  selector: '[appCardTitle]',
-})
-export class CardTitleTemplateDirective {}
-
-@Directive({
-  selector: '[appCardMessage]',
-})
-export class CardMessageTemplateDirective {}
-
 @Component({
-  imports: [
-    CardComponent,
-    CardTitleTemplateDirective,
-    CardMessageTemplateDirective,
-  ],
+  imports: [CardComponent],
   selector: 'app-root',
   template: `
     <app-card>
-      <ng-template appCardTitle>
-        <div class="text-2xl"><div>Card 1</div></div>
-      </ng-template>
-      <ng-template appCardMessage><div>Message 1</div></ng-template>
+      <div title>Card 1</div>
+      <div message>Message 1</div>
     </app-card>
     <app-card [small]="true">
-      <ng-template appCardTitle>
-        <div>Card 2</div>
-      </ng-template>
-      <ng-template appCardMessage><div>Message 2</div></ng-template>
+      <div title>Card 2</div>
+      <div message>Message 2</div>
     </app-card>
   `,
   host: {
--- apps/angular/58-content-projection-condition/src/app/card.component.ts
+++ apps/angular/58-content-projection-condition/src/app/card.component.ts
@@ -1,32 +1,20 @@
-import { NgTemplateOutlet } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  contentChild,
-  input,
-  TemplateRef,
-} from '@angular/core';
-import {
-  CardMessageTemplateDirective,
-  CardTitleTemplateDirective,
-} from './app.component';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   template: `
     @if (small()) {
-      <ng-container [ngTemplateOutlet]="cardTitle()" />
-      <ng-container [ngTemplateOutlet]="cardMessage()" />
+      <ng-content select="[title]" />
+      <ng-content select="[message]" />
     } @else {
       <div class="p-4">
         <div class="text-2xl">
-          <ng-container [ngTemplateOutlet]="cardTitle()" />
+          <ng-content select="[title]" />
         </div>
-        <ng-container [ngTemplateOutlet]="cardMessage()" />
+        <ng-content select="[message]" />
       </div>
     }
   `,
-  imports: [NgTemplateOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'p-4 border border-grey rounded-sm flex flex-col w-[200px]',
@@ -34,12 +22,4 @@ import {
 })
 export class CardComponent {
   small = input<boolean>(false);
-
-  cardTitle = contentChild.required(CardTitleTemplateDirective, {
-    read: TemplateRef,
-  });
-
-  cardMessage = contentChild.required(CardMessageTemplateDirective, {
-    read: TemplateRef,
-  });
 }
