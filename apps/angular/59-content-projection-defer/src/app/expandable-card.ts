17e9a9a9b424d56030018fce0e8ff8f3dcbf731a
stormlx <caldato.alexandre@gmail.com>
2025-04-01 20:31:18 +0200

    feat: added lazy loading for posts (now loads when card expands)

--- apps/angular/59-content-projection-defer/src/app/expandable-card.ts
+++ apps/angular/59-content-projection-defer/src/app/expandable-card.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  output,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-expandable-card',
   template: `
     <button
       class="text-fg-subtle hover:bg-button-secondary-bg-hover active:bg-button-secondary-bg-active focus:outline-button-border-highlight flex w-fit items-center gap-1 py-2 focus:outline focus:outline-2 focus:outline-offset-1"
-      (click)="isExpanded.set(!isExpanded())"
+      (click)="toggleExpansion()"
       data-cy="expandable-panel-toggle">
       @if (isExpanded()) {
         <svg
@@ -51,4 +56,11 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 })
 export class ExpandableCard {
   public isExpanded = signal(false);
+
+  expanded = output<boolean>();
+
+  toggleExpansion(): void {
+    this.isExpanded.update((isExpanded) => !isExpanded);
+    this.expanded.emit(this.isExpanded());
+  }
 }
