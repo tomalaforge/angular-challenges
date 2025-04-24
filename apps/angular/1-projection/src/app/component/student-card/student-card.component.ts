6be8c0c79d56a640d3963e386204b426afab7c54
Karim Balayev <balayevkerim@gmail.com>
2025-04-24 13:54:05 +0300

    feat: refactor card and list component to project elements from parent

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -3,19 +3,43 @@ import {
   Component,
   inject,
   OnInit,
+  ViewEncapsulation,
 } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
 import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
-    <app-card
-      [list]="students()"
-      [type]="cardType"
-      customClass="bg-light-green" />
+    <app-card [list]="students()" customClass="bg-light-green">
+      <img
+        ngProjectAs="card-img"
+        src="assets/img/student.webp"
+        width="200"
+        height="200" />
+      <ng-template #listTemplate let-student>
+        <app-list-item>
+          {{ student.firstName }}
+          <button
+            (click)="deleteStudent(student.id)"
+            ngProjectAs="delete-button">
+            <img class="h-5" src="assets/svg/trash.svg" alt="icon trash" />
+          </button>
+        </app-list-item>
+      </ng-template>
+      <button
+        (click)="addStudent()"
+        ngProjectAs="add-button"
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2">
+        Add
+      </button>
+    </app-card>
   `,
   styles: [
     `
@@ -24,7 +48,8 @@ import { CardComponent } from '../../ui/card/card.component';
       }
     `,
   ],
-  imports: [CardComponent],
+  encapsulation: ViewEncapsulation.None,
+  imports: [CardComponent, ListItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentCardComponent implements OnInit {
@@ -36,5 +61,12 @@ export class StudentCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+  }
+  deleteStudent(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addStudent() {
+    this.store.addOne(randStudent());
   }
 }
--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,25 +1,49 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
 import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
-    <app-card
-      [list]="teachers()"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+    <app-card [list]="teachers()" customClass="bg-light-red">
+      <img
+        ngProjectAs="card-img"
+        src="assets/img/teacher.png"
+        width="200"
+        height="200" />
+      <ng-template #listTemplate let-teacher>
+        <app-list-item>
+          {{ teacher.firstName }}
+          <button
+            (click)="deleteTeacher(teacher.id)"
+            ngProjectAs="delete-button">
+            <img class="h-5" src="assets/svg/trash.svg" alt="icon trash" />
+          </button>
+        </app-list-item>
+      </ng-template>
+      <button
+        (click)="addTeacher()"
+        ngProjectAs="add-button"
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2">
+        Add
+      </button>
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      .bg-light-red {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  encapsulation: ViewEncapsulation.None,
+  imports: [CardComponent, ListItemComponent],
 })
 export class TeacherCardComponent implements OnInit {
   private http = inject(FakeHttpService);
@@ -30,5 +54,12 @@ export class TeacherCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
+  deleteTeacher(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addTeacher() {
+    this.store.addOne(randTeacher());
   }
 }
