216886570d3cc6bd341ca5d4cd3a01779a316483
Yuri Eveev <hddevseev@gmail.com>
2025-05-15 21:46:21 +0400

    feat: high-order-operator-bug

--- apps/rxjs/11-high-order-operator-bug/src/app/app.service.ts
+++ apps/rxjs/11-high-order-operator-bug/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { merge, Observable, of } from 'rxjs';
+import { filter, from, map, mergeMap, Observable, of, toArray } from 'rxjs';
 import { LocalDBService, TopicType } from './localDB.service';
 
 @Injectable({ providedIn: 'root' })
@@ -10,10 +10,16 @@ export class AppService {
 
   deleteOldTopics(type: TopicType): Observable<boolean> {
     const infoByType = this.dbService.searchByType(type);
-    return infoByType.length > 0
-      ? infoByType
-          .map((t) => this.dbService.deleteOneTopic(t.id))
-          .reduce((acc, curr) => merge(acc, curr), of(true))
-      : of(true);
+
+    if (!infoByType.length) {
+      return of(true);
+    }
+
+    return from(infoByType).pipe(
+      mergeMap((topic) => this.dbService.deleteOneTopic(topic.id)),
+      filter((topic) => !topic),
+      toArray(),
+      map((t) => t.length === 0),
+    );
   }
 }
