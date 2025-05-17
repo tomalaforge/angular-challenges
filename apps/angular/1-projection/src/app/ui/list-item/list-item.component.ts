c70ca933377690453454d828f4a13b50a4bad176
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-05-16 21:28:50 -0300

    feat(): challenge 1 - Projection solution

--- apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
@@ -1,40 +1,31 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
-  input,
+  EventEmitter,
+  Output,
 } from '@angular/core';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 
 @Component({
   selector: 'app-list-item',
   template: `
-    <div class="border-grey-300 flex justify-between border px-2 py-1">
-      {{ name() }}
-      <button (click)="delete(id())">
+    <div
+      class="border-grey-300 bg-light-green flex justify-between border px-2 py-1">
+      <ng-content></ng-content>
+      <button (click)="delete.next()">
         <img class="h-5" src="assets/svg/trash.svg" />
       </button>
     </div>
   `,
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [
+    `
+      .bg-light-green {
+        background-color: rgba(0, 250, 0, 0.1);
+      }
+    `,
+  ],
 })
 export class ListItemComponent {
-  private teacherStore = inject(TeacherStore);
-  private studentStore = inject(StudentStore);
-
-  readonly id = input.required<number>();
-  readonly name = input.required<string>();
-  readonly type = input.required<CardType>();
-
-  delete(id: number) {
-    const type = this.type();
-    if (type === CardType.TEACHER) {
-      this.teacherStore.deleteOne(id);
-    } else if (type === CardType.STUDENT) {
-      this.studentStore.deleteOne(id);
-    }
-  }
+  @Output() delete = new EventEmitter<void>();
 }
