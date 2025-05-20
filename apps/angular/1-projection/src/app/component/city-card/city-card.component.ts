e510ae8fd66f2d1178f3081f1bc603bf2b5dd724
Paul Chesa <paulchesa1@gmail.com>
2025-05-20 05:13:46 +0300

    feat(city-card): created reusable card for cities

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,9 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  imports: [],
+  template: `
+    <app-card
+      [list]="cities()"
+      [cardImgSrc]="'assets/img/teacher.png'"
+      (delete)="delete($event)"
+      (addNewItem)="addNewItem()"
+      customClass="bg-light-red"></app-card>
+  `,
+  imports: [CardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent {}
+export class CityCardComponent implements OnInit {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
+
+  cities = this.store.cities;
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((t) => this.store.addAll(t));
+  }
+
+  addNewItem() {
+    this.store.addOne(randomCity());
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+}
