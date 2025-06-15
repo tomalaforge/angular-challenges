a2860b8d1e140f8127f662874d750f04fa92c9a6
Александр Соколов <sairon1@yandex.ru>
2025-06-15 21:01:52 +0300

    feat(challenge 1): projection - my solution

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,9 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardImgDirective } from '../../ui/card-img-directive/card-img.directive';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
+import { TemplateDirective } from '../../ui/template-directive/template.directive';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  imports: [],
+  template: `
+    <app-card [list]="cities()" [addItemFn]="addItemFn">
+      <img appCardImg ngSrc="assets/img/city.png" width="200" height="200" />
+      <ng-template appTemplate let-item>
+        <app-list-item
+          [name]="item.name"
+          [id]="item.id"
+          [deleteItemFn]="deleteItemFn"></app-list-item>
+      </ng-template>
+    </app-card>
+  `,
+  styles: [
+    `
+      :host {
+        --card-background-color: rgba(221, 250, 0, 0.1);
+      }
+    `,
+  ],
+  imports: [
+    CardComponent,
+    NgOptimizedImage,
+    ListItemComponent,
+    CardImgDirective,
+    TemplateDirective,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent {}
+export class CityCardComponent implements OnInit {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
+
+  cities = this.store.cities;
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  readonly deleteItemFn = (id: number) => this.store.deleteOne(id);
+  readonly addItemFn = () => this.store.addOne(randomCity());
+}
