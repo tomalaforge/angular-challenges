684ed24f355a6df51cff2355571aec1fa31ac1f7
Esteban Suarez Prado <estebansuarezprado420@gmail.com>
2025-07-02 23:34:00 -0400

    feat(card): adding projection to card

--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -1,10 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, input, output } from '@angular/core';
-import { City } from '../../model/city.model';
-import { Student } from '../../model/student.model';
-import { Teacher } from '../../model/teacher.model';
-import { ListItemComponent } from '../list-item/list-item.component';
-import { DisplayNamePipe } from './display-name-pipe';
 
 @Component({
   selector: 'app-card',
@@ -15,12 +10,7 @@ import { DisplayNamePipe } from './display-name-pipe';
       <img [ngSrc]="img()" width="200" height="200" />
 
       <section>
-        @for (item of list(); track item) {
-          <app-list-item
-            [name]="item | appDisplayName"
-            [id]="item.id"
-            (deleteItem)="deleteItemClick($event)"></app-list-item>
-        }
+        <ng-content></ng-content>
       </section>
 
       <button
@@ -30,19 +20,14 @@ import { DisplayNamePipe } from './display-name-pipe';
       </button>
     </div>
   `,
-  imports: [ListItemComponent, NgOptimizedImage, DisplayNamePipe],
+  imports: [NgOptimizedImage],
 })
 export class CardComponent {
-  readonly list = input<Teacher[] | City[] | Student[] | null>(null);
   readonly customClass = input('');
   readonly img = input<string>('');
   addNewItem = output<void>();
-  deleteItem = output<number>();
 
   addNewItemClick() {
     this.addNewItem.emit();
-  }
-  deleteItemClick(id: number) {
-    this.deleteItem.emit(id);
   }
 }
