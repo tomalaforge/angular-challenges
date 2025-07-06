c355fe4aad0fb5e907adc889c9aa25d72aefe79b
thomas <toma.laforge@gmail.com>
2025-07-06 20:26:26 +0200

    fix(): cypress

--- apps/testing/20-modal/cypress.config.ts
+++ apps/testing/20-modal/cypress.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   component: {
     ...nxComponentTestingPreset(__filename),
-    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
-    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
-    injectDocumentDomain: true,
   },
 });
