218b42d28245e8c48e63815aba69913dcee54de1
thomas <toma.laforge@gmail.com>
2025-07-08 23:13:53 +0200

    fix(): server

--- doc-angular/api/index.js
+++ doc-angular/api/index.js
@@ -1,8 +1,3 @@
-const path = require('path');
-
-const serverDistPath = path.join(
-  process.cwd(),
-  'doc-angular/src/server/server.mjs',
+export default import('../dist/server/server.mjs').then((module) =>
+  module.app(),
 );
-
-export default import(serverDistPath).then((module) => module.app);
