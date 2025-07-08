58f4b825efaa23344a3a17538a30776b35a27a16
thomas <toma.laforge@gmail.com>
2025-07-08 22:22:38 +0200

    fix: refresh bug

--- doc-angular/api/index.js
+++ doc-angular/api/index.js
@@ -1,10 +1,3 @@
-import('../dist/server/server.mjs')
-  .then((module) => module.app)
-  .catch((error) => {
-    console.error('Failed to load server module:', error);
-    throw error;
-  });
-export default async (req, res) => {
-  const { app } = await import('../dist/server/server.mjs');
-  return app(req, res);
-};
+export default import('../dist/server/server.mjs').then((module) =>
+  module.app(),
+);
