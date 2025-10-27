97d46d586986743d659b7f6f462272d2f88392d8
lukasss88 <lukaszszymanski88.99@gmail.com>
2025-10-27 13:37:52 +0100

    feat: answer:60 async-redirect solution

--- apps/angular/60-async-redirect/src/app/routes.ts
+++ apps/angular/60-async-redirect/src/app/routes.ts
@@ -1,9 +1,11 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
 import { AdminPage } from './admin-page';
 import { App } from './app';
 import { Dashboard } from './dashboard';
 import { ProfilePage } from './profile-page';
 import { UserPage } from './user-page';
+import { UserProfileService } from './user-profile.service';
 
 export const routes: Routes = [
   {
@@ -12,6 +14,11 @@ export const routes: Routes = [
     children: [
       { path: '', pathMatch: 'full', component: Dashboard },
       { path: 'profile', component: ProfilePage },
+      {
+        path: 'account',
+        pathMatch: 'full',
+        redirectTo: () => inject(UserProfileService).getProfile(),
+      },
       { path: 'admin', component: AdminPage },
       { path: 'user', component: UserPage },
     ],
