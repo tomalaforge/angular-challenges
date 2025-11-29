4cd7445aeb8d41a8b430f8efb1acf59251190a2e
lukasss88 <lukaszszymanski88.99@gmail.com>
2025-11-29 13:22:02 +0100

    feat(answer:48): avoid_losing_data

--- apps/forms/48-avoid-losing-form-data/src/app/ui/dialog.component.ts
+++ apps/forms/48-avoid-losing-form-data/src/app/ui/dialog.component.ts
@@ -1,24 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 // NOTE : this is just the dialog content, you need to implement dialog logic
 
 @Component({
   template: `
     <div role="alert" class="rounded-xl border border-gray-100 bg-white p-5">
-      <h3 class="block text-xl font-medium text-red-600">
+      <h3 class="block text-xl font-medium text-red-600" id="dialog-title">
         You have unsaved information!
       </h3>
 
-      <p class="mt-1 text-gray-700">Do you want to continue and lose them?</p>
+      <p id="dialog-description" class="mt-1 text-gray-700">
+        Do you want to continue and lose them?
+      </p>
 
       <div class="mt-4 flex gap-2">
         <button
-          class="inline-flex items-center gap-2 rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700">
+          class="inline-flex items-center gap-2 rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+          (click)="dialogRef.close(true)">
           Yes continue
         </button>
 
         <button
-          class="block rounded-lg px-4 py-2 text-gray-700 transition hover:bg-gray-50">
+          class="block rounded-lg px-4 py-2 text-gray-700 transition hover:bg-gray-50"
+          (click)="dialogRef.close(false)">
           Stay on page
         </button>
       </div>
@@ -26,4 +31,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlertDialogComponent {}
+export class AlertDialogComponent {
+  dialogRef = inject<DialogRef<boolean>>(DialogRef<boolean>);
+}
