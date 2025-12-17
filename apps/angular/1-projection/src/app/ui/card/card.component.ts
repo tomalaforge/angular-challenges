e55c1767fef99c31b3a049117c76602f43d8c6d9
Daniel Cortes <danncortes@gmail.com>
2025-12-17 15:04:20 +0100

    feat: angular projection

--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -1,10 +1,6 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { ListItemComponent } from '../list-item/list-item.component';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, contentChild, input, output } from '@angular/core';
+import { CardListItemTemplateDirective } from './card-list-item-template.directive';
 
 @Component({
   selector: 'app-card',
@@ -12,19 +8,14 @@ import { ListItemComponent } from '../list-item/list-item.component';
     <div
       class="flex w-fit flex-col gap-3 rounded-md border-2 border-black p-4"
       [class]="customClass()">
-      @if (type() === CardType.TEACHER) {
-        <img ngSrc="assets/img/teacher.png" width="200" height="200" />
-      }
-      @if (type() === CardType.STUDENT) {
-        <img ngSrc="assets/img/student.webp" width="200" height="200" />
-      }
-
+      <ng-content />
       <section>
         @for (item of list(); track item) {
-          <app-list-item
-            [name]="item.firstName"
-            [id]="item.id"
-            [type]="type()"></app-list-item>
+          <ng-container
+            *ngTemplateOutlet="
+              listItemTemplate()?.template ?? null;
+              context: { $implicit: item, item: item }
+            "></ng-container>
         }
       </section>
 
@@ -35,24 +26,32 @@ import { ListItemComponent } from '../list-item/list-item.component';
       </button>
     </div>
   `,
-  imports: [ListItemComponent, NgOptimizedImage],
+  styles: [
+    `
+      .bg-light-red {
+        background-color: rgba(250, 0, 0, 0.1);
+      }
+
+      .bg-light-green {
+        background-color: rgba(0, 250, 0, 0.1);
+      }
+
+      .bg-light-blue {
+        background-color: rgba(0, 0, 250, 0.1);
+      }
+    `,
+  ],
+  imports: [NgTemplateOutlet, CardListItemTemplateDirective],
 })
 export class CardComponent {
-  private teacherStore = inject(TeacherStore);
-  private studentStore = inject(StudentStore);
-
   readonly list = input<any[] | null>(null);
-  readonly type = input.required<CardType>();
   readonly customClass = input('');
+  onAddNewItem = output<void>();
 
-  CardType = CardType;
+  // Use contentChild to query for the directive
+  listItemTemplate = contentChild(CardListItemTemplateDirective);
 
   addNewItem() {
-    const type = this.type();
-    if (type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
+    this.onAddNewItem.emit();
   }
 }
