5a7038a959d6181d3db8af04e4bff072254b53ff
gw0zdziu <gw0zdziu09@gmail.com>
2025-12-25 21:58:46 +0100

    feat(student-card): enhance template with projection support and new actions

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,21 +1,46 @@
+import { NgOptimizedImage } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
+import { OptionTemplateDirective } from '../../directive/option-template.directive';
 import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
-    <app-card
-      [list]="students()"
-      [type]="cardType"
-      customClass="bg-light-green" />
+    <app-card [list]="students()" [customClass]="'bg-light-green'">
+      <img
+        ngProjectAs="card-image"
+        ngSrc="assets/img/student.webp"
+        width="200"
+        height="200" />
+      <ng-template appOptionTemplate let-student>
+        <app-list-item>
+          <ng-container ngProjectAs="item-name">
+            {{ student.firstName }}
+          </ng-container>
+          <button ngProjectAs="item-action" (click)="delete(student.id)">
+            <img class="h-5" src="assets/svg/trash.svg" />
+          </button>
+        </app-list-item>
+      </ng-template>
+      <button
+        ngProjectAs="card-action"
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
   `,
   styles: [
     `
@@ -24,7 +49,12 @@ import { CardComponent } from '../../ui/card/card.component';
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [
+    CardComponent,
+    NgOptimizedImage,
+    ListItemComponent,
+    OptionTemplateDirective,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentCardComponent implements OnInit {
@@ -36,5 +66,13 @@ export class StudentCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addNewItem() {
+    this.store.addOne(randStudent());
   }
 }
