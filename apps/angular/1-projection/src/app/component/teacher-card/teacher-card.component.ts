a920e5a6a58418fb4a02eb0b08d337235051354c
gw0zdziu <gw0zdziu09@gmail.com>
2025-12-25 21:59:01 +0100

    feat(teacher-card): enhance template with projection support and new actions

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,25 +1,55 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
+import { OptionTemplateDirective } from '../../directive/option-template.directive';
 import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
-    <app-card
-      [list]="teachers()"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+    <app-card [list]="teachers()" [customClass]="'bg-light-red'">
+      <img
+        ngProjectAs="card-image"
+        ngSrc="assets/img/teacher.png"
+        width="200"
+        height="200" />
+      <ng-template appOptionTemplate let-teacher>
+        <app-list-item>
+          <ng-container ngProjectAs="item-name">
+            {{ teacher.firstName }}
+          </ng-container>
+          <button ngProjectAs="item-action" (click)="delete(teacher.id)">
+            <img class="h-5" src="assets/svg/trash.svg" />
+          </button>
+        </app-list-item>
+      </ng-template>
+      <button
+        ngProjectAs="card-action"
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      :host .bg-light-red {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [
+    CardComponent,
+    ListItemComponent,
+    NgOptimizedImage,
+    OptionTemplateDirective,
+  ],
 })
 export class TeacherCardComponent implements OnInit {
   private http = inject(FakeHttpService);
@@ -30,5 +60,13 @@ export class TeacherCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addNewItem() {
+    this.store.addOne(randTeacher());
   }
 }
