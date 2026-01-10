10aa9f5dfae2e6faedd8976f7101424702cdcd14
victor <gfeng005@uottawa.ca>
2026-01-09 22:28:30 -0500

    refactor(list-item): simplify the component variables and the methods

--- apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
@@ -1,12 +1,10 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
+  EventEmitter,
   input,
+  Output,
 } from '@angular/core';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 
 @Component({
   selector: 'app-list-item',
@@ -21,19 +19,13 @@ import { CardType } from '../../model/card.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListItemComponent {
-  private teacherStore = inject(TeacherStore);
-  private studentStore = inject(StudentStore);
+  @Output()
+  deleteClicked = new EventEmitter<number>();
 
   readonly id = input.required<number>();
   readonly name = input.required<string>();
-  readonly type = input.required<CardType>();
 
   delete(id: number) {
-    const type = this.type();
-    if (type === CardType.TEACHER) {
-      this.teacherStore.deleteOne(id);
-    } else if (type === CardType.STUDENT) {
-      this.studentStore.deleteOne(id);
-    }
+    this.deleteClicked.emit(id);
   }
 }
