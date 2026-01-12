037308f857a71cfe15de39bf6e96f41dd55c7dab
victor <gfeng005@uottawa.ca>
2026-01-11 20:10:15 -0500

    refactor(list-item): simplyfy the list-item using content projection

--- apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
+++ apps/angular/1-projection/src/app/ui/list-item/list-item.component.ts
@@ -1,31 +1,13 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   template: `
     <div class="border-grey-300 flex justify-between border px-2 py-1">
-      {{ name() }}
-      <button (click)="delete(id())">
-        <img class="h-5" src="assets/svg/trash.svg" />
-      </button>
+      <ng-content select="span"></ng-content>
+      <ng-content select="button"></ng-content>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListItemComponent {
-  @Output()
-  deleteClicked = new EventEmitter<number>();
-
-  readonly id = input.required<number>();
-  readonly name = input.required<string>();
-
-  delete(id: number) {
-    this.deleteClicked.emit(id);
-  }
-}
+export class ListItemComponent {}
