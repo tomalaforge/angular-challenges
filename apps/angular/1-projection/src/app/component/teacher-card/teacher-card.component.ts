05c7d5d63967ea95bbb3574987a9d718b8db8b81
victor <gfeng005@uottawa.ca>
2026-01-13 14:08:19 -0500

    refactor(teacher-card): recontructor teacher card by content projection

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,34 +1,53 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
+    <ng-template #configurableList let-itemName="itemName" let-itemId="itemId">
+      <app-list-item>
+        <span>{{ itemName }}</span>
+        <button (click)="delete(itemId)">
+          <img class="h-5" src="assets/svg/trash.svg" />
+        </button>
+      </app-list-item>
+    </ng-template>
     <app-card
       [list]="teachers()"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+      [customBgColor]="bgColor"
+      [cardContent]="configurableList">
+      <img ngSrc="assets/img/teacher.png" width="200" height="200" />
+      <button
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
   `,
-  styles: [
-    `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
-      }
-    `,
-  ],
-  imports: [CardComponent],
+  imports: [CardComponent, NgOptimizedImage, ListItemComponent],
 })
 export class TeacherCardComponent implements OnInit {
   private http = inject(FakeHttpService);
   private store = inject(TeacherStore);
-
   teachers = this.store.teachers;
-  cardType = CardType.TEACHER;
+  bgColor = { 'background-color': 'rgba(250, 0, 0, 0.1)' };
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addNewItem() {
+    this.store.addOne(randTeacher());
   }
 }
