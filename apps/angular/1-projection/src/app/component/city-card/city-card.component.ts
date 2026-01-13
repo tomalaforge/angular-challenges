db12ed148fced901488e752fa425f3875bbde249
victor <gfeng005@uottawa.ca>
2026-01-13 14:53:16 -0500

    feat(city): implmented the city-card

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,9 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  imports: [],
+  template: `
+    <ng-template #configurableList let-item>
+      <app-list-item>
+        <span>{{ item.name }}</span>
+        <button (click)="delete(item.id)">
+          <img class="h-5" src="assets/svg/trash.svg" />
+        </button>
+      </app-list-item>
+    </ng-template>
+    <app-card
+      [list]="cities()"
+      [cardContent]="configurableList"
+      [customBgColor]="bgColor">
+      <img ngSrc="assets/img/city.png" width="200" height="200" />
+      <button
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
+  `,
+  imports: [ListItemComponent, CardComponent, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent {}
+export class CityCardComponent implements OnInit {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
+  cities = this.store.cities;
+  bgColor = { 'background-color': 'rgba(0, 0, 250, 0.1)' };
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  addNewItem() {
+    this.store.addOne(randomCity());
+  }
+}
