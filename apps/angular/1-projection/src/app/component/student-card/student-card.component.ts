9270fa1260d578afc0b18cfd9f178b84a0a6c8b7
victor <gfeng005@uottawa.ca>
2026-01-13 14:06:39 -0500

    refactor(student-card): recontructor student card by content projection

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,40 +1,59 @@
+import { NgOptimizedImage } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
-import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
+    <ng-template #configurableList let-itemName="itemName" let-itemId="itemId">
+      <app-list-item>
+        <span>{{ itemName }}</span>
+        <button (click)="delete(itemId)">
+          <img class="h-5" src="assets/svg/trash.svg" />
+        </button>
+      </app-list-item>
+    </ng-template>
     <app-card
       [list]="students()"
-      [type]="cardType"
-      customClass="bg-light-green" />
+      [cardContent]="configurableList"
+      [customBgColor]="bgColor">
+      <img ngSrc="assets/img/student.webp" width="200" height="200" />
+      <button
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="addNewItem()">
+        Add
+      </button>
+    </app-card>
   `,
-  styles: [
-    `
-      ::ng-deep .bg-light-green {
-        background-color: rgba(0, 250, 0, 0.1);
-      }
-    `,
-  ],
-  imports: [CardComponent],
+  imports: [CardComponent, NgOptimizedImage, ListItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentCardComponent implements OnInit {
   private http = inject(FakeHttpService);
   private store = inject(StudentStore);
-
   students = this.store.students;
-  cardType = CardType.STUDENT;
+  bgColor = { 'background-color': 'rgba(0, 250, 0, 0.1)' };
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+  }
+
+  addNewItem() {
+    this.store.addOne(randStudent());
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
   }
 }
