fb4cafa95b6440474044f5e803a739593da9730a
shaparenko_v <shaparenko_v@itstep.org>
2026-01-16 08:29:49 +0100

    fix: resolve task 21-anchor-navigation

--- apps/angular/21-anchor-navigation/src/app/foo.component.ts
+++ apps/angular/21-anchor-navigation/src/app/foo.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { MatNavList } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterLink, MatNavList],
   selector: 'app-foo',
   template: `
-    Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
-    <div class="h-screen bg-blue-200">section 1</div>
+    <div class="h-screen bg-blue-200">
+      <mat-nav-list>
+        <h2>Welcome to foo page</h2>
+        <app-nav-button [routerLink]="['/home']" class="fixed left-1/2 top-3">
+          Home Page
+        </app-nav-button>
+      </mat-nav-list>
+    </div>
     <div class="h-screen bg-red-200">section 2</div>
   `,
 })
--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,19 @@
-/* eslint-disable @angular-eslint/component-selector */
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'nav-button',
+  selector: 'app-nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="routerLink()">
       <ng-content />
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
-  href = input('');
+  routerLink = input<string[] | string>('');
+  fragment = input<string | null>(null);
 }
