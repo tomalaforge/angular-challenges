8c64171fd500e8401211b56c629e7ee844731dca
Korno <korno9612@gmail.com>
2026-01-20 20:57:27 +0200

    feat: implement content projection

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -5,8 +5,14 @@ import { CardComponent } from './card.component';
   imports: [CardComponent],
   selector: 'app-root',
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <app-card>
+      <div class="title">Titre 1</div>
+      <div class="message">Message1</div>
+    </app-card>
+    <app-card>
+      <div class="title">Titre 2</div>
+      <div class="message">Aucun message</div>
+    </app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
