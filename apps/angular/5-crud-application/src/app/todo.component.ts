8ad66c65def0b878c2aaf5f92e37567465f73b94
Korno <korno9612@gmail.com>
2026-01-27 18:43:17 +0200

    feat: add mat-spinner

--- apps/angular/5-crud-application/src/app/todo.component.ts
+++ apps/angular/5-crud-application/src/app/todo.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { firstValueFrom } from 'rxjs';
 import { todo } from './todo.model';
 import { ServiceApp } from './todo.service';
 
 @Component({
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   selector: 'app-todo',
   template: `
-    @for (todo of todos(); track todo.id) {
-      {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
-      <button (click)="delete(todo)">Delete</button>
+    @if (isLoading()) {
+      <mat-spinner></mat-spinner>
+    } @else {
+      @for (todo of todos(); track todo.id) {
+        {{ todo.title }}
+        <button (click)="update(todo)">Update</button>
+        <button (click)="delete(todo)">Delete</button>
+      }
     }
   `,
   styles: [],
@@ -18,23 +23,29 @@ import { ServiceApp } from './todo.service';
 export class TodoComponent implements OnInit {
   dataStore = inject(ServiceApp);
   todos = signal<todo[]>([]);
+  isLoading = signal(true);
 
   async ngOnInit() {
     const todos = await firstValueFrom(this.dataStore.getTodos());
     this.todos.set(todos);
+    this.isLoading.set(false);
   }
 
   update(todo: todo) {
+    this.isLoading.set(true);
     this.dataStore.updateTodo(todo).subscribe((todoUpdated: todo) => {
       this.todos.update((todos) =>
         todos.map((t) => (t.id === todoUpdated.id ? todoUpdated : t)),
       );
+      this.isLoading.set(false);
     });
   }
 
   delete(todo: todo) {
+    this.isLoading.set(true);
     this.dataStore.deleteTodo(todo).subscribe(() => {
       this.todos.update((todos) => todos.filter((t) => t.id !== todo.id));
+      this.isLoading.set(false);
     });
   }
 }
