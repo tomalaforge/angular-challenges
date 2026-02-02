02ed0d7f7f03407507d9d6f030607e03a5c54793
bymshihab <khaledshihab221@gmail.com>
2026-02-02 16:36:33 +0600

    refactor(test): simplify TestComponent by removing async pipes and using direct inputs

--- apps/angular/22-router-input/src/main.ts
+++ apps/angular/22-router-input/src/main.ts
@@ -1,9 +1,8 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [provideZoneChangeDetection(), ...appConfig.providers],
-}).catch((err) => console.error(err));
+  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+});
