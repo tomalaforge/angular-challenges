c76f582f3323868074f443f235061121169384d8
Tsironis Ioannis <gtsironis8@gmail.com>
2026-03-05 18:19:07 +0200

    feat: add angular async redirect challenge solution

--- apps/angular/60-async-redirect/src/app/routes.ts
+++ apps/angular/60-async-redirect/src/app/routes.ts
@@ -1,9 +1,27 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { distinctUntilChanged, map } from 'rxjs';
 import { AdminPage } from './admin-page';
 import { App } from './app';
 import { Dashboard } from './dashboard';
 import { ProfilePage } from './profile-page';
 import { UserPage } from './user-page';
+import { UserProfileService } from './user-profile.service';
+
+const redirectTo = () => {
+  const router = inject(Router);
+
+  return inject(UserProfileService)
+    .getProfile()
+    .pipe(
+      distinctUntilChanged(),
+      map((profile) =>
+        profile === 'admin'
+          ? router.createUrlTree(['/admin'])
+          : router.createUrlTree(['/user']),
+      ),
+    );
+};
 
 export const routes: Routes = [
   {
@@ -12,6 +30,10 @@ export const routes: Routes = [
     children: [
       { path: '', pathMatch: 'full', component: Dashboard },
       { path: 'profile', component: ProfilePage },
+      {
+        path: 'navigateTo',
+        redirectTo,
+      },
       { path: 'admin', component: AdminPage },
       { path: 'user', component: UserPage },
     ],
