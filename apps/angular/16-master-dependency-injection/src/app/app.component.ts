4c2c8be945a50825fbc4c71622d5a82d7e09bbbc
Hien Pham <phhien203@gmail.com>
2026-06-23 22:22:05 +0300

    chore: answer:16 dependency injection

--- apps/angular/16-master-dependency-injection/src/app/app.component.ts
+++ apps/angular/16-master-dependency-injection/src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { TableComponent } from '@angular-challenges/shared/ui';
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Directive } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Directive,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
 import { CurrencyPipe } from './currency.pipe';
 import { CurrencyService } from './currency.service';
 import { Product, products } from './product.model';
@@ -22,9 +29,27 @@ export class ProductDirective {
   }
 }
 
-@Component({
-  imports: [TableComponent, CurrencyPipe, AsyncPipe, ProductDirective],
+@Directive({
+  selector: 'tr[currencyCode]',
   providers: [CurrencyService],
+})
+export class ProductRowDirective {
+  #currencyService = inject(CurrencyService);
+  currencyCode = input.required<string>();
+
+  #syncCodeEffectRef = effect(() => {
+    this.#currencyService.patchState({ code: this.currencyCode() });
+  });
+}
+
+@Component({
+  imports: [
+    TableComponent,
+    CurrencyPipe,
+    AsyncPipe,
+    ProductDirective,
+    ProductRowDirective,
+  ],
   selector: 'app-root',
   template: `
     <table [items]="products">
@@ -38,7 +63,7 @@ export class ProductDirective {
         </tr>
       </ng-template>
       <ng-template #body product let-product>
-        <tr>
+        <tr [currencyCode]="product.currencyCode">
           <td>{{ product.name }}</td>
           <td>{{ product.priceA | currency | async }}</td>
           <td>{{ product.priceB | currency | async }}</td>
