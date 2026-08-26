238644b549fa0554b83436f8214938104e127c52
jpaberzs <janis.paberzs18@gmail.com>
2026-08-26 20:47:12 +0300

    feat: implemented router link

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,18 +1,26 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  InputSignal,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="href()" [fragment]="fragment()">
       <ng-content />
     </a>
   `,
   changeDetection: ChangeDetectionStrategy.Eager,
+  imports: [RouterLink],
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
 })
 export class NavButtonComponent {
-  href = input('');
+  public href: InputSignal<string> = input<string>('');
+  public fragment: InputSignal<string> = input<string>('');
 }
