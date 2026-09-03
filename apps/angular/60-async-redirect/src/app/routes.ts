45d48b9bf6fd26a76bb3122d7124e3b8922548a8
jpaberzs <janis.paberzs18@gmail.com>
2026-09-03 13:55:47 +0300

    feat: added redirectTo

--- apps/angular/60-async-redirect/src/app/routes.ts
+++ apps/angular/60-async-redirect/src/app/routes.ts
@@ -1,16 +1,30 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
 import { AdminPage } from './admin-page';
 import { App } from './app';
 import { Dashboard } from './dashboard';
 import { ProfilePage } from './profile-page';
 import { UserPage } from './user-page';
+import { UserProfileService } from './user-profile.service';
 
 export const routes: Routes = [
   {
     path: '',
     component: App,
     children: [
-      { path: '', pathMatch: 'full', component: Dashboard },
+      {
+        path: '',
+        pathMatch: 'full',
+        component: Dashboard,
+      },
+      {
+        path: 'getProfile',
+        redirectTo: () => {
+          const userProfile: UserProfileService = inject(UserProfileService);
+
+          return userProfile.getProfile();
+        },
+      },
       { path: 'profile', component: ProfilePage },
       { path: 'admin', component: AdminPage },
       { path: 'user', component: UserPage },
